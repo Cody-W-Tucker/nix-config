d7bfe644483c53e4ea2a80508005204a2c6411ea
Cody W Tucker <cody@tmvsocial.com>
2024-03-02 15:37:45 -0600

    Added hyprland to home-manager

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -8,6 +8,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      <home-manager/nixos>
     ];
 
   # Run the latest kernel
@@ -18,7 +19,6 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
@@ -45,24 +45,10 @@
     LC_TIME = "en_US.UTF-8";
   };
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
   # Enable the GNOME Desktop Environment.
+  services.xserver.enable = true;
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
-
-  # Enable the Hyprland Desktop Environment.
-  programs.hyprland = {
-    enable = true;
-    xwayland.enable = true;
-  };
-
-  # Configure keymap in X11
-  services.xserver = {
-    layout = "us";
-    xkbVariant = "";
-  };
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
@@ -77,21 +63,11 @@
     alsa.support32Bit = true;
     pulse.enable = true;
     # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
+    jack.enable = true;
 
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
     #media-session.enable = true;
-  };
-
-  # When you click a link it should open the correct program, it needs a portal to do so
-  xdg.portal = { 
-    enable = true;
-  };
-
-  environment.sessionVariables = {
-    WLR_NO_HARDWARE_CURSORS = "1";
-    NIXOS_OZONE_WL = "1";
   };
 
   # Enable OpenGL
@@ -100,17 +76,17 @@
       enable = true;
       powerOnBoot = true;
     };
-    # opengl = {
-    #   enable = true;
-    #   driSupport = true;
-    #   driSupport32Bit = true;
-    #   extraPackages = with pkgs; [
-    #     intel-media-driver # LIBVA_DRIVER_NAME=iHD
-    #     vaapiIntel         # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
-    #     vaapiVdpau
-    #     libvdpau-va-gl
-    #   ];
-    # };
+    opengl = {
+      enable = true;
+      driSupport = true;
+      driSupport32Bit = true;
+      extraPackages = with pkgs; [
+        intel-media-driver # LIBVA_DRIVER_NAME=iHD
+        vaapiIntel         # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+        vaapiVdpau
+        libvdpau-va-gl
+      ];
+    };
     openrazer.enable = true;
     openrazer.devicesOffOnScreensaver = true;
   };
@@ -122,6 +98,224 @@
     extraGroups = [ "networkmanager" "wheel" "docker"];
     # packages = with pkgs; [
     # ];
+  };
+
+  # Enable the Hyprland Desktop Environment.
+  programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+  };
+
+  # When you click a link it should open the correct program, it needs a portal to do so
+  xdg.portal = { 
+    enable = true;
+  };
+
+  environment.sessionVariables = {
+    NIXOS_OZONE_WL = "1";
+  };
+
+  # Manage user environment with home-manager
+  home-manager.users.codyt = { pkgs, ... }: {
+    home.packages = with pkgs; [
+      # Add some packages to the user environment.
+      dconf
+      grim
+      slurp
+      wl-clipboard
+      nodejs
+      hyprpicker
+    ];
+    dconf.settings = {
+      "org/gnome/desktop/background" = {
+        picture-uri-dark = "file://${pkgs.nixos-artwork.wallpapers.nineish-dark-gray.src}";
+      };
+      "org/gnome/desktop/interface" = {
+        color-scheme = "prefer-dark";
+        gtk-theme = "WhiteSur-Dark";
+      };
+    };
+    home.keyboard = {
+      layout = "us";
+      model = "pc105";
+    };
+    home.pointerCursor = {
+      gtk.enable = true;
+      # x11.enable = true;
+      package = pkgs.bibata-cursors;
+      name = "Bibata-Modern-Classic";
+      size = 16;
+    };
+    gtk = {
+      enable = true;
+      iconTheme = {
+        package = pkgs.gnome.adwaita-icon-theme;
+        name = "Adwaita";
+      };
+      theme = {
+        name = "WhiteSur-Dark";
+        package = pkgs.whitesur-gtk-theme;
+      };
+      gtk3.extraConfig = {
+        gtk-application-prefer-dark-theme=1;
+      };
+      gtk4.extraConfig = {
+        gtk-application-prefer-dark-theme=1;
+      };
+    };
+    wayland.windowManager.hyprland = {
+      enable = true;
+      extraConfig = ''
+        monitor=DP-2,2560x1080@60,0x0,1
+        monitor=DP-1,2560x1080@60,0x1080,1
+        animations {
+          enabled = yes
+          bezier = myBezier, 0.05, 0.9, 0.1, 1.05
+          animation = windows, 1, 7, myBezier
+          animation = windowsOut, 1, 7, default, popin 80%
+          animation = border, 1, 10, default
+          animation = borderangle, 1, 8, default
+          animation = fade, 1, 7, default
+          animation = workspaces, 1, 6, default
+        }
+        exec-once = /home/codyt/Code/dotfiles/scripts/sleep.sh
+        exec-once = waybar
+        exec-once = mako
+        exec-once = swww init 
+        exec-once = /home/codyt/Code/dotfiles/scripts/wallpaper.sh ~/Pictures/Wallpapers
+      '';
+      settings = {
+        input = {
+          numlock_by_default = "true";
+          follow_mouse = "1";
+          sensitivity = "0";
+        };
+        general =  {
+          border_size = "2";
+          gaps_in = "5";
+          gaps_out = "20";
+          layout = "master";
+          "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
+          "col.inactive_border" = "rgba(595959aa)";
+        };
+        decoration = {
+          rounding = "10";
+          blur = {
+            enabled = "true";
+            size = "3";
+            passes = "1";
+            new_optimizations = "true";
+          };
+        drop_shadow = "yes";
+        shadow_range = "4";
+        shadow_render_power = "3";
+        "col.shadow" = "rgba(1a1a1aee)";
+        };
+        dwindle = {
+          pseudotile = "yes";
+          preserve_split = "yes";
+        };
+        master = {
+          new_is_master = "true";
+        };
+        gestures = {
+          workspace_swipe = "off";
+        };
+        misc = {
+
+          mouse_move_enables_dpms = "true";
+          key_press_enables_dpms = "true";
+          force_default_wallpaper = "0";
+        };
+        # Keybindings
+        "$mainMod" = "SUPER";
+        bindm = [
+            # Move/resize windows with mainMod + LMB/RMB and dragging
+            "$mainMod, mouse:272, movewindow"
+            "$mainMod, mouse:273, resizewindow"
+          ];
+        bind =
+          [
+            "$mainMod, RETURN, exec, pkill waybar && waybar &"
+            "$mainMod, ESCAPE, exec, wlogout"
+            "$mainMod, Q, exec, kitty"
+            "$mainMod, C, killactive"
+            "$mainMod, E, exec, nautilus"
+            "$mainMod, V, togglefloating"
+            "$mainMod, Tab, exec, rofi -show drun -show-icons"
+            "$mainMod, F, fullscreen"
+            "$mainMod SHIFT, F, fakefullscreen"
+            # Number keys (0, -, +)
+            "$mainMod, KP_Insert, exec, google-chrome-stable --app=https://chat.openai.com"
+            "$mainMod, KP_Add, exec, hyprctl dispatch exec [floating] gnome-calculator"
+            "$mainMod, KP_Enter, exec, google-chrome-stable --app=https://essay.app/home/"
+            # Number keys (1, 2, 3)
+            "$mainMod, KP_End, exec, google-chrome-stable --app=https://mail.google.com"
+            "$mainMod, KP_Down, exec, google-chrome-stable --app=https://messages.google.com/web/u/0/conversations"
+            "$mainMod, KP_Next, exec, google-chrome-stable --app=https://calendar.google.com/calendar/u/0/r"
+            # Number keys (4, 5, 6)
+            "$mainMod, KP_Left, exec, google-chrome-stable --app=https://app.asana.com/home"
+            "$mainMod, KP_Begin, exec, google-chrome-stable --app=https://app.reclaim.ai/planner?taskSort=schedule"
+            "$mainMod, KP_Right, exec, google-chrome-stable --app=https://tmvsocial.harvestapp.com/projects?filter=active"
+            # Number keys (7, 8, 9)
+            "$mainMod, KP_Home, exec, code"
+            # Skipped 8 KP_Up
+            "$mainMod, KP_Prior, exec, google-chrome-stable --app=https://tmv-social.odoo.com/web?action=277&model=account.journal&view_type=kanban&cids=1&menu_id=114"
+            # Screenshots
+            "$mainMod, S, exec, grim -g '$(slurp)'"
+            "$mainMod SHIFT, S, exec, grim -g '$(slurp)' - | wl-copy"
+            # Move focus with mainMod + arrow keys
+            "$mainMod, left, movefocus, l"
+            "$mainMod, right, movefocus, r"
+            "$mainMod, up, movefocus, u"
+            "$mainMod, down, movefocus, d"
+            # Move windows with mainMod + shift + arrow keys
+            "$mainMod SHIFT, left, movewindow, l"
+            "$mainMod SHIFT, right, movewindow, r"
+            "$mainMod SHIFT, up, movewindow, u"
+            "$mainMod SHIFT, down, movewindow, d"
+            # Special workspace (scratchpad)
+            "$mainMod, A, togglespecialworkspace, magic"
+            "$mainMod SHIFT, A, movetoworkspacesilent, special:magic"
+            # Hyprpicker color picker
+            "$mainMod, mouse:274, exec, hyprpicker -a"
+          ]
+          ++ (
+            # workspaces
+            # binds $mainMod + [shift +] {1..9} to [move to] workspace {1..9}
+            builtins.concatLists (builtins.genList (
+                x: let
+                  ws = let
+                    c = (x + 1) / 10;
+                  in
+                    builtins.toString (x + 1 - (c * 10));
+                in [
+                  "$mainMod, ${ws}, workspace, ${toString (x + 1)}"
+                  "$mainMod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
+                ]
+              )
+              10)
+          );
+      };
+    };
+    home.sessionVariables = {
+      GTK_THEME = "WhiteSur-Dark";
+      XDG_CURRENT_DESKTOP = "Hyprland";
+      XDG_SESSION_TYPE = "wayland";
+      XDG_SESSION_DESKTOP = "Hyprland";
+    };
+    programs.bash = {
+      enable = true;
+      enableCompletion = true;
+      bashrcExtra = ''
+        eval "$(direnv hook bash)"
+        eval "$(starship init bash)"
+        export PATH=$HOME/.npm-global/bin:$PATH
+      '';
+    };
+
+    # The state version is required and should stay at the version you originally installed.
+    home.stateVersion = "23.11";
   };
 
   # Enable automatic login for the user.
@@ -164,7 +358,6 @@
    swww
    kitty
    rofi-wayland
-   openrazer-daemon
    xdg-utils # Seems to be needed for noticing which desktop environment is in use
    wlogout
    swaylock-effects
@@ -180,6 +373,12 @@
    docker-compose
    docker-client
    pavucontrol
+   libreoffice-qt
+   hunspell
+   xwaylandvideobridge
+   gcalcli
+   openrazer-daemon
+   polychromatic
    #wolfram-engine
    # wget
   ];
@@ -201,6 +400,18 @@
   # Enable Docker.
   virtualisation.docker.enable = true;
 
+  # Once I figure this out I might want to start bytebase docker container on boot
+  # I need a postgres database for it to connect to, so I'll use the postgres container for that
+  # https://nixos.wiki/wiki/Docker
+  # https://mynixos.com/options/virtualisation.oci-containers.containers.%3Cname%3E
+  #   virtualisation.oci-containers = {
+  #   backend = "docker";
+  #   containers = {
+  #     foo = {
+  #       # ...
+  #     };
+  #   };
+  # };
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
