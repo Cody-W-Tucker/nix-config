e5dd78d1cf483d8ef9dd1f99a58abc1712524946
Cody W Tucker <cody@tmvsocial.com>
2024-03-09 12:06:06 -0600

    Remove sleep.sh and updatewal-swww.sh scripts

--- scripts/sleep.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# This script is used to lock the screen after a certain time of inactivity.
-
-swayidle -w timeout 900 'swaylock -f -c 000000' \
-            timeout 1800 'hyprctl dispatch dpms off' \
-            resume 'hyprctl dispatch dpms on' \
-            timeout 2700 'systemctl suspend' \
-            before-sleep 'swaylock -f -c 000000' &
