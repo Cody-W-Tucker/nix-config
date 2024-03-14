f4599ad8c899c3711377672d398044122e691916
Cody W Tucker <cody@tmvsocial.com>
2024-03-13 22:24:01 -0500

    Update NixOS configuration and home-manager settings

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -19,7 +19,6 @@ in
     [
       # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      <home-manager/nixos>
       ./config/system
     ];
 
@@ -116,80 +115,6 @@ in
       xwaylandvideobridge
     ];
     shell = pkgs.zsh;
-  };
-
-  # Manage user environment with home-manager
-  home-manager.users.codyt = { pkgs, ... }: {
-    home.packages = with pkgs; [
-      # Add some packages to the user environment.
-      dconf
-      grim
-      slurp
-      wl-clipboard
-      hyprpicker
-      starship
-    ];
-    imports = [ ./config/home ];
-    dconf = {
-      enable = true;
-      settings = {
-        "org/gnome/desktop/interface" = {
-          color-scheme = "prefer-dark";
-        };
-      };
-    };
-    # X keyboard
-    home.keyboard = {
-      layout = "us";
-      model = "pc104";
-    };
-    home.pointerCursor = {
-      gtk.enable = true;
-      # x11.enable = true;
-      package = pkgs.bibata-cursors;
-      name = "Bibata-Modern-Classic";
-      size = 24;
-    };
-    gtk = {
-      enable = true;
-      iconTheme = {
-        package = pkgs.gnome.adwaita-icon-theme;
-        name = "Adwaita";
-      };
-      theme = {
-        name = "WhiteSur-Dark-solid";
-        package = pkgs.whitesur-gtk-theme;
-      };
-      gtk3.extraConfig = {
-        gtk-application-prefer-dark-theme = 1;
-      };
-      gtk4.extraConfig = {
-        gtk-application-prefer-dark-theme = 1;
-      };
-    };
-
-    # Theme QT -> GTK
-    qt = {
-      enable = true;
-      platformTheme = "gtk";
-      style = {
-        name = "adwaita-dark";
-        package = pkgs.adwaita-qt;
-      };
-    };
-    home.sessionVariables = {
-      GTK_THEME = "WhiteSur-Dark";
-      BROWSER = "google-chrome";
-      EDITOR = "nvim";
-      VISUAL = "code";
-      TERMINAL = "kitty";
-      LIBVA_DRIVER_NAME = "iHD";
-      VDPAU_DRIVER = "va_gl";
-      NIXOS_OZONE_WL = "1";
-    };
-
-    # The state version is required and should stay at the version you originally installed.
-    home.stateVersion = "23.11";
   };
 
   fonts = {
--- nixos/flake.nix
+++ nixos/flake.nix
@@ -2,37 +2,52 @@
   description = "CodyOS";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-  };
-
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
-  };
-}
-
-{
-  description = "Nixos config flake";
-
-  inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-
-    # home-manager = {
-    #   url = "github:nix-community/home-manager";
+    home-manager.url = "github:nix-community/home-manager/master";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    nix-colors.url = "github:misterio77/nix-colors";
+    hyprland.url = "github:hyprwm/Hyprland";
+    # nixvim = {
+    #   url = "github:nix-community/nixvim";
     #   inputs.nixpkgs.follows = "nixpkgs";
     # };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
-    nixosConfigurations.default = nixpkgs.lib.nixosSystem {
-      specialArgs = { inherit inputs; };
-      modules = [
-        ./configuration.nix
-        # inputs.home-manager.nixosModules.default
-      ];
+  outputs = inputs@{ nixpkgs, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+      username = "codyt";
+      hostname = "nixos";
+
+      pkgs = import nixpkgs {
+        inherit system;
+        config = {
+          allowUnfree = true;
+        };
+      };
+    in
+    {
+      nixosConfigurations = {
+        "${hostname}" = nixpkgs.lib.nixosSystem {
+          specialArgs = {
+            inherit system; inherit inputs;
+            inherit username; inherit hostname;
+          };
+          modules = [
+            ./configuration.nix
+            inputs.home-manager.nixosModules.home-manager
+            {
+              home-manager.extraSpecialArgs = {
+                inherit username; inherit inputs;
+                inherit (inputs.nix-colors.lib-contrib { inherit pkgs; }) gtkThemeFromScheme;
+              };
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.backupFileExtension = "backup";
+              home-manager.users.${username} = import ./home.nix;
+            }
+          ];
+        };
+      };
     };
-  };
 }
