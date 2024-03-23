10fddea29a5f08ba367a8e977d94223f1c4b9d35
Cody W Tucker <cody@tmvsocial.com>
2024-03-23 11:25:08 -0500

    Delete apply-hm-env script and update run-as-service script

--- nixos/config/scripts/run-as-service.nix
+++ nixos/config/scripts/run-as-service.nix
@@ -1,6 +1,19 @@
 { pkgs, config, inputs, lib, ... }:
 
 {
+  apply-hm-env = pkgs.writeShellScript "apply-hm-env" ''
+    ${lib.optionalString (config.home.sessionPath != []) ''
+      export PATH=${builtins.concatStringsSep ":" config.home.sessionPath}:$PATH
+    ''}
+    ${builtins.concatStringsSep "\n" (
+      lib.mapAttrsToList (k: v: ''
+        export ${k}=${toString v}
+      '')
+      config.home.sessionVariables
+    )}
+    ${config.home.sessionVariablesExtra}
+    exec "$@"
+  '';
 
   # runs processes as systemd transient services
   run-as-service = pkgs.writeShellScriptBin "run-as-service" ''
