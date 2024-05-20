2e94aadce8dee0ad6485ea312752d543cf575d49
Cody W Tucker <cody@tmvsocial.com>
2024-05-20 14:17:58 -0500

    Update GTK theme and add dark mode support

--- nixos/home.nix
+++ nixos/home.nix
@@ -73,6 +73,22 @@ in
     layout = "us";
     model = "pc104";
   };
+
+  # Theme GTK
+  gtk = {
+    enable = true;
+    iconTheme = {
+      package = pkgs.gnome.adwaita-icon-theme;
+      name = "Adwaita";
+    };
+    gtk3.extraConfig = {
+      gtk-application-prefer-dark-theme = 1;
+    };
+    gtk4.extraConfig = {
+      gtk-application-prefer-dark-theme = 1;
+    };
+  };
+
   home.sessionVariables = {
     BROWSER = "google-chrome";
     EDITOR = "code --wait";
