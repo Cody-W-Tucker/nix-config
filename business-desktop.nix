eaa62b6fc863794e93f66c30a9eaa525bb727b2f
Cody W Tucker <cody@tmvsocial.com>
2024-06-15 17:12:57 -0500

    Enable pipewire audio services and update configuration

--- business-desktop.nix
+++ business-desktop.nix
@@ -88,16 +88,6 @@
     };
   };
 
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # jack.enable = true;
-  };
-
   # Enable support for removable devices.
   services.gvfs.enable = true;
   services.udisks2.enable = true;
