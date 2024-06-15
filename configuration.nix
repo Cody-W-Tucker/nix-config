6c3bb84b1517c3df8cd6143fbbbc4056ca558366
Cody W Tucker <cody@tmvsocial.com>
2024-06-15 17:07:27 -0500

    feat: Update hashed passwords for users Cody Tucker and Jordan Tucker
    This commit updates the hashed passwords for the users Cody Tucker and Jordan Tucker in the `configuration.nix` file. The new hashed passwords provide enhanced security for their user accounts.

--- configuration.nix
+++ configuration.nix
@@ -102,7 +102,8 @@
     description = "Cody Tucker";
     extraGroups = [ "networkmanager" "wheel" "docker" ];
     shell = pkgs.zsh;
-    hashedPasswordFile = config.sops.secrets.codyt.path;
+    hashedPassword = "$y$j9T$2gGzaHfv1JMUMtHdaXBGF/$RoEaBINI46v1yFpR1bSgPc9ovAyzqjgSSTxuNhRiOn4";
+    # hashedPasswordFile = config.sops.secrets.codyt.path;
     packages = with pkgs; [
     #  thunderbird
     ];
@@ -117,7 +118,8 @@
     description = "Jordan Tucker";
     extraGroups = [ "networkmanager" "wheel" ];
     shell = pkgs.zsh;
-    hashedPasswordFile = config.sops.secrets.jordant.path;
+    hashedPassword = "$y$j9T$T1YkmagP6ULuI6DvQz8IK0$38YlfZN9eR1jH286/9kZn13flzy.wFtPX74ukXKJhM7";
+    # hashedPasswordFile = config.sops.secrets.jordant.path;
     packages = with pkgs; [
     # thunderbird
     ];
