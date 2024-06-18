37cafa5160c46c95b350430278fc7fae5cd75f39
Cody W Tucker <cody@tmvsocial.com>
2024-06-18 17:04:41 -0500

    Pre-update commit

---
+++ modules/common/desktop/printers.nix
@@ -0,0 +1,23 @@
+{ pkgs, config, lib, ... }: {
+
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    openFirewall = true;
+    publish = {
+      enable = true;
+      userServices = true;
+    };
+  };
+  services.printing = {
+    listenAddresses = [ "*:631" ];
+    allowFrom = [ "all" ];
+    browsing = true;
+    defaultShared = true;
+    openFirewall = true;
+  };
+
+  # environment.systemPackages = with pkgs; [
+  #   system-config-printer
+  # ];
+}
