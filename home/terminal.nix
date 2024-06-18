e240a4880aa8a8396a4c04da9f6b221f6db33cdc
Cody W Tucker <cody@tmvsocial.com>
2024-06-18 10:58:36 -0500

    ```text chore: Update terminal shell aliases and commands
    Refactor the shellAliases section in the terminal.nix file to update and improve the existing shell aliases and commands. This includes adding new aliases for pulling updates and upgrades from the git repository, as well as committing and pushing changes. The purpose of this change is to enhance the developer workflow and make it easier to manage the NixOS configuration.

--- home/terminal.nix
+++ home/terminal.nix
@@ -140,8 +140,23 @@
       shellAliases = {
         ssh = "kitty +kitten ssh";
         ll = "ls -l";
-        update = "sudo nixos-rebuild switch --option eval-cache false";
-        upgrade = "nix flake update /etc/nixos && sudo nixos-rebuild switch --option eval-cache false";
+        pullUpdate = "cd /etc/nixos && git pull && sudo nixos-rebuild switch";
+        pullUpgrade = "cd /etc/nixos && git pull && sudo nix flake update /etc/nixos && sudo nixos-rebuild switch";
+        update = ''
+          cd /etc/nixos &&
+          git add . &&
+          git commit -m "Pre-update commit" &&
+          sudo nixos-rebuild switch &&
+          git push
+        '';
+        upgrade = ''
+          cd /etc/nixos &&
+          git add . &&
+          git commit -m "Pre-upgrade commit" &&
+          sudo nix flake update /etc/nixos &&
+          sudo nixos-rebuild switch &&
+          git push
+        '';
         gcCleanup = "nix-collect-garbage --delete-old && sudo nix-collect-garbage -d && sudo /run/current-system/bin/switch-to-configuration boot";
       };
       initExtra = ''
