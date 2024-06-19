6527ea2b0059182f9b39dcc9647abf8e4834a083
Cody W Tucker <cody@tmvsocial.com>
2024-06-19 11:21:10 -0500

    chore: remove desktop config from server base config

--- modules/common/desktop/default.nix
+++ modules/common/desktop/default.nix
@@ -5,4 +5,57 @@
     ./printers.nix
   ];
 
+  services.displayManager.autoLogin.enable = true;
+
+  # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
+  systemd.services."getty@tty1".enable = false;
+  systemd.services."autovt@tty1".enable = false;
+
+  # Default Display Manager and Windowing system.
+  services = {
+    # Set up the X11 windowing system.
+    xserver = {
+      enable = true;
+      displayManager.gdm.enable = true;
+      desktopManager.gnome.enable = true;
+      xkb = {
+        layout = "us";
+        model = "pc105";
+      };
+    };
+  };
+
+  # Enable the Hyprland Desktop Environment.
+  programs.hyprland = {
+    enable = true;
+    package = inputs.hyprland.packages.${pkgs.system}.hyprland;
+  };
+
+  #xdg  
+  xdg.portal = {
+    enable = true;
+    extraPortals = [
+      pkgs.xdg-desktop-portal
+    ];
+    configPackages = [
+      pkgs.xdg-desktop-portal-hyprland
+      pkgs.xdg-desktop-portal
+    ];
+  };
+
+  # List packages installed in system profile. To search, run:
+  environment.systemPackages = with pkgs; [
+    google-chrome
+    firefox
+    obsidian
+    feh
+    zathura
+    pavucontrol
+    polkit_gnome
+    xdg-utils # xdg-open
+    libreoffice
+    hunspell
+    vlc
+  ];
+
 }
