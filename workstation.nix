d3512f6e269922846f504ef12b9e25de08255799
Cody W Tucker <cody@tmvsocial.com>
2024-06-24 14:31:56 -0500

    Pre-update commit

--- workstation.nix
+++ workstation.nix
@@ -38,11 +38,17 @@
       options = [ "fmask=0022" "dmask=0022" ];
     };
 
-  # fileSystems."/home/codyt" =
-  #   {
-  #     device = "//192.168.254.25/home/codyt/Share";
-  #     fsType = "cifs";
-  #   };
+  environment.systemPackages = [ pkgs.cifs-utils ];
+  fileSystems."/mnt/share" = {
+    device = "//192.168.254.25/home/codyt/Share";
+    fsType = "cifs";
+    options =
+      let
+        automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s,user,users";
+
+      in
+      [ "${automount_opts},credentials=/etc/nixos/secrets/smb-secrets,${config.users.users.codyt.uid},gid=${config.users.groups.users.gid}" ];
+  };
 
   swapDevices = [ ];
 
