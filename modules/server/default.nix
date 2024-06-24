897ea2d47602f99be7a921821da3f1070116b3e2
Cody W Tucker <cody@tmvsocial.com>
2024-06-24 16:38:23 -0500

    Pre-update commit

--- modules/server/default.nix
+++ modules/server/default.nix
@@ -33,6 +33,26 @@
         "force user" = "codyt";
         "force group" = "users";
       };
+      media = {
+        path = "/mnt/media";
+        browseable = "yes";
+        "read only" = "no";
+        "guest ok" = "no";
+        "create mask" = "0644";
+        "directory mask" = "0755";
+        "force user" = "codyt";
+        "force group" = "users";
+      };
+      photos = {
+        path = "/mnt/photos";
+        browseable = "yes";
+        "read only" = "no";
+        "guest ok" = "no";
+        "create mask" = "0644";
+        "directory mask" = "0755";
+        "force user" = "codyt";
+        "force group" = "users";
+      };
     };
   };
 
