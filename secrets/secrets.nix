7678fb47abb3286f35f58cc068312f11df16e454
Cody W Tucker <cody@tmvsocial.com>
2024-06-25 22:03:19 -0500

    Pre-update commit

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,7 @@
 { pkgs, inputs, config, ... }:
 
 {
-  sops.defaultSopsFile = ./secrets/secrets.yaml;
+  sops.defaultSopsFile = ./secrets.yaml;
 
   # This will automatically import SSH keys as age keys
   sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
