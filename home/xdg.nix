30bdea017d6e7aa19c9519047f6ceef5ba0ff76e
Cody W Tucker <cody@tmvsocial.com>
2024-06-26 10:30:48 -0500

    Pre-update commit

--- home/xdg.nix
+++ home/xdg.nix
@@ -7,10 +7,17 @@ in
     enable = true;
     userDirs = {
       enable = true;
+      createDirectories = true;
       documents = "/mnt/share/Documents";
       music = "/mnt/share/Music";
       pictures = "/mnt/share/Pictures";
       videos = "/mnt/share/Videos";
+      extraConfig = {
+        Business = "/mnt/share/Business";
+        Projects = "/mnt/share/Projects";
+        Records = "/mnt/share/Records";
+        Tools = "/mnt/share/Tools";
+      };
     };
   };
 }
