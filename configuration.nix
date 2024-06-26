c1bfbc11eb44da50f786885354482b1768305b80
Cody W Tucker <cody@tmvsocial.com>
2024-06-25 22:11:38 -0500

    Pre-update commit

--- configuration.nix
+++ configuration.nix
@@ -43,11 +43,11 @@
   # Make passwords uneditable
   users.mutableUsers = false;
 
-  # Create the passwords so they exist across all hosts
-  sops.secrets = {
-    codyt.neededForUsers = true;
-    jordant.neededForUsers = true;
-  };
+  # # Create the passwords so they exist across all hosts
+  # sops.secrets = {
+  #   codyt.neededForUsers = true;
+  #   jordant.neededForUsers = true;
+  # };
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.codyt = {
