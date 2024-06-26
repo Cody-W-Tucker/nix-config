32199094d5518d25109877514d867c9a12b22eb3
Cody W Tucker <cody@tmvsocial.com>
2024-06-25 22:40:50 -0500

    feat: Enable ACME certificate acceptance and update email address

--- modules/server/photos.nix
+++ modules/server/photos.nix
@@ -34,35 +34,35 @@
     }];
   };
 
-  # ACME Certificates
-  security.acme.defaults.email = "cody@tmvsocial.com";
-  security.acme.acceptTerms = true;
-  security.acme.certs.codyt.dnsProvider = "cloudflare";
+  # # ACME Certificates
+  # security.acme.defaults.email = "cody@tmvsocial.com";
+  # security.acme.acceptTerms = true;
+  # security.acme.certs.codyt.dnsProvider = "cloudflare";
 
-  # NGINX
-  services.nginx = {
-    enable = true;
-    recommendedTlsSettings = true;
-    recommendedOptimisation = true;
-    recommendedGzipSettings = true;
-    recommendedProxySettings = true;
-    clientMaxBodySize = "500m";
-    virtualHosts = {
-      "sub.domain.tld" = {
-        forceSSL = true;
-        enableACME = true;
-        http2 = true;
-        locations."/" = {
-          proxyPass = "http://127.0.0.1:2342";
-          proxyWebsockets = true;
-          extraConfig = ''
-            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-            proxy_set_header Host $host;
-            proxy_buffering off;
-            proxy_http_version 1.1;
-          '';
-        };
-      };
-    };
-  };
+  # # NGINX
+  # services.nginx = {
+  #   enable = true;
+  #   recommendedTlsSettings = true;
+  #   recommendedOptimisation = true;
+  #   recommendedGzipSettings = true;
+  #   recommendedProxySettings = true;
+  #   clientMaxBodySize = "500m";
+  #   virtualHosts = {
+  #     "sub.domain.tld" = {
+  #       forceSSL = true;
+  #       enableACME = true;
+  #       http2 = true;
+  #       locations."/" = {
+  #         proxyPass = "http://127.0.0.1:2342";
+  #         proxyWebsockets = true;
+  #         extraConfig = ''
+  #           proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+  #           proxy_set_header Host $host;
+  #           proxy_buffering off;
+  #           proxy_http_version 1.1;
+  #         '';
+  #       };
+  #     };
+  #   };
+  # };
 }
