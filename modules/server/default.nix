31dd347b1949e1bafe7307fbb14fafba2a9df863
Cody W Tucker <cody@tmvsocial.com>
2024-06-29 15:34:47 -0500

    chore: Update server configuration to remove unused dnsProvider and proxy_set_header directives

--- modules/server/default.nix
+++ modules/server/default.nix
@@ -13,7 +13,6 @@
     acceptTerms = true;
     defaults = {
       email = "cody@tmvsocial.com";
-      dnsProvider = "cloudflare";
       server = "https://acme-staging-v02.api.letsencrypt.org/directory";
     };
   };
