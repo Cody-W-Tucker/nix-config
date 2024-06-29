10f3cd8200438cf55ed13cd6048fb3e8249fd578
Cody W Tucker <cody@tmvsocial.com>
2024-06-29 13:07:59 -0500

    chore: Remove unused proxy_http_version directive in server configuration

--- modules/server/photos.nix
+++ modules/server/photos.nix
@@ -55,8 +55,6 @@
             proxy_set_header Host $host;
             proxy_buffering off;
             proxy_http_version 1.1;
-            proxy_set_header Upgrade $http_upgrade;
-            proxy_set_header Connection "upgrade";
           '';
         };
       };
