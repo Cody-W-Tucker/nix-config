741c7e53700ca0a436cc08f7dc1fe5f424bc5ac4
Cody W Tucker <cody@tmvsocial.com>
2024-06-29 16:44:57 -0500

    chore: Update server configuration to use lazy loading for images and remove unused proxy_set_header directives

--- modules/server/homeAssistant.nix
+++ modules/server/homeAssistant.nix
@@ -13,6 +13,25 @@
       # Includes dependencies for a basic setup
       # https://www.home-assistant.io/integrations/default_config/
       default_config = { };
+      http = {
+        server_host = "::1";
+        trusted_proxies = [ "::1" ];
+        use_x_forwarded_for = true;
+      };
+    };
+  };
+  services.nginx = {
+    recommendedProxySettings = true;
+    virtualHosts."home.homehub.tv" = {
+      forceSSL = false;
+      enableACME = false;
+      extraConfig = ''
+        proxy_buffering off;
+      '';
+      locations."/" = {
+        proxyPass = "http://[::1]:8123";
+        proxyWebsockets = true;
+      };
     };
   };
   networking.firewall.allowedTCPPorts = [ 8123 ];
