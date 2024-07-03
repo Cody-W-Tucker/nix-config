7afbf9032d511b9814f5a9b3285e9a0b98c72697
Cody W Tucker <cody@tmvsocial.com>
2024-07-03 15:26:29 -0500

    chore: Remove commented out JWT secret file path for OnlyOffice configuration

--- modules/server/nextcloud.nix
+++ modules/server/nextcloud.nix
@@ -46,7 +46,7 @@
     onlyoffice = {
       enable = true;
       hostname = "docs.homehub.tv";
-      jwtSecretFile = "/etc/onlyoffice/DocumentServer/local.json";
+      # jwtSecretFile = "/etc/onlyoffice/DocumentServer/local.json";
     };
   };
 }
