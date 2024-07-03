7a289cef3198bb4f2dba226d5a70f587f5b024f3
Cody W Tucker <cody@tmvsocial.com>
2024-07-03 15:01:50 -0500

    Pre-update commit

--- configuration.nix
+++ configuration.nix
@@ -44,10 +44,16 @@
   users.mutableUsers = false;
 
   # Create the passwords so they exist across all hosts
-  sops.secrets = {
-    codyt.neededForUsers = true;
-    jordant.neededForUsers = true;
-  };
+  # sops.secrets = {
+  #   codyt = {
+  #     neededForUsers = true;
+  #     owner = config.users.users.codyt.name;
+  #   };
+  #   jordant = {
+  #     neededForUsers = true;
+  #     owner = config.users.users.jordant.name;
+  #   };
+  # };
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.codyt = {
@@ -55,8 +61,8 @@
     description = "Cody Tucker";
     extraGroups = [ "networkmanager" "wheel" "docker" ];
     shell = pkgs.zsh;
-    hashedPasswordFile = config.sops.secrets.codyt.path;
-    #hashedPassword = "$y$j9T$2gGzaHfv1JMUMtHdaXBGF/$RoEaBINI46v1yFpR1bSgPc9ovAyzqjgSSTxuNhRiOn4";
+    #hashedPasswordFile = config.sops.secrets.codyt.path;
+    hashedPassword = "$y$j9T$2gGzaHfv1JMUMtHdaXBGF/$RoEaBINI46v1yFpR1bSgPc9ovAyzqjgSSTxuNhRiOn4";
     packages = with pkgs; [
       #  thunderbird
     ];
@@ -71,8 +77,8 @@
     description = "Jordan Tucker";
     extraGroups = [ "networkmanager" "wheel" ];
     shell = pkgs.zsh;
-    hashedPasswordFile = config.sops.secrets.jordant.path;
-    #hashedPassword = "$y$j9T$T1YkmagP6ULuI6DvQz8IK0$38YlfZN9eR1jH286/9kZn13flzy.wFtPX74ukXKJhM7";
+    #hashedPasswordFile = config.sops.secrets.jordant.path;
+    hashedPassword = "$y$j9T$T1YkmagP6ULuI6DvQz8IK0$38YlfZN9eR1jH286/9kZn13flzy.wFtPX74ukXKJhM7";
     packages = with pkgs; [
       # thunderbird
     ];
