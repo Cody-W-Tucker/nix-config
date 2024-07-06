b0c34b9bfeeef1bbef90b1349dc5dfe7e8ca0e7b
Cody W Tucker <cody@tmvsocial.com>
2024-07-06 14:03:58 -0500

    chore: Update AI server configuration to fix SEARXNG_QUERY_URL

--- modules/server/ai.nix
+++ modules/server/ai.nix
@@ -25,7 +25,7 @@
         RAG_WEB_SEARCH_ENGINE = "searxng";
         RAG_WEB_SEARCH_RESULT_COUNT = "3";
         RAG_WEB_SEARCH_CONCURRENT_REQUESTS = "10";
-        SEARXNG_QUERY_URL = "http://homehub.tv:8888//search?q=<query>";
+        SEARXNG_QUERY_URL = "http://homehub.tv:8888/search?q=<query>";
       };
     };
     searx = {
