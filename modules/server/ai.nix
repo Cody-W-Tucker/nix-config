bde7f0657b4c69f6aeb7aa395b5fc9b26a674ed5
Cody W Tucker <cody@tmvsocial.com>
2024-07-06 10:14:41 -0500

    chore: Update AI server configuration to enable Ollama service and open web UI

--- modules/server/ai.nix
+++ modules/server/ai.nix
@@ -2,7 +2,27 @@
 
 {
   # Ollama local llm
-  services.ollama = {
-    enable = true;
+  services = {
+    ollama = {
+      enable = true;
+      port = 11434;
+      openFirewall = true;
+      host = "0.0.0.0";
+      loadModules = [ "codellama" "llama3" ];
+    };
+    open-webui = {
+      enable = true;
+      port = 11435;
+      host = "0.0.0.0";
+      openFirewall = true;
+      environment = {
+        OLLAMA_API_BASE_URL = "http://127.0.0.1:11434";
+        # Disable authentication
+        WEBUI_AUTH = "False";
+        ANONYMIZED_TELEMETRY = "False";
+        DO_NOT_TRACK = "True";
+        SCARF_NO_ANALYTICS = "True";
+      };
+    };
   };
 }
