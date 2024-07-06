88796e6dcbf8c4084dfa775a65fcd8613fc45eb5
Cody W Tucker <cody@tmvsocial.com>
2024-07-06 13:56:43 -0500

    chore: Update AI server configuration to use string value for RAG_WEB_SEARCH_CONCURRENT_REQUESTS

--- modules/server/ai.nix
+++ modules/server/ai.nix
@@ -24,7 +24,7 @@
         ENABLE_RAG_WEB_SEARCH = "True";
         RAG_WEB_SEARCH_ENGINE = "searxng";
         RAG_WEB_SEARCH_RESULT_COUNT = 3;
-        RAG_WEB_SEARCH_CONCURRENT_REQUESTS = 10;
+        RAG_WEB_SEARCH_CONCURRENT_REQUESTS = "10";
         SEARXNG_QUERY_URL = "http://homehub.tv:8888//search?q=<query>";
       };
     };
