280ecc0e78f7507c59d5dd4a4bf74b342bd2003d
Cody W Tucker <cody@tmvsocial.com>
2024-07-08 11:33:09 -0500

    chore: Update Nextcloud configuration to enable Collabora Online document editing

--- modules/server/nextcloud.nix
+++ modules/server/nextcloud.nix
@@ -2,11 +2,77 @@
 
 {
   environment.etc."nextcloud-admin-pass".text = "admin";
+
+  virtualisation = {
+    docker.enable = true;
+    oci-containers.backend = "docker";
+    # Online document editing
+    oci-containers.containers.collabora = {
+      image = "docker.io/collabora/code:latest";
+      ports = [ "9980:9980/tcp" ];
+      environment = {
+        domain = "homehub.tv";
+        dictionaries = "en_US";
+        extra_params = "--o:ssl.enable=false --o:ssl.termination=true";
+      };
+      extraOptions = [
+        "--pull=newer"
+      ];
+    };
+  };
+
   services = {
     nginx.virtualHosts = {
       "homehub.tv" = {
         forceSSL = false;
         enableACME = false;
+      };
+      "docs.homehub.tv" = {
+        enableACME = fales;
+        forceSSL = fales;
+        extraConfig = ''
+           # static files
+           location ^~ /browser {
+             proxy_pass http://127.0.0.1:9980;
+             proxy_set_header Host $host;
+           }
+
+           # WOPI discovery URL
+           location ^~ /hosting/discovery {
+             proxy_pass http://127.0.0.1:9980;
+             proxy_set_header Host $host;
+           }
+
+           # Capabilities
+           location ^~ /hosting/capabilities {
+             proxy_pass http://127.0.0.1:9980;
+             proxy_set_header Host $host;
+          }
+
+          # main websocket
+          location ~ ^/cool/(.*)/ws$ {
+            proxy_pass http://127.0.0.1:9980;
+            proxy_set_header Upgrade $http_upgrade;
+            proxy_set_header Connection "Upgrade";
+            proxy_set_header Host $host;
+            proxy_read_timeout 36000s;
+          }
+
+          # download, presentation and image upload
+          location ~ ^/(c|l)ool {
+            proxy_pass http://127.0.0.1:9980;
+            proxy_set_header Host $host;
+          }
+
+          # Admin Console websocket
+          location ^~ /cool/adminws {
+            proxy_pass http://127.0.0.1:9980;
+            proxy_set_header Upgrade $http_upgrade;
+            proxy_set_header Connection "Upgrade";
+            proxy_set_header Host $host;
+            proxy_read_timeout 36000s;
+          }
+        '';
       };
     };
     nextcloud = {
@@ -36,11 +102,6 @@
         trusted_proxies = [ "127.0.0.1" ];
         trusted_domains = [ "homehub.tv" "docs.homehub.tv" ];
       };
-    };
-
-    onlyoffice = {
-      enable = true;
-      hostname = "docs.homehub.tv";
     };
   };
 }
