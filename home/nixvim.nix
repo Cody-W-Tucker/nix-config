6f96ff48b45b4a9d67619ca005ce5c5956ff99d9
Cody W Tucker <cody@tmvsocial.com>
2024-07-09 21:40:45 -0500

    Pre-update commit

--- home/nixvim.nix
+++ home/nixvim.nix
@@ -6,5 +6,16 @@
     defaultEditor = true;
     viAlias = true;
     vimAlias = true;
+    opts = {
+      number = true;
+      relativenumber = true;
+      shiftwidth = 2;
+    };
+    profiles = {
+      telescope.enable = true;
+      treesitter.enable = true;
+      lsp.enable = true;
+      direnv.enable = true;
+    };
   };
 }
