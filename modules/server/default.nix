244f57c7af34ac47199b72f52fffdc61a63c3f1d
Cody W Tucker <cody@tmvsocial.com>
2024-07-17 17:03:23 -0500

    feat: Add Cloudflare API credentials for SSL certificate management

--- modules/server/default.nix
+++ modules/server/default.nix
@@ -11,12 +11,26 @@
     # ./automation.nix
   ];
 
+  # Create the acme secret in sops
+  sops.secrets.cloudflare-api-email = { };
+  sops.secrets.cloudflare-api-key = { };
+
   # Acme for SSL
   security.acme = {
     acceptTerms = true;
     defaults = {
       email = "cody@tmvsocial.com";
       server = "https://acme-staging-v02.api.letsencrypt.org/directory";
+    };
+    certs."homehub.tv" = {
+      domain = "homehub.tv";
+      extraDomainNames = [ "*.homehub.tv" ];
+      dnsProvider = "cloudflare";
+      dnsPropagationCheck = true;
+      # inspo: https://go-acme.github.io/lego/dns/cloudflare/
+      credentialFiles = {
+        "CLOUDFLARE_DNS_API_TOKEN_FILE" = config.sops.secrets.cloudflare-api-key.path;
+      };
     };
   };
 
