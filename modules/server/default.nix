0052f9d6b37ddcc5e5a928bfd0c53fbe2eec86ad
Cody W Tucker <cody@tmvsocial.com>
2024-07-18 10:06:28 -0500

    chore: Update DNS resolver configuration in server

--- modules/server/default.nix
+++ modules/server/default.nix
@@ -22,7 +22,7 @@
     defaults = {
       email = "cody@tmvsocial.com";
       server = "https://acme-staging-v02.api.letsencrypt.org/directory";
-      dnsResolver = "192.168.254.38:53";
+      dnsResolver = "192.168.254.25:53";
     };
     certs."homehub.tv" = {
       domain = "homehub.tv";
