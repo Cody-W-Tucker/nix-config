39f7b151af7a1939d2971b94426c5f8f9837c682
Cody W Tucker <cody@tmvsocial.com>
2024-07-19 14:06:29 -0500

    chore: Update server configuration to enable SSL and use ACME for homehub.tv domains

--- modules/server/ai.nix
+++ modules/server/ai.nix
@@ -12,26 +12,41 @@
           proxyWebsockets = true;
         };
       };
+      "ai.homehub.tv" = {
+        useACMEHost = "homehub.tv";
+        forceSSL = true;
+        locations."/" = {
+          proxyPass = "http://192.168.254.25:11434";
+          proxyWebsockets = true;
+        };
+      };
+      "ollama.homehub.tv" = {
+        useACMEHost = "homehub.tv";
+        forceSSL = true;
+        locations."/" = {
+          proxyPass = "http://192.168.254.25:11434";
+        };
+      };
     };
     ollama = {
       enable = true;
       port = 11434;
-      openFirewall = true;
+      openFirewall = false;
       host = "0.0.0.0";
     };
     open-webui = {
       enable = true;
       port = 11435;
       host = "0.0.0.0";
-      openFirewall = true;
+      openFirewall = false;
       environment = {
-        OLLAMA_API_BASE_URL = "http://192.168.254.25:11434";
+        OLLAMA_API_BASE_URL = "https://ollama.homehub.tv";
         # Disable authentication
         WEBUI_AUTH = "False";
         ANONYMIZED_TELEMETRY = "False";
         DO_NOT_TRACK = "True";
         SCARF_NO_ANALYTICS = "True";
-        SEARXNG_QUERY_URL = "http://search.homehub.tv/search?q=<query>";
+        SEARXNG_QUERY_URL = "https://search.homehub.tv/search?q=<query>";
         USER_AGENT = "Ollama";
         ENABLE_RAG_WEB_LOADER_SSL_VERIFICATION = "False";
         AIOHTTP_CLIENT_TIMEOUT = "600";
