4ca13d56d3b355a4a2c3b907590f0b3efdc45bd8
Cody W Tucker <cody@tmvsocial.com>
2024-07-19 14:43:06 -0500

    chore: Update nginx virtual host configuration for budget.homehub.tv

--- modules/containers/actualBudget.nix
+++ modules/containers/actualBudget.nix
@@ -9,6 +9,14 @@
     extraOptions = [ "--pull=always" ];
   };
 
+  services.nginx.virtualHosts."budget.homehub.tv" = {
+    forceSSL = true;
+    useACMEHost = "homehub.tv";
+    locations."/" = {
+      proxyPass = "http://localhost:5006";
+      proxyWebsockets = true;
+    };
+  };
 
 }
 # docker run --pull=always --restart=unless-stopped -d -p 5006:5006 -v /var/lib/actual-budget:/data --name actualBudget ghcr.io/actualbudget/actual-server:latest
