5307989f5c6067bff8f63c3abffbcddaffbd4bd9
Cody W Tucker <cody@tmvsocial.com>
2024-07-20 13:22:01 -0500

    chore: Update Syncthing configuration to use 0.0.0.0:8384 as the GUI address

--- modules/server/syncthing.nix
+++ modules/server/syncthing.nix
@@ -5,27 +5,16 @@
     nginx.virtualHosts."backup.homehub.tv" = {
       forceSSL = true;
       useACMEHost = "homehub.tv";
-      http2 = true;
       locations."/" = {
         proxyPass = "http://127.0.0.1:8384/";
-        proxyWebsockets = true;
-        extraConfig = ''
-          proxy_set_header        Host $host;
-          proxy_set_header        X-Real-IP $remote_addr;
-          proxy_set_header        X-Forwarded-For $proxy_add_x_forwarded_for;
-          proxy_set_header        X-Forwarded-Proto $scheme;
-          proxy_read_timeout      600s;
-          proxy_send_timeout      600s;
-        '';
       };
     };
     syncthing = {
       enable = true;
       user = "codyt";
       openDefaultPorts = true;
-      guiAddress = "127.0.0.1:8384";
+      guiAddress = "0.0.0.0:8384";
       configDir = "/home/codyt/Backup/.config/syncthing";
     };
   };
-
 }
