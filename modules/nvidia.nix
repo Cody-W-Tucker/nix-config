8e5bc3a751dfd310df32bc671cb9690aaf25ead2
Cody W Tucker <cody@tmvsocial.com>
2024-07-25 08:08:52 -0500

    Pre-update commit

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -16,20 +16,13 @@
     # Whether to enable power management thorugh systemd.
     # https://search.nixos.org/options?channel=24.05&show=hardware.nvidia.powerManagement.enable
     powerManagement.enable = true;
-    forceFullCompositionPipeline = false;
 
     # Fine-grained power management. Turns off GPU when not in use.
     # Experimental and only works on modern Nvidia GPUs (Turing or newer).
     powerManagement.finegrained = false;
 
-    # Use the NVidia open source kernel module (not to be confused with the
-    # independent third-party "nouveau" open source driver).
-    # Support is limited to the Turing and later architectures. Full list of 
-    # supported GPUs is at: 
     # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
-    # Only available from driver 515.43.04+
-    # Currently alpha-quality/buggy, so false is currently the recommended setting.
-    open = false;
+    open = true;
 
     # Enable the Nvidia settings menu,
     # accessible via `nvidia-settings`.
