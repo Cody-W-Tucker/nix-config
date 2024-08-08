b03256c69cdb4db49fe258b582ce6c4f4539d472
Cody W Tucker <cody@tmvsocial.com>
2024-08-08 17:08:29 -0500

    Containerized open webui (#21)
    * chore: Update ai.nix with open-webui container configuration
    
    * chore: Update ai.nix with open-webui container configuration
    
    * chore: Update ai.nix with open-webui container configuration
    
    * chore: Update ai.nix with open-webui container configuration
    
    * chore: Update ai.nix with open-webui container configuration
    
    * test: Did they fix the service yet?
    
    * Answer: No they didn't, lets just use the right envs
    
    * chore: Fix proxy setting for https
    
    * chore: Update ai.nix with corrected port mapping for open-webui container
    
    * chore: Remove unused import of arm.nix in default.nix
    
    * chore: Update ai.nix with corrected port mapping for open-webui container
    
    * chore: Update ai.nix with corrected port mapping for open-webui container
    
    * chore: Update ai.nix with additional environment variables for open-webui container
    
    * chore: Update ai.nix with corrected port mapping and additional volumes for open-webui container
    
    * chore: Update ai.nix with corrected port mapping and remove unused volumes for open-webui container
    
    * chore: Update ai.nix with corrected port mapping and remove unused volumes for open-webui container
    
    * chore: Update ai.nix with corrected port mapping and remove unused volumes for open-webui container
    
    * chore: Update ai.nix with corrected port mapping and additional volumes for open-webui container
    
    * chore: Update ai.nix with corrected port mapping and additional volumes for open-webui container
    
    * chore: Update ai.nix with corrected port mapping and file system name for RAG-Docs
    
    * chore: Update ai.nix with corrected file system path for RAG-Docs

--- modules/server/ai.nix
+++ modules/server/ai.nix
@@ -1,6 +1,35 @@
-{ config, pkgs, ... }:
-
+{ config, ... }:
+let userDir = "${config.users.users.codyt.home}";
+in
 {
+  # docker run --name open-webui --network=host -e PORT=11435 -e OLLAMA_BASE_URL=http://192.168.254.25:11434 -v ~/open-webui:/app/backend/data ghcr.io/open-webui/open-webui:main
+  virtualisation.oci-containers.containers."open-webui" = {
+    autoStart = true;
+    image = "ghcr.io/open-webui/open-webui:main";
+    ports = [ "3000:8080" ];
+    volumes = [ "${userDir}/open-webui:/app/backend/data" "${userDir}/RAG-Docs:${userDir}/RAG-Docs" ];
+    extraOptions = [ "--network=host" ];
+    environment = {
+      OLLAMA_BASE_URL = "http://192.168.254.25:11434";
+      # Disable authentication
+      WEBUI_AUTH = "False";
+      ANONYMIZED_TELEMETRY = "False";
+      DO_NOT_TRACK = "True";
+      SCARF_NO_ANALYTICS = "True";
+      USER_AGENT = "Ollama";
+      AIOHTTP_CLIENT_TIMEOUT = "600";
+      ENABLE_RAG_WEB_SEARCH = "True";
+      RAG_WEB_SEARCH_ENGINE = "searxng";
+      SEARXNG_QUERY_URL = "https://search.homehub.tv/search?q=<query>";
+      ENABLE_RAG_HYBRID_SEARCH = "True";
+      DOCS_DIR = "${userDir}/RAG-Docs";
+    };
+  };
+  fileSystems."/home/codyt/RAG-Docs" = {
+    device = "/mnt/hdd/Share/Documents/RAG-Docs";
+    fsType = "none";
+    options = [ "bind" "rw" ];
+  };
   # Ollama local llm
   services = {
     nginx.virtualHosts = {
@@ -8,7 +37,7 @@
         useACMEHost = "homehub.tv";
         forceSSL = true;
         locations."/" = {
-          proxyPass = "http://search.homehub.tv:8888";
+          proxyPass = "http://127.0.0.1:8888";
           proxyWebsockets = true;
         };
       };
@@ -16,7 +45,7 @@
         useACMEHost = "homehub.tv";
         forceSSL = true;
         locations."/" = {
-          proxyPass = "http://127.0.0.1:11435";
+          proxyPass = "http://127.0.0.1:8080";
           proxyWebsockets = true;
         };
       };
@@ -27,24 +56,24 @@
       openFirewall = true;
       host = "0.0.0.0";
     };
-    open-webui = {
-      enable = true;
-      port = 11435;
-      host = "127.0.0.1";
-      openFirewall = false;
-      environment = {
-        OLLAMA_API_BASE_URL = "http://192.168.254.25:11434";
-        # Disable authentication
-        WEBUI_AUTH = "False";
-        ANONYMIZED_TELEMETRY = "False";
-        DO_NOT_TRACK = "True";
-        SCARF_NO_ANALYTICS = "True";
-        SEARXNG_QUERY_URL = "https://search.homehub.tv/search?q=<query>";
-        USER_AGENT = "Ollama";
-        ENABLE_RAG_WEB_LOADER_SSL_VERIFICATION = "False";
-        AIOHTTP_CLIENT_TIMEOUT = "600";
-      };
-    };
+    # open-webui = {
+    #   enable = true;
+    #   port = 11435;
+    #   host = "127.0.0.1";
+    #   openFirewall = false;
+    #   environment = {
+    #     OLLAMA_API_BASE_URL = "http://192.168.254.25:11434";
+    #     # Disable authentication
+    #     WEBUI_AUTH = "False";
+    #     ANONYMIZED_TELEMETRY = "False";
+    #     DO_NOT_TRACK = "True";
+    #     SCARF_NO_ANALYTICS = "True";
+    #     SEARXNG_QUERY_URL = "https://search.homehub.tv/search?q=<query>";
+    #     USER_AGENT = "Ollama";
+    #     ENABLE_RAG_WEB_LOADER_SSL_VERIFICATION = "False";
+    #     AIOHTTP_CLIENT_TIMEOUT = "600";
+    #   };
+    # };
     searx = {
       enable = true;
       redisCreateLocally = true;
@@ -60,7 +89,7 @@
           bind_address = "0.0.0.0";
           secret_key = "secret";
           limiter = false;
-          image_proxy = true;
+          image_proxy = false;
         };
         ui = {
           static_use_hash = true;
