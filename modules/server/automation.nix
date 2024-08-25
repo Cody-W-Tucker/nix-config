6cbfc561211c4e77992b2525d44a362e344e105f
Cody W Tucker <cody@tmvsocial.com>
2024-08-25 15:27:35 -0500

    chore: Update server automation and nginx configuration

--- modules/server/automation.nix
+++ modules/server/automation.nix
@@ -3,5 +3,54 @@
   services.n8n = {
     enable = true;
     openFirewall = true;
+    webhookUrl = "https://automation.homehub.tv";
+    settings = {
+      generic = {
+        timezone = config.time.timeZone;
+      };
+      endpoints = {
+        metrics = {
+          enable = true;
+        };
+      };
+    };
+  };
+
+  virtualHosts."automation.homehub.tv" = {
+    forceSSL = true;
+    useACMEHost = "homehub.tv";
+    locations."~ ^/(webhook|webhook-test)" = {
+      proxyPass = "http://127.0.0.1:${toString config.services.n8n.settings.port}";
+
+      extraConfig = ''
+        chunked_transfer_encoding off;
+        proxy_buffering off;
+        proxy_cache off;
+      '';
+    };
+
+    locations."~ ^/rest/oauth2-credential/callback" = {
+      proxyPass = "http://127.0.0.1:${toString config.services.n8n.settings.port}";
+
+      extraConfig = ''
+        chunked_transfer_encoding off;
+        proxy_buffering off;
+        proxy_cache off;
+      '';
+    };
+
+    locations."/" = {
+      proxyPass = "http://127.0.0.1:${toString config.services.n8n.settings.port}";
+      proxyWebsockets = true;
+
+      extraConfig = ''
+        chunked_transfer_encoding off;
+        proxy_buffering off;
+        proxy_cache off;
+
+        allow 192.168.254.0/24;
+        deny all;
+      '';
+    };
   };
 }
