5994cb5a9190149af07726619aa4bc4ed58895c5
Cody W Tucker <cody@tmvsocial.com>
2024-08-24 21:45:51 -0500

    chore: Update server automation and nginx configuration

--- modules/server/automation.nix
+++ modules/server/automation.nix
@@ -3,5 +3,27 @@
   services.n8n = {
     enable = true;
     openFirewall = true;
+    webhookUrl = "https://command.homehub.tv";
+    settings = {
+      # The top level domain to serve from
+      DOMAIN_NAME = "homehub.tv";
+
+      # The subdomain to serve from
+      SUBDOMAIN = "command";
+
+      # Optional timezone to set which gets used by Cron-Node by default
+      # If not set New York time will be used
+      GENERIC_TIMEZONE = "America/Chicago";
+    };
+  };
+
+  # Nginx reverse proxy for command
+  services.nginx.virtualHosts."command.homehub.tv" = {
+    forceSSL = true;
+    useACMEHost = "homehub.tv";
+    locations."/" = {
+      proxyPass = "http://localhost:5678";
+      proxyWebsockets = true;
+    };
   };
 }
