c6141ac35adea8e1a9c9e8e34c7f19b3e87472bb
Cody W Tucker <cody@tmvsocial.com>
2024-08-27 15:16:06 -0500

    Pre-update commit

--- home/default.nix
+++ home/default.nix
@@ -10,9 +10,34 @@
     ./xdg.nix
   ];
 
+  home.packages = with pkgs; [
+    # Add some packages to the user environment.
+    grim
+    slurp
+    wl-clipboard
+    hyprpicker
+    mako
+    swww
+    rofi-wayland
+    vscode
+    zotero
+    gh
+    ripdrag
+    spotify
+    tectonic
+  ];
+
   # Clipboard history
   services.cliphist.enable = true;
 
   # Document handling
-  programs.pandoc.enable = true;
+  programs.pandoc = {
+    enable = true;
+    defaults = {
+      metadata = {
+        author = "Cody W Tucker";
+      };
+      pdf-engine = "tectonic";
+    };
+  };
 }
