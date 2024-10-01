46a39c6d1e869f91a710aedf76ee24a3576420e4
Cody W Tucker <cody@tmvsocial.com>
2024-10-01 16:15:37 -0500

    Add monitoring services for Grafana and Prometheus

---
+++ modules/server/monitoring.nix
@@ -0,0 +1,32 @@
+{ config, pkgs, ... }:
+{
+  services = {
+    grafana = {
+      enable = true;
+      settings = {
+        server = {
+          # Listening Address
+          http_addr = "127.0.0.1";
+          # and Port
+          http_port = 3001;
+          # Grafana needs to know on which domain and URL it's running
+          domain = "monitoring.homehub.tv";
+          serve_from_sub_path = true;
+        };
+      };
+    };
+    nginx.virtualHosts."monitoring.homehub.tv" = {
+      useACMEHost = "homehub.tv";
+      forceSSL = true;
+      locations."/" = {
+        proxyPass = "http://127.0.0.1:${toString config.services.grafana.settings.server.http_port}";
+        proxyWebsockets = true;
+        recommendedProxySettings = true;
+      };
+    };
+    prometheus = {
+      enable = true;
+      port = 9001;
+    };
+  };
+}
