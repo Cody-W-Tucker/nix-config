e41a509d6c173155f0c3de965078b1124056d864
Cody W Tucker <cody@tmvsocial.com>
2024-09-30 21:39:07 -0500

    Update monitor configurations in waybar.nix and hyprland.nix

--- users/cody/waybar.nix
+++ users/cody/waybar.nix
@@ -124,8 +124,8 @@ in
     };
     settings = {
       # Duplicate the bars for each monitor
-      monitor1 = createBar waybarConfig "DP-1" "bottom";
-      monitor2 = createBar waybarConfig "DP-2" "top";
+      monitor1 = createBar waybarConfig "DP-1" "top";
+      monitor2 = createBar waybarConfig "DP-2" "bottom";
     };
     style = lib.mkForce ''
       * {
