b34a9ab45151ccf68a01dea833457a76b7ba22de
Cody W Tucker <cody@tmvsocial.com>
2024-10-02 09:55:05 -0500

    Update monitoring.nix: Disable analytics reporting and adjust Prometheus and Loki configurations

--- modules/server/monitoring.nix
+++ modules/server/monitoring.nix
@@ -72,7 +72,7 @@
           max_chunk_age = "1h";
           chunk_target_size = 999999;
           chunk_retain_period = "30s";
-          max_transfer_retries = 0;
+          # max_transfer_retries = 0;
         };
 
         schema_config = {
@@ -93,7 +93,7 @@
             active_index_directory = "/var/lib/loki/boltdb-shipper-active";
             cache_location = "/var/lib/loki/boltdb-shipper-cache";
             cache_ttl = "24h";
-            shared_store = "filesystem";
+            # shared_store = "filesystem";
           };
 
           filesystem = {
@@ -106,9 +106,9 @@
           reject_old_samples_max_age = "168h";
         };
 
-        chunk_store_config = {
-          max_look_back_period = "0s";
-        };
+        # chunk_store_config = {
+        #   max_look_back_period = "0s";
+        # };
 
         table_manager = {
           retention_deletes_enabled = false;
@@ -117,7 +117,7 @@
 
         compactor = {
           working_directory = "/var/lib/loki";
-          shared_store = "filesystem";
+          # shared_store = "filesystem";
           compactor_ring = {
             kvstore = {
               store = "inmemory";
