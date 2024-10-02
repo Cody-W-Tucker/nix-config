65b703c315383a9fb243273d4eb78fda3aec2f61
Cody W Tucker <cody@tmvsocial.com>
2024-10-02 10:02:24 -0500

    Update monitoring.nix: Disable analytics reporting, adjust Prometheus and Loki configurations, and fix typo in analytics reporting variable name

--- modules/server/monitoring.nix
+++ modules/server/monitoring.nix
@@ -7,11 +7,8 @@
       settings = {
         analytics.reporting_enabled = false;
         server = {
-          # Listening Address
           http_addr = "127.0.0.1";
-          # and Port
           http_port = 3001;
-          # Grafana needs to know on which domain and URL it's running
           domain = "monitoring.homehub.tv";
         };
       };
@@ -72,7 +69,6 @@
           max_chunk_age = "1h";
           chunk_target_size = 999999;
           chunk_retain_period = "30s";
-          # max_transfer_retries = 0;
         };
 
         schema_config = {
@@ -92,7 +88,6 @@
           tsdb = {
             dir = "/var/lib/loki/tsdb";
           };
-
           filesystem = {
             directory = "/var/lib/loki/chunks";
           };
@@ -101,11 +96,8 @@
         limits_config = {
           reject_old_samples = true;
           reject_old_samples_max_age = "168h";
+          allow_structured_metadata = false;
         };
-
-        # chunk_store_config = {
-        #   max_look_back_period = "0s";
-        # };
 
         table_manager = {
           retention_deletes_enabled = false;
@@ -114,7 +106,6 @@
 
         compactor = {
           working_directory = "/var/lib/loki";
-          # shared_store = "filesystem";
           compactor_ring = {
             kvstore = {
               store = "inmemory";
@@ -122,7 +113,6 @@
           };
         };
       };
-      # user, group, dataDir, extraFlags, (configFile)
     };
     promtail = {
       enable = true;
@@ -152,7 +142,6 @@
           }];
         }];
       };
-      # extraFlags
     };
     nginx.virtualHosts."monitoring.homehub.tv" = {
       useACMEHost = "homehub.tv";
