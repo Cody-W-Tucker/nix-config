8189bb5fae92bc73c9ed3884a1c6631d1a552d8d
Cody W Tucker <cody@tmvsocial.com>
2024-10-02 10:08:37 -0500

    Update monitoring.nix: Disable analytics reporting, adjust Prometheus and Loki configurations, and add Grafana listening address and port

--- modules/server/monitoring.nix
+++ modules/server/monitoring.nix
@@ -7,8 +7,11 @@
       settings = {
         analytics.reporting_enabled = false;
         server = {
+          # Listening Address
           http_addr = "127.0.0.1";
+          # and Port
           http_port = 3001;
+          # Grafana needs to know on which domain and URL it's running
           domain = "monitoring.homehub.tv";
         };
       };
@@ -51,97 +54,12 @@
     };
     loki = {
       enable = true;
-      configuration = {
-        server.http_listen_port = 3030;
-        auth_enabled = false;
+      # Add config later
 
-        ingester = {
-          lifecycler = {
-            address = "127.0.0.1";
-            ring = {
-              kvstore = {
-                store = "inmemory";
-              };
-              replication_factor = 1;
-            };
-          };
-          chunk_idle_period = "1h";
-          max_chunk_age = "1h";
-          chunk_target_size = 999999;
-          chunk_retain_period = "30s";
-        };
-
-        schema_config = {
-          configs = [{
-            from = "2024-01-01";
-            store = "tsdb";
-            object_store = "filesystem";
-            schema = "v13";
-            index = {
-              prefix = "index_";
-              period = "24h";
-            };
-          }];
-        };
-
-        storage_config = {
-          tsdb = {
-            dir = "/var/lib/loki/tsdb";
-          };
-          filesystem = {
-            directory = "/var/lib/loki/chunks";
-          };
-        };
-
-        limits_config = {
-          reject_old_samples = true;
-          reject_old_samples_max_age = "168h";
-          allow_structured_metadata = false;
-        };
-
-        table_manager = {
-          retention_deletes_enabled = false;
-          retention_period = "0s";
-        };
-
-        compactor = {
-          working_directory = "/var/lib/loki";
-          compactor_ring = {
-            kvstore = {
-              store = "inmemory";
-            };
-          };
-        };
-      };
     };
     promtail = {
       enable = true;
-      configuration = {
-        server = {
-          http_listen_port = 3031;
-          grpc_listen_port = 0;
-        };
-        positions = {
-          filename = "/tmp/positions.yaml";
-        };
-        clients = [{
-          url = "http://127.0.0.1:${toString config.services.loki.configuration.server.http_listen_port}/loki/api/v1/push";
-        }];
-        scrape_configs = [{
-          job_name = "journal";
-          journal = {
-            max_age = "12h";
-            labels = {
-              job = "systemd-journal";
-              host = "pihole";
-            };
-          };
-          relabel_configs = [{
-            source_labels = [ "__journal__systemd_unit" ];
-            target_label = "unit";
-          }];
-        }];
-      };
+      # Add config later
     };
     nginx.virtualHosts."monitoring.homehub.tv" = {
       useACMEHost = "homehub.tv";
