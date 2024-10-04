9087fa727b7400535c80c86fddbb6739a278e81a
Cody W Tucker <cody@tmvsocial.com>
2024-10-04 10:56:38 -0500

    Adjust extraOptions in ai.nix: Remove unnecessary proxy headers

--- modules/server/ai.nix
+++ modules/server/ai.nix
@@ -65,12 +65,6 @@ in
         locations."/" = {
           proxyPass = "http://127.0.0.1:3030";
           proxyWebsockets = true;
-          extraConfig = ''
-            proxy_set_header Host $host;
-            proxy_set_header X-Real-IP $remote_addr;
-            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-            proxy_set_header X-Forwarded-Proto $scheme;
-          '';
         };
       };
     };
