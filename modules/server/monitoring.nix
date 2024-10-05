16d0b584e71acd458085dbf474dae993a0bceabf
Cody W Tucker <cody@tmvsocial.com>
2024-10-05 18:05:24 -0500

    Adjust extraOptions in ai.nix: Update loki and promtail configurations

--- modules/server/monitoring.nix
+++ modules/server/monitoring.nix
@@ -54,10 +54,9 @@
     };
     loki = {
       enable = true;
-      # Add config later
       configuration = {
-        server.http_listen_port = 3030;
         auth_enabled = false;
+        server.http_listen_port = 3090;
 
         ingester = {
           lifecycler = {
@@ -73,7 +72,6 @@
           max_chunk_age = "1h";
           chunk_target_size = 999999;
           chunk_retain_period = "30s";
-          max_transfer_retries = 0;
         };
 
         schema_config = {
@@ -94,7 +92,6 @@
             active_index_directory = "/var/lib/loki/boltdb-shipper-active";
             cache_location = "/var/lib/loki/boltdb-shipper-cache";
             cache_ttl = "24h";
-            shared_store = "filesystem";
           };
 
           filesystem = {
@@ -107,10 +104,6 @@
           reject_old_samples_max_age = "168h";
         };
 
-        chunk_store_config = {
-          max_look_back_period = "0s";
-        };
-
         table_manager = {
           retention_deletes_enabled = false;
           retention_period = "0s";
@@ -118,7 +111,6 @@
 
         compactor = {
           working_directory = "/var/lib/loki";
-          shared_store = "filesystem";
           compactor_ring = {
             kvstore = {
               store = "inmemory";
@@ -126,40 +118,6 @@
           };
         };
       };
-      # user, group, dataDir, extraFlags, (configFile)
-    };
-
-    # promtail: port 3031 (8031)
-    #
-    promtail = {
-      enable = true;
-      configuration = {
-        server = {
-          http_listen_port = 3031;
-          grpc_listen_port = 0;
-        };
-        positions = {
-          filename = "/tmp/positions.yaml";
-        };
-        clients = [{
-          url = "http://127.0.0.1:${toString config.services.loki.configuration.server.http_listen_port}/loki/api/v1/push";
-        }];
-        scrape_configs = [{
-          job_name = "journal";
-          journal = {
-            max_age = "12h";
-            labels = {
-              job = "systemd-journal";
-              host = "pihole";
-            };
-          };
-          relabel_configs = [{
-            source_labels = [ "__journal__systemd_unit" ];
-            target_label = "unit";
-          }];
-        }];
-      };
-      # extraFlags
     };
 
     nginx.virtualHosts."monitoring.homehub.tv" = {
