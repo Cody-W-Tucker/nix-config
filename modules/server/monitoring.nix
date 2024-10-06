cdd061e514be5e2fed16bd48b599dcbd88b91c1a
Cody W Tucker <cody@tmvsocial.com>
2024-10-06 13:31:18 -0500

    Refactor monitoring configuration in ai.nix: Update promtail server http listen port and add scrape_configs for journal

--- modules/server/monitoring.nix
+++ modules/server/monitoring.nix
@@ -119,6 +119,37 @@
       };
     };
 
+    promtail = {
+      enable = true;
+      configuration = {
+        server = {
+          http_listen_port = 3091;
+          grpc_listen_port = 0;
+        };
+        positions = {
+          filename = "/tmp/positions.yaml";
+        };
+        clients = [{
+          url = "http://127.0.0.1:${toString config.services.loki.configuration.server.http_listen_port}/loki/api/v1/push";
+        }];
+        scrape_configs = [{
+          job_name = "journal";
+          journal = {
+            max_age = "12h";
+            labels = {
+              job = "systemd-journal";
+              host = "pihole";
+            };
+          };
+          relabel_configs = [{
+            source_labels = [ "__journal__systemd_unit" ];
+            target_label = "unit";
+          }];
+        }];
+      };
+      # extraFlags
+    };
+
     nginx.virtualHosts."monitoring.homehub.tv" = {
       useACMEHost = "homehub.tv";
       forceSSL = true;
