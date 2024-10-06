75b80028965ebef7c97045c3dfed1a7ef315a41f
Cody W Tucker <cody@tmvsocial.com>
2024-10-06 13:36:06 -0500

    Refactor monitoring configuration in ai.nix: Update server http listen port and adjust log level

--- modules/server/monitoring.nix
+++ modules/server/monitoring.nix
@@ -58,7 +58,7 @@
         auth_enabled = false;
         server = {
           http_listen_port = 3090;
-          log_level = "info"; # Temporarily increased for troubleshooting
+          log_level = "warn"; # Temporarily increased for troubleshooting
         };
 
         common = {
