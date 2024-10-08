41ec74471e95670db4f4b994814add39bdb0e273
Cody W Tucker <cody@tmvsocial.com>
2024-10-07 19:35:41 -0500

    Refactor monitoring configuration in default.nix: Update gzip buffer sizes for improved compression

--- modules/server/default.nix
+++ modules/server/default.nix
@@ -54,7 +54,7 @@
       gzip_min_length 256;
       gzip_proxied expired no-cache no-store private auth;
       gzip_types text/plain text/css application/json application/javascript text/xml application/xml application/xml+rss text/javascript;
-      gzip_buffers 64 8k;
+      gzip_buffers 128 8k;
     '';
   };
 
