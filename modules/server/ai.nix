d14ce7c1200e90592b14f67ae207739bc10a4c1e
Cody W Tucker <cody@tmvsocial.com>
2024-10-14 16:15:12 -0500

    Refactor server configuration: Update proxy_set_header in ai.nix

--- modules/server/ai.nix
+++ modules/server/ai.nix
@@ -63,7 +63,7 @@ in
           proxyWebsockets = true;
           extraConfig = ''
             proxy_http_version 1.1;
-            proxy_set_header Host $http_host;
+            proxy_set_header Host $host;
             proxy_set_header X-Forwarded-For $remote_addr;
           '';
         };
