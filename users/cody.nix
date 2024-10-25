6664062419a402f50cb784b0a7c559518a3653ae
Cody W Tucker <cody@tmvsocial.com>
2024-10-25 11:27:09 -0500

    Pre-update commit

--- users/cody.nix
+++ users/cody.nix
@@ -92,21 +92,6 @@
     };
   };
 
-  # Theme GTK
-  gtk = {
-    enable = true;
-    iconTheme = {
-      package = pkgs.adwaita-icon-theme;
-      name = "Adwaita";
-    };
-    gtk3.extraConfig = {
-      gtk-application-prefer-dark-theme = 1;
-    };
-    gtk4.extraConfig = {
-      gtk-application-prefer-dark-theme = 1;
-    };
-  };
-
   # Keyboard
   home.keyboard = {
     layout = "us";
