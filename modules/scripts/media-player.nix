4743fb404cbeb638843e42103a154ec21482b564
Cody W Tucker <cody@tmvsocial.com>
2024-10-26 20:19:22 -0500

    Pre-update commit

--- modules/scripts/media-player.nix
+++ modules/scripts/media-player.nix
@@ -29,5 +29,5 @@ pkgs.writeShellScriptBin "media-player" ''
   escaped_title=$(echo "$title" | sed 's/"/\\"/g')
 
   # Output the JSON
-  echo "{\"text\": \"$icon\" \"$escaped_title\", \"class\": \"custom-media\"}"
+  echo "{\"text\": \"$icon $escaped_title\", \"class\": \"custom-media\"}"
 ''
