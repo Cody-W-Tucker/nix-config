1d35da2898b1ab5bf99e0cf827697f81592e7100
Cody W Tucker <cody@tmvsocial.com>
2024-10-29 13:22:17 -0500

    Pre-update commit

--- users/cody/waybar.nix
+++ users/cody/waybar.nix
@@ -7,7 +7,7 @@ let
     layer = "top";
     reload_style_on_change = true;
     spacing = 4;
-    modules-center = [ "custom/notification" "clock" "custom/media" ];
+    modules-center = [ "custom/media" "clock" "custom/notification" ];
     modules-left = [ "hyprland/workspaces" ];
     modules-right = [
       "privacy"
