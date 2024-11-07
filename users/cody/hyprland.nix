7a25efc891c135f59de1aa47577c5588771c86ca
Cody W Tucker <cody@tmvsocial.com>
2024-11-07 15:03:19 -0600

    Pre-update commit

--- users/cody/hyprland.nix
+++ users/cody/hyprland.nix
@@ -66,10 +66,12 @@
           new_optimizations = "true";
           ignore_opacity = true;
         };
-        drop_shadow = "yes";
-        shadow_range = "4";
-        shadow_render_power = "3";
-        "col.shadow" = lib.mkForce "rgba(1a1a1aee)";
+        shadow = {
+          enabled = "true";
+          range = "4";
+          render_power = "3";
+          color = lib.mkForce "rgba(1a1a1aee)";
+        };
       };
       dwindle = {
         pseudotile = "yes";
