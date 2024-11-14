b8c8cd2e8144bcf9503bd40bb582243083a3f558
Cody W Tucker <cody@tmvsocial.com>
2024-11-14 15:40:14 -0600

    add BASE_URL and createDatabaseLocally options to Miniflux configuration

--- modules/server/content.nix
+++ modules/server/content.nix
@@ -2,10 +2,12 @@
   # Rss feed
   services.miniflux = {
     enable = true;
+    createDatabaseLocally = false;
     config = {
       CLEANUP_FREQUENCY = 48;
       LISTEN_ADDR = "localhost:7777";
       CREATE_ADMIN = 0;
+      BASE_URL = "https://rss.homehub.tv";
     };
   };
 
