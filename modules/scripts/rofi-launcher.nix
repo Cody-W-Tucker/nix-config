20d52247104d708c002c9890943134d9ed470317
Cody W Tucker <cody@tmvsocial.com>
2024-11-14 13:11:45 -0600

    Pre-update commit

--- modules/scripts/rofi-launcher.nix
+++ modules/scripts/rofi-launcher.nix
@@ -6,5 +6,37 @@ pkgs.writeShellScriptBin "rofi-launcher" ''
     pkill -x rofi
     exit 0
   fi
-  rofi -show drun -show-icons
+
+  # Options for rofi menu
+  options="Run Program\nWeb Scraper"
+
+  # Capture the selected option in a variable
+  selected=$(echo -e "$options" | rofi -dmenu -p "Select:")
+
+  case "$selected" in
+    "Run Program")
+      rofi -show drun -show-icons
+      ;;
+    "Web Scraper")
+      url=$(rofi -dmenu -p "Enter URL:")
+      
+      # Check if URL is empty
+      if [ -z "$url" ]; then
+        notify-send "No URL entered. Web scraper not executed."
+        exit 1
+      fi
+
+      # Now run the web-scraper with the provided URL
+      WEBSCRAPER_PATH="$(command -v web-scraper)"
+      if [ -z "$WEBSCRAPER_PATH" ]; then
+        notify-send "Web scraper not found in PATH!"
+        exit 1
+      fi
+
+      $WEBSCRAPER_PATH "$url" && notify-send "Web scraper executed" "File saved successfully!"
+      ;;
+    *)
+      notify-send "No option selected."
+      ;;
+  esac
 ''
