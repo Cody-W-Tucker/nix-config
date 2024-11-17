b7403a580ffdfa22a80fb2126ee3a54101aafdb6
Cody W Tucker <cody@tmvsocial.com>
2024-11-16 20:56:09 -0600

    Pre-update commit

--- users/cody/waybar.nix
+++ users/cody/waybar.nix
@@ -38,8 +38,8 @@ let
       on-click = "waybar-timer new 25 'notify-send \"Session finished\"'";
       on-click-middle = "waybar-timer cancel";
       on-click-right = "waybar-timer togglepause";
-      on-scroll-up = "waybar-timer increase 300 || waybar-timer new 1 'notify-send -u critical \"Timer expired.\"'";
-      on-scroll-down = "waybar-timer increase -300";
+      on-scroll-up = "waybar-timer increase 300 || waybar-timer new 1";
+      on-scroll-down = "waybar-timer increase -300 || 'notify-send -u critical \"Timer expired.\"'";
     };
     "custom/media" = {
       format = "{}";
