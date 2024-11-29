2db26e2ed9943152cf94543dea97e0a1c911d6cc
Cody W Tucker <cody@tmvsocial.com>
2024-11-29 13:58:47 -0600

    Pre-update commit

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,13 +1,5 @@
-{
-  systems = [ "x86_64-linux" ];
-
-  perSystem =
-    { pkgs
-    , inputs'
-    , ...
-    }: {
-      packages = {
-        bibata-hyprcursor = pkgs.callPackage ./bibata-hyprcursor { };
-      };
-    };
+{ pkgs }: {
+  packages = {
+    bibata-hyprcursor = pkgs.callPackage ./bibata-hyprcursor { };
+  };
 }
