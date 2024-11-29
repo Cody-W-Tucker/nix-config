e15c856505362b42f3382a2c34caf17519e8a491
Cody W Tucker <cody@tmvsocial.com>
2024-11-29 14:33:04 -0600

    Pre-update commit

--- users/cody.nix
+++ users/cody.nix
@@ -92,37 +92,12 @@
     };
   };
 
-  # cursor = {
-  #   package = pkgs.bibata-cursors;
-  #   name = "Bibata-Modern-Classic";
-  #   size = 24;
-  # };
-
-  home.pointerCursor = lib.mkForce {
-    gtk.enable = true;
-    # x11.enable = true;
-    package = pkgs.bibata-cursors;
-    name = "Bibata-Modern-Classic";
-    size = 24;
-  };
-
   gtk = {
     enable = true;
-
-    # theme = {
-    #   package = pkgs.flat-remix-gtk;
-    #   name = "Flat-Remix-GTK-Grey-Darkest";
-    # };
-
     iconTheme = {
       package = pkgs.adwaita-icon-theme;
       name = "Adwaita";
     };
-
-    # font = {
-    #   name = "Sans";
-    #   size = 11;
-    # };
   };
 
   # Keyboard
