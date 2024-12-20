d4d4e12967c65a8ccb2473bfa0cb67c94f3db008
Cody W Tucker <cody@tmvsocial.com>
2024-12-20 13:13:32 -0600

    Pre-update commit

--- users/cody/hyprland/keybinds.nix
+++ users/cody/hyprland/keybinds.nix
@@ -1,60 +1,86 @@
 { config, ... }:
-{
-  # Keybindings
-  wayland.windowManager.hyprland.settings = {
-    "$mainMod" = "SUPER";
-    bindm =
-      [
-        # Move/resize windows with mainMod + LMB/RMB and dragging
-        "$mainMod, mouse:272, movewindow"
-        "$mainMod, mouse:273, resizewindow"
-      ];
-    bind =
-      [
-        "$mainMod, V, exec, cliphist list | rofi -dmenu | cliphist decode | wl-copy"
-        "$mainMod, RETURN, exec, pkill waybar && waybar &"
-        "$mainMod, Q, exec, kitty"
-        "$mainMod, E, exec, kitty -- ranger"
-        "$mainMod, Tab, exec, rofi-launcher"
-        "$mainMod SHIFT, E, exec, nautilus"
-        "$mainMod SHIFT, Tab, exec, rofi -show web_scraper -modi 'web_scraper:web-scraper'"
-        "$mainMod, KP_Insert, exec, google-chrome-stable --app=https://ai.homehub.tv"
-        "$mainMod, KP_Add, exec, hyprctl dispatch exec [floating] gnome-calculator"
-        "$mainMod, KP_End, exec, google-chrome-stable --app=https://mail.google.com"
-        "$mainMod, KP_Down, exec, google-chrome-stable --app=https://messages.google.com/web/u/0/conversations"
-        "$mainMod, KP_Next, exec, google-chrome-stable --app=https://calendar.google.com/calendar/u/0/r"
-        "$mainMod, KP_Home, exec, code"
-        # Screenshots
-        ''$mainMod, S, exec, grim -g "$(slurp)" "$HOME/Pictures/Screenshots/$(date '+%y%m%d_%H-%M-%S').png"''
-        ''$mainMod SHIFT, S, exec, grim -g "$(slurp)" - | wl-copy''
-        # Hyprpicker color picker
-        "$mainMod, mouse:274, exec, hyprpicker -a"
-        # Switching workspaces
-        "$mainMod, mouse_down, exec, hyprnome --previous"
-        "$mainMod, mouse_up, exec, hyprnome"
-        # Moving windows to workspaces
-        "$mainMod SHIFT, mouse_down, exec, hyprnome --previous --move"
-        "$mainMod SHIFT, mouse_up, exec, hyprnome --move"
+let
+  mainMod = "SUPER";
 
-        # Unlogged keybinds
-        "$mainMod, SPACE, togglefloating"
-        "$mainMod, C, killactive"
-        "$mainMod, F, fullscreen"
-        "$mainMod SHIFT, P, fullscreenstate"
-        "$mainMod, P, pin"
-        # Move focus with mainMod + arrow keys
-        "$mainMod, left, movefocus, l"
-        "$mainMod, right, movefocus, r"
-        "$mainMod, up, movefocus, u"
-        "$mainMod, down, movefocus, d"
-        # Move windows with mainMod + shift + arrow keys
-        "$mainMod SHIFT, left, movewindow, l"
-        "$mainMod SHIFT, right, movewindow, r"
-        "$mainMod SHIFT, up, movewindow, u"
-        "$mainMod SHIFT, down, movewindow, d"
-        # Special workspace (scratchpad)
-        "$mainMod, A, togglespecialworkspace, magic"
-        "$mainMod SHIFT, A, movetoworkspacesilent, special:magic"
-      ];
+  mousebinds = [
+    # Move/resize windows with mainMod + LMB/RMB and dragging
+    "${mainMod}, mouse:272, movewindow"
+    "${mainMod}, mouse:273, resizewindow"
+  ];
+
+  keybinds = [
+    # Application launchers
+    "${mainMod}, V, exec, cliphist list | rofi -dmenu | cliphist decode | wl-copy"
+    "${mainMod}, V, exec, keybind-logger 'cliphist list | rofi -dmenu | cliphist decode | wl-copy'"
+    "${mainMod}, RETURN, exec, pkill waybar && waybar &"
+    "${mainMod}, RETURN, exec, keybind-logger 'pkill waybar && waybar &'"
+    "${mainMod}, Q, exec, kitty"
+    "${mainMod}, Q, exec, keybind-logger 'kitty'"
+    "${mainMod}, E, exec, kitty -- ranger"
+    "${mainMod}, E, exec, keybind-logger 'kitty -- ranger'"
+    "${mainMod}, Tab, exec, rofi-launcher"
+    "${mainMod}, Tab, exec, keybind-logger 'rofi-launcher'"
+    "${mainMod} SHIFT, E, exec, nautilus"
+    "${mainMod} SHIFT, E, exec, keybind-logger 'nautilus'"
+    "${mainMod} SHIFT, Tab, exec, rofi -show web_scraper -modi 'web_scraper:web-scraper'"
+    "${mainMod} SHIFT, Tab, exec, keybind-logger 'rofi -show web_scraper -modi 'web_scraper:web-scraper''"
+
+    # Quick launch apps
+    "${mainMod}, KP_Insert, exec, google-chrome-stable --app=https://ai.homehub.tv"
+    "${mainMod}, KP_Insert, exec, keybind-logger 'google-chrome-stable --app=https://ai.homehub.tv'"
+    "${mainMod}, KP_Add, exec, hyprctl dispatch exec [floating] gnome-calculator"
+    "${mainMod}, KP_Add, exec, keybind-logger 'hyprctl dispatch exec [floating] gnome-calculator'"
+    "${mainMod}, KP_End, exec, google-chrome-stable --app=https://mail.google.com"
+    "${mainMod}, KP_End, exec, keybind-logger 'google-chrome-stable --app=https://mail.google.com'"
+    "${mainMod}, KP_Down, exec, google-chrome-stable --app=https://messages.google.com/web/u/0/conversations"
+    "${mainMod}, KP_Down, exec, keybind-logger 'google-chrome-stable --app=https://messages.google.com/web/u/0/conversations'"
+    "${mainMod}, KP_Next, exec, google-chrome-stable --app=https://calendar.google.com/calendar/u/0/r"
+    "${mainMod}, KP_Next, exec, keybind-logger 'google-chrome-stable --app=https://calendar.google.com/calendar/u/0/r'"
+    "${mainMod}, KP_Home, exec, code"
+    "${mainMod}, KP_Home, exec, keybind-logger 'code'"
+
+    # Screenshots
+    ''${mainMod}, S, exec, grim -g "$(slurp)" "$HOME/Pictures/Screenshots/$(date '+%y%m%d_%H-%M-%S').png"''
+    ''${mainMod} SHIFT, S, exec, grim -g "$(slurp)" - | wl-copy''
+
+    # Color picker
+    "${mainMod}, mouse:274, exec, hyprpicker -a"
+    "${mainMod}, mouse:275, exec, keybind-logger 'hyprpicker -a'"
+
+    # Workspace navigation
+    "${mainMod}, mouse_down, exec, hyprnome --previous"
+    "${mainMod}, mouse_up, exec, hyprnome"
+    "${mainMod} SHIFT, mouse_down, exec, hyprnome --previous --move"
+    "${mainMod} SHIFT, mouse_up, exec, hyprnome --move"
+
+    # Window management
+    "${mainMod}, SPACE, togglefloating"
+    "${mainMod}, C, killactive"
+    "${mainMod}, F, fullscreen"
+    "${mainMod} SHIFT, P, fullscreenstate"
+    "${mainMod}, P, pin"
+
+    # Focus movement
+    "${mainMod}, left, movefocus, l"
+    "${mainMod}, right, movefocus, r"
+    "${mainMod}, up, movefocus, u"
+    "${mainMod}, down, movefocus, d"
+
+    # Window movement
+    "${mainMod} SHIFT, left, movewindow, l"
+    "${mainMod} SHIFT, right, movewindow, r"
+    "${mainMod} SHIFT, up, movewindow, u"
+    "${mainMod} SHIFT, down, movewindow, d"
+
+    # Scratchpad
+    "${mainMod}, A, togglespecialworkspace, magic"
+    "${mainMod} SHIFT, A, movetoworkspacesilent, special:magic"
+  ];
+in
+{
+  wayland.windowManager.hyprland.settings = {
+    "$mainMod" = mainMod;
+    bindm = mousebinds;
+    bind = keybinds;
   };
 }
