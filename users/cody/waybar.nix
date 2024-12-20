60197bceaf7507985dd32e73f374e44ecd5f68ea
Cody W Tucker <cody@tmvsocial.com>
2024-12-20 08:28:18 -0600

    Pre-update commit

--- users/cody/waybar.nix
+++ users/cody/waybar.nix
@@ -18,7 +18,7 @@ let
     ];
     "hyprland/workspaces" = {
       on-click = "activate";
-      format = "{icon}";
+      format = "{windows}";
       window-rewrite = {
         "google-chrome" = "";
         "spotify" = "";
@@ -157,7 +157,7 @@ let
     ];
     "hyprland/workspaces" = {
       on-click = "activate";
-      format = "{icon}";
+      format = "{windows}";
       window-rewrite = {
         "google-chrome" = "";
         "spotify" = "";
