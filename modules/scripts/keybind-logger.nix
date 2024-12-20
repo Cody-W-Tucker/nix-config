0561041e5dcc7a9550487a3bc3ffdd1b62ebc1dc
Cody W Tucker <cody@tmvsocial.com>
2024-12-20 13:13:32 -0600

    Pre-update commit

--- modules/scripts/keybind-logger.nix
+++ modules/scripts/keybind-logger.nix
@@ -4,15 +4,10 @@ pkgs.writeShellScriptBin "keybind-logger" ''
   #!/bin/bash
 
   LOG_FILE="/tmp/keybind_usage.log"
+
+  # Get current timestamp
   timestamp=$(date '+%Y-%m-%d %H:%M:%S')
-  cmd="$*"
 
-  # Log the keybind with timestamp and all arguments
-  echo "$timestamp | \"$cmd\"" >> "$LOG_FILE" || {
-    echo "Failed to write to log file" >&2
-    exit 1
-  }
-
-  # Execute the command
-  exec "$@"
+  # Log the keybind with timestamp
+  echo "$timestamp | $1" >> "$LOG_FILE"
 ''
