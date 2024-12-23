9df7ef8a6dfa9e55462b479fa9791510440af110
Cody W Tucker <cody@tmvsocial.com>
2024-12-23 08:30:03 -0600

    Pre-update commit

--- modules/scripts/rofi-launcher.nix
+++ modules/scripts/rofi-launcher.nix
@@ -1,11 +1,10 @@
 { pkgs }:
-pkgs.writeShellScriptBin "rofi-launcher" ''
-  #!/bin/bash
 
-  # Check if Rofi is already running and kill it if so
+pkgs.writeShellScriptBin "rofi-launcher" ''
   if pgrep -x "rofi" > /dev/null; then
+    # Rofi is running, kill it
     pkill -x rofi
     exit 0
   fi
+  rofi -show drun
 ''
-
