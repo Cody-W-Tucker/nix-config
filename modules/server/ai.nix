7f173350ba3a9a23cceac880987522284a6a0651
Cody W Tucker <cody@tmvsocial.com>
2025-01-08 13:58:50 -0600

    Revert "try: Qdrant on open-webui"
    This reverts commit 1169d98b76fe2b0a4e0490a4b31093ce6573dd79.

--- modules/server/ai.nix
+++ modules/server/ai.nix
@@ -27,8 +27,6 @@ in
         SEARXNG_QUERY_URL = "https://search.homehub.tv/search?q=<query>";
         ENABLE_RAG_HYBRID_SEARCH = "True";
         RAG_RERANKING_MODEL = "";
-        VECTOR_DB = "qdrant";
-        QDRANT_URI = "http://qdrant.homehub.tv";
       };
     };
     # docker run -d -p 9099:9099 --add-host=host.docker.internal:host-gateway -v pipelines:/app/pipelines --name pipelines --restart always ghcr.io/open-webui/pipelines:main
