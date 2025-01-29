e303023bef5c7df258b5b1e2af8124de742c5dd8
Cody W Tucker <cody@tmvsocial.com>
2025-01-29 10:58:02 -0600

    pre-upgrade commit

--- cody/ui/hyprland/autostart.nix
+++ cody/ui/hyprland/autostart.nix
@@ -9,8 +9,8 @@
       "systemctl --user enable --now hypridle.service"
       "systemctl --user enable --now hyprpaper.service"
       "systemctl --user enable --now waybar.service"
-      "[workspace 1 silent] uwsm app -- obsidian"
-      "[workspace 2 silent] uwsm app -- todoist-electron --ozone-platform-hint=auto"
+      "[workspace 1 silent ] uwsm app -- obsidian"
+      "[workspace 2 silent ] uwsm app -- todoist-electron --ozone-platform-hint=auto"
       "uwsm app -- ferdium"
       "[workspace special silent] uwsm app -- spotube"
 
