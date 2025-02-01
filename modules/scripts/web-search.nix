3f02e6989e5f5914bc7693f28713fc1b3ee2c938
Cody W Tucker <cody@tmvsocial.com>
2025-02-01 16:58:43 -0600

    update web searcher

--- modules/scripts/web-search.nix
+++ modules/scripts/web-search.nix
@@ -1,15 +1,18 @@
 { pkgs }:
 
+# TODO: get obsidian to work (XDG magic): ["🟪 Obsidian"]="obsidian://search?vault=your_vault_name&query="
+
 pkgs.writeShellScriptBin "web-search" ''
   declare -A URLS
 
   URLS=(
     ["🔍 Google"]="https://www.google.com/search?q="
     ["🧠 Perplexity"]="https://www.perplexity.ai/search/?q="
-    ["📦 Nix Options"]="https://mynixos.com/search?q="
     ["🗃️ Hoarder"]="https://hoarder.homehub.tv/dashboard/search?q="
-    ["📚 Wikipedia"]="https://en.wikipedia.org/wiki/Special:Search?search="
+    ["❄️ Nix Options"]="https://mynixos.com/search?q="
+    ["💻 GitHub"]="https://github.com/search?q="
     ["🎥 YouTube"]="https://www.youtube.com/results?search_query="
+    ["📚 Wikipedia"]="https://en.wikipedia.org/wiki/Special:Search?search="
   )
 
   gen_list() {
@@ -17,10 +20,10 @@ pkgs.writeShellScriptBin "web-search" ''
   }
 
   main() {
-    platform=$(gen_list | ${pkgs.rofi}/bin/rofi -dmenu -i -l 6 -p 'Select Search Platform' -no-custom)
+    platform=$(gen_list | ${pkgs.rofi}/bin/rofi -dmenu -i -l 7 -p 'Select Search Platform' -no-custom)
 
     if [[ -n "$platform" ]]; then
-      query=$(${pkgs.rofi}/bin/rofi -dmenu -p 'Enter Search Query' -l 0)
+      query=$(${pkgs.rofi}/bin/rofi -dmenu -p 'Enter Search Query' -l 0 -multi-select)
 
       if [[ -n "$query" ]]; then
         url="''${URLS[$platform]}$(${pkgs.jq}/bin/jq -sRr @uri <<< "$query")"
