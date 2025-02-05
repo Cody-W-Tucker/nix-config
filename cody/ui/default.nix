488b436e0721dd9194e124e228b7429b3f33f6df
Cody W Tucker <cody@tmvsocial.com>
2025-02-05 07:51:26 -0600

    Added obs and timer to keep opeb-webui updated

--- cody/ui/default.nix
+++ cody/ui/default.nix
@@ -50,6 +50,17 @@
       vscode
     ]);
 
+  # Obs for screenrecording
+  programs.obs-studio = {
+    enable = true;
+    plugins = with pkgs.obs-studio-plugins; [
+      wlrobs
+      obs-backgroundremoval
+      obs-pipewire-audio-capture
+    ];
+  };
+
+
   # Clipboard history
   services.cliphist.enable = true;
 
