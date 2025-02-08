35c14aaad63b81385b48c1669852cfb0b94b502f
Cody W Tucker <cody@tmvsocial.com>
2025-02-08 16:35:02 -0600

    upgrade flake

--- modules/server/data.nix
+++ modules/server/data.nix
@@ -34,8 +34,6 @@
       local all       all                       trust
       host  metabase  metabase  127.0.0.1/32    md5
       host  metabase  metabase  ::1/128         md5
-      host  miniflux  metabase  127.0.0.1/32    md5
-      host  miniflux  metabase  ::1/128         md5
     '';
   };
   virtualisation.oci-containers.containers = {
