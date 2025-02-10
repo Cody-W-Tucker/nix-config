19dda6e6b0a1209209b254a2555843d4fc4cfa70
Cody W Tucker <cody@tmvsocial.com>
2025-02-10 14:14:23 -0600

    remove duplicate systemd services

--- cody/ui/hyprland/autostart.nix
+++ cody/ui/hyprland/autostart.nix
@@ -9,13 +9,8 @@
       "systemctl --user enable --now hypridle.service"
       "systemctl --user enable --now hyprpaper.service"
 
-      # Clipboard (requires WAYLAND_DISPLAY)
-      "uwsm app -- wl-clipboard-history -t"
-      "uwsm app -- wl-paste --watch cliphist store"
+      # Clipboard
       ''rm "$HOME/.cache/cliphist/db"''
-
-      # GUI Apps (needs DBUS_SESSION_BUS_ADDRESS from finalize)
-      "uwsm app -- swaync"
 
       # Workspace-specific apps
       "[workspace 1 silent] uwsm app -- obsidian"
