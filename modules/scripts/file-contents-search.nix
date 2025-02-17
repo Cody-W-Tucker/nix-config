90c57a601b2d6dbdcb052845eff664c7bbbefa26
Cody W Tucker <cody@tmvsocial.com>
2025-02-17 09:17:45 -0600

    enable file contents searching

--- modules/scripts/file-contents-search.nix
+++ modules/scripts/file-contents-search.nix
@@ -1,16 +1,12 @@
 { pkgs }:
 
-pkgs.writeShellScriptBin {
-  name = "fif"; # Find in file
-  runtimeInputs = [ pkgs.ripgrep pkgs.fzf pkgs.bat ];
-  text = ''
-    #!/bin/bash
-    fif() {
-      if [ -z "$1" ]; then
-        echo "Usage: fif <search_term>"
-        return 1
-      fi
-      rg --files-with-matches "$1" | fzf --preview 'bat --style=numbers --color=always --line-range :500 {}'
-    }
-  '';
-}
+pkgs.writeShellScriptBin "fif" ''
+  #!/bin/bash
+  fif() {
+    if [ -z "$1" ]; then
+      echo "Usage: fif <search_term>"
+      return 1
+    fi
+    rg --files-with-matches "$1" | fzf --preview 'bat --style=numbers --color=always --line-range :500 {}'
+  }
+''
