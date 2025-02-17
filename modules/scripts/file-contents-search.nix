004141086bb5812076e493548c75a38281c9369a
Cody W Tucker <cody@tmvsocial.com>
2025-02-17 09:28:07 -0600

    enable file contents searching

--- modules/scripts/file-contents-search.nix
+++ modules/scripts/file-contents-search.nix
@@ -1,12 +1,14 @@
 { pkgs }:
 
-pkgs.writeShellScriptBin "fif" ''
-  #!/bin/bash
-  fif() {
+pkgs.writeShellScriptBin {
+  name = "fif";
+  runtimeInputs = [ pkgs.ripgrep pkgs.fzf pkgs.bat ]; # Ensure dependencies are included
+  text = ''
+    #!/bin/bash
     if [ -z "$1" ]; then
       echo "Usage: fif <search_term>"
-      return 1
+      exit 1
     fi
     rg --files-with-matches "$1" | fzf --preview 'bat --style=numbers --color=always --line-range :500 {}'
-  }
-''
+  '';
+}
