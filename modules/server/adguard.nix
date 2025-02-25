47f1cdcdb4b3f06a8b6c9bf7290e20424be78270
Cody W Tucker <cody@tmvsocial.com>
2025-02-24 21:44:26 -0600

    add adguardhome

--- modules/server/adguard.nix
+++ modules/server/adguard.nix
@@ -3,13 +3,12 @@
 {
   services.adguardhome = {
     enable = true;
-    port = 9999;
   };
   services.nginx.virtualHosts."adguardhome.homehub.tv" = {
     useACMEHost = "homehub.tv";
     forceSSL = true;
     locations."/" = {
-      proxyPass = "http://127.0.0.1:9999";
+      proxyPass = "http://127.0.0.1:3000";
       proxyWebsockets = true;
     };
   };
