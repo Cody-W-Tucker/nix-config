8dd34319a380b625c41ecab9c6df827347933e67
Cody W Tucker <cody@tmvsocial.com>
2025-03-12 16:37:54 -0500

    update bat config

--- cody/cli/default.nix
+++ cody/cli/default.nix
@@ -9,7 +9,6 @@
   home.packages =
     (with pkgs; [
       gh
-      bat
       fd
     ]);
 
@@ -65,6 +64,31 @@
         wayland_titlebar_color = "system";
         cursor_shape = "block";
       };
+    };
+    bat = {
+      enable = true;
+      config = {
+        pager = "less -FR";
+      };
+      themes =
+        let
+          src = pkgs.fetchFromGitHub {
+            owner = "catppuccin";
+            repo = "bat";
+            rev = "ba4d16880d63e656acced2b7d4e034e4a93f74b1";
+            hash = "sha256-6WVKQErGdaqb++oaXnY3i6/GuH2FhTgK0v4TN4Y0Wbw=";
+          };
+        in
+        {
+          Catppuccin-mocha = {
+            inherit src;
+            file = "Catppuccin-mocha.tmTheme";
+          };
+          Catppuccin-latte = {
+            inherit src;
+            file = "Catppuccin-latte.tmTheme";
+          };
+        };
     };
     zoxide.enable = true;
     ripgrep.enable = true;
