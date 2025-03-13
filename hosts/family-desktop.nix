7e6bc0c532f5a3deb31d2d8be8efc793f2400045
Cody W Tucker <cody@tmvsocial.com>
2025-03-12 19:29:41 -0500

    Add wallpapers

--- hosts/family-desktop.nix
+++ hosts/family-desktop.nix
@@ -67,7 +67,7 @@
   };
 
   # Setting the color theme and default wallpaper
-  stylix.image = ../modules/wallpapers/Dancer.png;
+  stylix.image = ../modules/wallpapers/galaxy-waves.jpg;
 
   # Don't change this
   system.stateVersion = "24.05"; # Did you read the comment?
