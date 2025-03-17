983e30ccee6c125ae36afb7b8e6b82bbd1320352
Cody W Tucker <cody@tmvsocial.com>
2025-03-17 16:27:55 -0500

    add immich

--- modules/server/photos.nix
+++ modules/server/photos.nix
@@ -8,24 +8,19 @@
   users.users.immich.extraGroups = [ "video" "render" "media" ];
 
   # NGINX
-  services.nginx = {
-    virtualHosts = {
-      "photos.homehub.tv" = {
-        forceSSL = true;
-        useACMEHost = "homehub.tv";
-        http2 = true;
-        locations."/" = {
-          proxyPass = "http://127.0.0.1:2283";
-          proxyWebsockets = true;
-          recommendedProxySettings = true;
-          extraConfig = ''
-            client_max_body_size 50000M;
-            proxy_read_timeout   600s;
-            proxy_send_timeout   600s;
-            send_timeout         600s;
-          '';
-        };
-      };
+  services.nginx.virtualHosts."photos.homehub.tv" = {
+    forceSSL = true;
+    useACMEHost = "homehub.tv";
+    locations."/" = {
+      proxyPass = "http://127.0.0.1:2283";
+      proxyWebsockets = true;
+      recommendedProxySettings = true;
+      extraConfig = ''
+        client_max_body_size 50000M;
+        proxy_read_timeout   600s;
+        proxy_send_timeout   600s;
+        send_timeout         600s;
+      '';
     };
   };
 
