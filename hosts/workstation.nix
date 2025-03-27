28b7e49721c81bf403fa8aeb7ac97311d87c81b5
Cody W Tucker <cody@tmvsocial.com>
2025-03-26 21:58:08 -0500

    add nvidia again...

--- hosts/workstation.nix
+++ hosts/workstation.nix
@@ -271,9 +271,13 @@
 
   # Nvidia PRIME
   hardware.nvidia.prime = {
+    offload = {
+			enable = true;
+			enableOffloadCmd = true;
+		};
 		# Make sure to use the correct Bus ID values for your system!
 		intelBusId = "PCI:0:2:0";
-		nvidiaBusId = "PCI:10:0:0";
+		nvidiaBusId = "PCI:1:0:0";
 	};
 
   # Don't change this
