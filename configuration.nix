41ce2ecf61770324662cd388d4dfd302ba8da2b6
Cody W Tucker <cody@tmvsocial.com>
2025-03-28 19:39:06 -0500

    fix slow zsh startup

--- configuration.nix
+++ configuration.nix
@@ -42,7 +42,6 @@
     };
     zsh = {
       enable = true;
-      autosuggestions.enable = true;
       # Disable the completion in the global module because it would call compinit
       # but the home manager config also calls compinit. This causes the cache to be invalidated
       # because the fpath changes in-between, causing constant re-evaluation and thus startup
