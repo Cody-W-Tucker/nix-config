cdfdd580c20cae4b84bbff0adb9e5296770ccd04
Cody W Tucker <cody@tmvsocial.com>
2025-04-06 16:04:25 -0500

    get nvidia-offload work with electron

--- hosts/workstation.nix
+++ hosts/workstation.nix
@@ -241,6 +241,7 @@
 
   environment.sessionVariables = {
     NIXOS_OZONE_WL = "1";
+    ELECTRON_OZONE_PLATFORM_HINT = "auto";
     WAYLAND_DISPLAY = "wayland-0";
     # Qt Variables
     QT_AUTO_SCREEN_SCALE_FACTOR = "1";
