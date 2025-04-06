bd699850c5e55eab80eae78ef9ee8786a73256d5
Cody W Tucker <cody@tmvsocial.com>
2025-04-06 15:42:47 -0500

    get nvidia-offload work with electron

--- hosts/workstation.nix
+++ hosts/workstation.nix
@@ -6,8 +6,8 @@ let
         export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
         export __GLX_VENDOR_LIBRARY_NAME=nvidia
         export __VK_LAYER_NV_optimus=NVIDIA_only
-        export LIBVA_DRIVER_NAME = "nvidia";
-        export GBM_BACKEND = "nvidia-drm";
+        export LIBVA_DRIVER_NAME = "nvidia"
+        export GBM_BACKEND = "nvidia-drm"
         exec "$@"
       '';
 
