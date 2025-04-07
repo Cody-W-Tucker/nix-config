18517dc000545f9364c5cec4b101b6f5b307f6d1
Cody W Tucker <cody@tmvsocial.com>
2025-04-06 20:16:55 -0500

    add tailscale to server

--- modules/server/vpn.nix
+++ modules/server/vpn.nix
@@ -3,5 +3,5 @@
   networking.firewall.allowedUDPPorts = [ 41641 ];
   services.tailscale.useRoutingFeatures = "server";
   networking.firewall.trustedInterfaces = [ "tailscale0" ];  # Allow traffic from Tailscale
-  services.tailscale.advertiseRoutes = [ "192.168.254.254/24" ];  # Replace with your actual LAN subnet
+  services.tailscale.advertiseRoutes = [ "192.168.254.254/24" ];
 }
