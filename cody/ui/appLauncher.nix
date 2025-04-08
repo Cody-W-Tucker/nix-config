9110d18dc961711218881f7771a8f85fb38b8993
Cody W Tucker <cody@tmvsocial.com>
2025-04-08 17:19:34 -0500

    home-manager rofi

--- cody/ui/appLauncher.nix
+++ cody/ui/appLauncher.nix
@@ -1,6 +1,7 @@
-{ lib, config, ... }:
+{ lib, config, pkgs, ... }:
 
 {
+  home-manager.users.codyt.programs.rofi.plugins =[pkgs.rofi-calc];
   home.file.".config/rofi/config.rasi".text = lib.mkForce ''
     @theme "/dev/null"
 
