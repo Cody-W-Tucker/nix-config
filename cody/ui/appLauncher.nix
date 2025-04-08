2723a47bb872c5c98625855177ca717e85bd428e
Cody W Tucker <cody@tmvsocial.com>
2025-04-08 18:07:28 -0500

    home-manager rofi

--- cody/ui/appLauncher.nix
+++ cody/ui/appLauncher.nix
@@ -20,7 +20,6 @@
       "*" = {
         bg = mkLiteral "#${config.lib.stylix.colors.base00}";
         bg-alt = mkLiteral "#${config.lib.stylix.colors.base09}";
-        foreground = mkLiteral "#${config.lib.stylix.colors.base01}";
         selected = mkLiteral "#${config.lib.stylix.colors.base08}";
         active = mkLiteral "#${config.lib.stylix.colors.base0B}";
         text-selected = mkLiteral "#${config.lib.stylix.colors.base00}";
