522a116e031bbfaae8b942867e52cb00659a567c
Cody W Tucker <cody@tmvsocial.com>
2025-04-08 21:24:49 -0500

    home-manager rofi

--- cody/ui/appLauncher.nix
+++ cody/ui/appLauncher.nix
@@ -12,7 +12,6 @@
       font = "JetBrains Nerd Font 16";
       drun-display-format = "{icon} {name}";
       display-web-search = "";
-      kb-custom-1 = "Control+Return";  # Select platform without query
       display-drun = "";
       # display-filebrowser = "";
     };
