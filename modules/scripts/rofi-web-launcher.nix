4b6561857e7b6061a3822ae24e6cc356842ad101
Cody W Tucker <cody@tmvsocial.com>
2025-04-08 20:49:27 -0500

    home-manager rofi

--- modules/scripts/rofi-web-launcher.nix
+++ modules/scripts/rofi-web-launcher.nix
@@ -3,13 +3,6 @@
 pkgs.writeShellScriptBin "web-search" ''
   #!/usr/bin/env bash
 
-  # Check if Rofi is already running
-  if pgrep -x "rofi" > /dev/null; then
-    pkill -x rofi
-    exit 0
-  fi
-
-  # Declare search platforms and their base URLs
   declare -A URLS=(
     ["🔍 Google"]="https://www.google.com/search?q="
     ["🧠 Perplexity"]="https://www.perplexity.ai/search/?q="
@@ -20,37 +13,28 @@ pkgs.writeShellScriptBin "web-search" ''
     ["📚 Wikipedia"]="https://en.wikipedia.org/wiki/Special:Search?search="
   )
 
-  # Generate list of search platforms for Rofi
   gen_list() {
     for platform in "''${!URLS[@]}"; do
-      echo "$platform"
+      echo -en "$platform\0info\x1f''${URLS[$platform]}\n"
     done
   }
 
-  # Main function
-  main() {
-    # Prompt user to select a platform using Rofi
-    platform=$(gen_list | rofi -dmenu -i -l 7 -p 'Select Search Platform' -no-custom)
+  handle_selection() {
+    platform="$1"
+    base_url="''${URLS[$platform]}"
+    query=$(echo -en "\0prompt\x1fQuery for $platform\n" | ${pkgs.rofi}/bin/rofi -dmenu -l 0)
 
-    # If a platform is selected, prompt for a search query
-    if [[ -n "$platform" ]]; then
-      query=$(rofi -dmenu -p 'Enter Search Query' -l 0)
-
-      # Get the base URL for the selected platform
-      base_url="''${URLS[$platform]}"
-
-      if [[ -n "$query" ]]; then
-        # Encode the query string properly
-        url="''${base_url}$(echo "$query" | jq -Rr @uri)"
-      else
-        # If no query is entered, open the base URL
-        url="$base_url"
-      fi
-
-      # Open the URL in the default browser
-      xdg-open "$url"
+    if [[ -n "$query" ]]; then
+      url="''${base_url}$(echo "$query" | ${pkgs.jq}/bin/jq -Rr @uri)"
+    else
+      url="$base_url"
     fi
+    ${pkgs.xdg-utils}/bin/xdg-open "$url"
   }
 
-  main
+  # Rofi script mode logic
+  case "$ROFI_RETV" in
+    0) gen_list ;;    # Initial call
+    1) handle_selection "$1" ;;  # Selection made
+  esac
 ''
