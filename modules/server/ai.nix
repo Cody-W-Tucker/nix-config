297103a14ed1efab5f64bf4d78c6e2ffa14436c3
Cody W Tucker <cody@tmvsocial.com>
2025-04-15 13:54:01 -0500

    reduce websocket config

--- modules/server/ai.nix
+++ modules/server/ai.nix
@@ -101,12 +101,8 @@ in
           proxyPass = "http://127.0.0.1:8080";
           proxyWebsockets = true;
           extraConfig = ''
-            proxy_set_header Host $host;
-            proxy_set_header X-Real-IP $remote_addr;
-            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-            proxy_set_header X-Forwarded-Proto $scheme;
-
-            add_header X-Robots-Tag "none";
+            proxy_buffering off;
+            client_max_body_size 256m;
           '';
         };
       };
