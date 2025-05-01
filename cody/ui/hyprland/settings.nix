eb2ab968637105e27927b52d9209101d9aeb3da9
Cody W Tucker <cody@tmvsocial.com>
2025-05-01 16:43:37 -0500

    change animations

--- cody/ui/hyprland/settings.nix
+++ cody/ui/hyprland/settings.nix
@@ -92,7 +92,7 @@ in
         bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
         animation = [
           "windows, 1, 2, myBezier"
-          "windowsOut, 1, 2, default, popin 80%"
+          "windowsOut, 1, 2, myBezier"
           "border, 1, 10, default"
           "borderangle, 1, 8, default"
           "fade, 1, 2, default"
