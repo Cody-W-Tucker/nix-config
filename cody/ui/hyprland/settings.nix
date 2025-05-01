b8001456aad167dddbf6370666c874907a9601c5
Cody W Tucker <cody@tmvsocial.com>
2025-05-01 16:35:16 -0500

    change animations

--- cody/ui/hyprland/settings.nix
+++ cody/ui/hyprland/settings.nix
@@ -91,12 +91,12 @@ in
         enabled = true;
         bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
         animation = [
-          "windows, 1, 5, myBezier"
-          "windowsOut, 1, 4, default, popin 80%"
+          "windows, 1, 2, myBezier"
+          "windowsOut, 1, 2, default, popin 80%"
           "border, 1, 10, default"
           "borderangle, 1, 8, default"
-          "fade, 1, 5, default"
-          "workspaces, 1, 5, default"
+          "fade, 1, 2, default"
+          "workspaces, 1, 2, default"
         ];
       };
       input = {
