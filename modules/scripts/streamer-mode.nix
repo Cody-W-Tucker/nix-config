cfa81757e26394d24e43f2ded425cbdccec7599b
Cody W Tucker <cody@tmvsocial.com>
2025-05-02 08:05:23 -0500

    change streamer mode

--- modules/scripts/streamer-mode.nix
+++ modules/scripts/streamer-mode.nix
@@ -10,15 +10,9 @@ if [ "$MODE" = "stream" ]; then
     # Set reserved area for 1920x1080 centered on 2560x1440
     hyprctl keyword monitor "$MONITOR,addreserved,0,0,320,320"
 
-    # Stop waybar
-    systemctl --user stop waybar.service
-
 elif [ "$MODE" = "reset" ]; then
     # Remove reserved area (set all to 0)
     hyprctl keyword monitor "$MONITOR,addreserved,0,0,0,0"
-
-    # Start waybar
-    systemctl --user start waybar.service
 
 else
     echo "Usage: $0 [stream|reset]"
