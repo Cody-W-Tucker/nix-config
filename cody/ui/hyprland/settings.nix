4468eb717e18b4fe288334b1c9654d04341a743e
Cody W Tucker <cody@tmvsocial.com>
2025-05-03 11:26:58 -0500

    change animations

--- cody/ui/hyprland/settings.nix
+++ cody/ui/hyprland/settings.nix
@@ -102,8 +102,8 @@ in
           "easeOutExpo, 0.16, 1, 0.3, 1"
           ];
         animation = [
-          "windows, 1, 3, easeInExpo, slide 80%"
-          "windowsOut, 1, 3, easeOutExpo, slide 80%"
+          "windows, 1, 1, easeInExpo, slide 80%"
+          "windowsOut, 1, 1, easeOutExpo, slide 80%"
           "border, 1, 10, default"
           "borderangle, 1, 8, default"
           "fade, 1, 2, default"
