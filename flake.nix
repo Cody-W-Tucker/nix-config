51c5ff7a178c02aafbe9d817e6d5beac26c38c0c
Cody W Tucker <cody@tmvsocial.com>
2025-05-04 15:00:58 -0500

    remove whisper-overlay

--- flake.nix
+++ flake.nix
@@ -30,7 +30,7 @@
     };
   };
 
-  outputs = inputs@{ self, nixpkgs, nixpkgs-unstable, home-manager, sops-nix, nixos-hardware, stylix, nixvim, whisper-overlay, ... }:
+  outputs = inputs@{ self, nixpkgs, nixpkgs-unstable, home-manager, sops-nix, nixos-hardware, stylix, nixvim, ... }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs {
