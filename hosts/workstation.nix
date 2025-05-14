a4590cb23fa109c33b851fcd2e4198f0fbf935a7
Cody W Tucker <cody@tmvsocial.com>
2025-05-14 18:42:47 -0500

    remove sound device priority

--- hosts/workstation.nix
+++ hosts/workstation.nix
@@ -317,36 +317,6 @@ environment.sessionVariables = {
   virtualisation.oci-containers.backend = "docker";
 
 
-  # WirePlumber device priorities
-  environment.etc."wireplumber/policy-device.conf.d/50-device-priorities.conf".text = ''
-    device.match = [
-      {
-        node.name = "alsa_output.usb-Dell_Dell_AC511_USB_SoundBar-00.iec958-stereo"
-        priority = 100
-        profile = "iec958-stereo"
-      },
-      {
-        node.name = "bluez_output.74_74_46_1C_20_61.1"
-        priority = 200
-        profile = "a2dp-sink"
-      },
-      {
-        node.name = "alsa_output.usb-Blue_Microphones_Yeti_Stereo_Microphone_797_2018_01_30_47703-00.pro-output-0"
-        priority = 0
-        enabled = false
-      },
-      {
-        node.name = "alsa_input.usb-Blue_Microphones_Yeti_Stereo_Microphone_797_2018_01_30_47703-00.pro-input-0"
-        priority = 300
-        profile = "pro-input-0"
-      },
-      {
-        node.name = "rnnoise_source"
-        priority = 400
-      }
-    ]
-  '';
-
   # Automatic sink switching
   systemd.user.services.pipewire-switch-on-connect = {
     description = "Load PipeWire module-switch-on-connect";
