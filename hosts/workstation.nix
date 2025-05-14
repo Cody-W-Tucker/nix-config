a85ab79d82921d5f37fa735143be379e63b51d49
Cody W Tucker <cody@tmvsocial.com>
2025-05-14 18:24:26 -0500

    set sound device priority

--- hosts/workstation.nix
+++ hosts/workstation.nix
@@ -316,57 +316,6 @@ environment.sessionVariables = {
   };
   virtualisation.oci-containers.backend = "docker";
 
-  # WirePlumber device priorities
-  xdg.configFile."wireplumber/main.lua.d/50-device-priorities.lua" = {
-    text = ''
-      alsa_monitor.rules = {
-        {
-          matches = {
-            { { "node.name", "matches", "alsa_output.usb-Dell_Dell_AC511_USB_SoundBar-00.iec958-stereo" } },
-          },
-          apply_properties = {
-            ["priority.session"] = 100,
-            ["api.alsa.use-acp"] = true,
-          },
-        },
-        {
-          matches = {
-            { { "node.name", "matches", "bluez_output.74_74_46_1C_20_61.1" } },
-          },
-          apply_properties = {
-            ["priority.session"] = 200,
-          },
-        },
-        {
-          matches = {
-            { { "node.name", "matches", "alsa_output.usb-Blue_Microphones_Yeti_Stereo_Microphone_797_2018_01_30_47703-00.pro-output-0" } },
-          },
-          apply_properties = {
-            ["priority.session"] = 0,
-            ["node.disabled"] = true, -- Explicitly disable Yeti output
-          },
-        },
-        {
-          matches = {
-            { { "node.name", "matches", "alsa_input.usb-Blue_Microphones_Yeti_Stereo_Microphone_797_2018_01_30_47703-00.pro-input-0" } },
-          },
-          apply_properties = {
-            ["priority.session"] = 300,
-            ["api.alsa.use-acp"] = true,
-          },
-        },
-        {
-          matches = {
-            { { "node.name", "matches", "rnnoise_source" } },
-          },
-          apply_properties = {
-            ["priority.session"] = 400,
-          },
-        },
-      }
-    '';
-  };
-
   # Automatic sink switching
   systemd.user.services.pipewire-switch-on-connect = {
     description = "Load PipeWire module-switch-on-connect";
