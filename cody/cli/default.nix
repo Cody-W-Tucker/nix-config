431578d7047ad7bbc52ad00bcaafd933bc271183
Cody W Tucker <cody@tmvsocial.com>
2025-05-15 14:23:30 -0500

    add ranger plugins

--- cody/cli/default.nix
+++ cody/cli/default.nix
@@ -43,8 +43,18 @@
       "preview_images_method" = "kitty";
     };
     extraConfig = ''
-      default_linemode devicons
+      default_linemode devicons2
     '';
+    plugins = [
+      {
+        name = "ranger-devicons2";
+        src = builtins.fetchGit {
+          url = "https://github.com/cdump/ranger-devicons2";
+          ref = "master";
+          rev = "94bdcc19218681debb252475fd9d11cfd274d9b1";
+        };
+      }
+    ];
   };
 
   programs = {
