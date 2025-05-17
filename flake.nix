b84bf2f732573d75950b43957948ae04ffb83a95
Cody W Tucker <cody@tmvsocial.com>
2025-05-16 19:13:31 -0500

    add Arr stack

--- flake.nix
+++ flake.nix
@@ -110,6 +110,7 @@
             nixos-hardware.nixosModules.common-gpu-intel-kaby-lake
             nixos-hardware.nixosModules.common-pc-ssd
             inputs.sops-nix.nixosModules.sops
+            nixarr.nixosModules.default
             ./secrets/secrets.nix
             inputs.home-manager.nixosModules.home-manager
             {
