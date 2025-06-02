4dc64fd61dd305fa2db8666d33ec433718c2808c
Cody W Tucker <cody@tmvsocial.com>
2025-06-01 19:04:50 -0500

    update ACME config

--- modules/server/default.nix
+++ modules/server/default.nix
@@ -43,7 +43,7 @@
       dnsPropagationCheck = true;
       credentialFiles = {
         "CF_API_EMAIL_FILE" = config.sops.secrets.cloudflare-api-email.path;
-        "CLOUDFLARE_API_KEY" = config.sops.secrets.cloudflare-api-key.path;
+        "CLOUDFLARE_API_KEY_FILE" = config.sops.secrets.cloudflare-api-key.path;
       };
     };
   };
