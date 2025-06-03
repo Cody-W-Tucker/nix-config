37d2b33adebe032d17e5f16fbfd506899b064954
Cody W Tucker <cody@tmvsocial.com>
2025-06-02 23:53:51 -0500

    add speed tweaks transmission

--- modules/server/media.nix
+++ modules/server/media.nix
@@ -38,25 +38,25 @@
         umask = 2; # Group write permissions (so Sonarr/Radarr can move files)
         dht-enabled = true;
         encryption = 1; # Prefer encrypted peers
-        download-queue-enabled = true;
-        download-queue-size = 5;
         anti-brute-force-enabled = true;
         anti-brute-force-threshold = 10;
         blocklist-enabled = true;
         blocklist-url = "https://github.com/Naunter/BT_BlockLists/raw/master/bt_blocklists.gz";
-        idle-seeding-limit = 30;
+        preallocation = 1;
+        lazy-bitfield-enabled = true;
+
+        # Speed Limits
+        speed-limit-up = 110;
+        speed-limit-up-enabled = true;
+        speed-limit-down = 1400;
+        speed-limit-down-enabled = true;
+        download-queue-enabled = true;
+        download-queue-size = 5;
 
         # Speed tweaks
         peer-limit-global = 100;
         peer-limit-per-torrent = 40;
         upload-slots-per-torrent = 4;
-        speed-limit-up = 100;
-        speed-limit-up-enabled = true;
-        speed-limit-down = 1400;
-        speed-limit-down-enabled = true;
-        seed-queue-size = 2;
-        preallocation = 1;
-        lazy-bitfield-enabled = true;
 
         # Download Schedule
         alt-speed-enabled = true; # Enable alternative speed limits
