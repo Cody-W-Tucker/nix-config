d87ad6c52f265a84062f283a6325f40ff0f9172a
Cody W Tucker <cody@tmvsocial.com>
2025-06-12 18:39:54 -0500

    add samba

--- modules/server/samba.nix
+++ modules/server/samba.nix
@@ -1,60 +1,49 @@
-# Broken due to replacing extraConfig with settings.
 {
-  # services.samba = {
-  #   enable = true;
-  #   securityType = "user";
-  #   openFirewall = true;
-  #   extraConfig = ''
-  #     workgroup = WORKGROUP
-  #     server string = smbnix
-  #     netbios name = smbnix
-  #     security = user 
-  #     #use sendfile = yes
-  #     #max protocol = smb2
-  #     # note: localhost is the ipv6 localhost ::1
-  #     hosts allow = 192.168.1. 127.0.0.1 localhost
-  #     hosts deny = 0.0.0.0/0
-  #     guest account = nobody
-  #     map to guest = bad user
-  #   '';
-  #   shares = {
-  #     codytHome = {
-  #       path = "/mnt/hdd/Share";
-  #       browseable = "yes";
-  #       "read only" = "no";
-  #       "guest ok" = "no";
-  #       "create mask" = "0644";
-  #       "directory mask" = "0755";
-  #       "force user" = "codyt";
-  #       "force group" = "users";
-  #     };
-  #     media = {
-  #       path = "/mnt/hdd/Media";
-  #       browseable = "yes";
-  #       "read only" = "no";
-  #       "guest ok" = "no";
-  #       "create mask" = "0644";
-  #       "directory mask" = "0755";
-  #       "force user" = "codyt";
-  #       "force group" = "users";
-  #     };
-  #     photos = {
-  #       path = "/mnt/hdd/Photos";
-  #       browseable = "yes";
-  #       "read only" = "no";
-  #       "guest ok" = "no";
-  #       "create mask" = "0644";
-  #       "directory mask" = "0755";
-  #       "force user" = "codyt";
-  #       "force group" = "users";
-  #     };
-  #   };
-  # };
+  services.samba = {
+    enable = true;
+    securityType = "user";
+    openFirewall = true;
+    extraConfig = ''
+      workgroup = WORKGROUP
+      server string = smbnix
+      netbios name = smbnix
+      security = user 
+      #use sendfile = yes
+      #max protocol = smb2
+      # note: localhost is the ipv6 localhost ::1
+      hosts allow = 192.168.1. 127.0.0.1 localhost
+      hosts deny = 0.0.0.0/0
+      guest account = nobody
+      map to guest = bad user
+    '';
+    shares = {
+      codytHome = {
+        path = "/mnt/hdd/Share";
+        browseable = "yes";
+        "read only" = "no";
+        "guest ok" = "no";
+        "create mask" = "0644";
+        "directory mask" = "0755";
+        "force user" = "codyt";
+        "force group" = "users";
+      };
+      Music = {
+        path = "/mnt/hdd/Media/Music";
+        browseable = "yes";
+        "read only" = "no";
+        "guest ok" = "no";
+        "create mask" = "0644";
+        "directory mask" = "0755";
+        "force user" = "codyt";
+        "force group" = "users";
+      };
+    };
+  };
 
-  # services.samba-wsdd = {
-  #   enable = true;
-  #   openFirewall = true;
-  # };
+  services.samba-wsdd = {
+    enable = true;
+    openFirewall = true;
+  };
 
   # Syncthing backup
   services.syncthing.settings.folders."share" = {
