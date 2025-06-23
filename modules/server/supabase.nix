e36354179ce673c4899b1e5549bd6d6ddac26373
Cody W Tucker <cody@tmvsocial.com>
2025-06-23 08:51:24 -0500

    update supabase

--- modules/server/supabase.nix
+++ modules/server/supabase.nix
@@ -8,6 +8,7 @@
       useACMEHost = "homehub.tv";
       locations."/" = {
         proxyPass = "http://localhost:3000";
+        proxyWebsockets = true;
       };
     };
   };
@@ -598,7 +599,7 @@
       "POSTGRES_PASSWORD" = "your-super-secret-and-long-postgres-password";
       "STUDIO_PG_META_URL" = "http://meta:8080";
       "SUPABASE_ANON_KEY" = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyAgCiAgICAicm9sZSI6ICJhbm9uIiwKICAgICJpc3MiOiAic3VwYWJhc2UtZGVtbyIsCiAgICAiaWF0IjogMTY0MTc2OTIwMCwKICAgICJleHAiOiAxNzk5NTM1NjAwCn0.dc_X5iR_VP_qT0zsiyj_I_OZ2T9FtRU2BBNWN8Bu4GE";
-      "SUPABASE_PUBLIC_URL" = "http://localhost:3000";
+      "SUPABASE_PUBLIC_URL" = "https://studio.homehub.tv";
       "SUPABASE_SERVICE_KEY" = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyAgCiAgICAicm9sZSI6ICJzZXJ2aWNlX3JvbGUiLAogICAgImlzcyI6ICJzdXBhYmFzZS1kZW1vIiwKICAgICJpYXQiOiAxNjQxNzY5MjAwLAogICAgImV4cCI6IDE3OTk1MzU2MDAKfQ.DaYlNEoUrrEn2Ig7tqibS-PHK5vgusbcbo7X36XVt4Q";
       "SUPABASE_URL" = "http://kong:8000";
     };
@@ -606,6 +607,7 @@
       "supabase-analytics"
     ];
     log-driver = "journald";
+    ports = [ "3000:3000/tcp" ];
     extraOptions = [
       "--health-cmd=node -e \"require('http').get('http://' + require('os').hostname() + ':3000/api/platform/profile', r => process.exit(r.statusCode === 200 ? 0 : 1))\""
       "--health-interval=5s"
