1d63c4aaae911d0e33840a1181f3f14b0529a318
Cody W Tucker <cody@tmvsocial.com>
2025-06-23 08:39:29 -0500

    update supabase

--- modules/server/supabase.nix
+++ modules/server/supabase.nix
@@ -354,7 +354,7 @@
       "SUPABASE_SERVICE_KEY" = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyAgCiAgICAicm9sZSI6ICJzZXJ2aWNlX3JvbGUiLAogICAgImlzcyI6ICJzdXBhYmFzZS1kZW1vIiwKICAgICJpYXQiOiAxNjQxNzY5MjAwLAogICAgImV4cCI6IDE3OTk1MzU2MDAKfQ.DaYlNEoUrrEn2Ig7tqibS-PHK5vgusbcbo7X36XVt4Q";
     };
     volumes = [
-      "/home/codyt/supabase-docker/volumes/api/kong.yml:/home/kong/temp.yml:ro,z"
+      "/home/codyt/supabase-docker/volumes/api/kong.yml:/home/kong/temp.yml:ro"
     ];
     ports = [
       "8800:8000/tcp"
