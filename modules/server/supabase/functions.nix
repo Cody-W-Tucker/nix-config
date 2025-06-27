8e7ee4ee03f215c187f2718207d1f4147ea0e01e
Cody W Tucker <cody@tmvsocial.com>
2025-06-27 13:28:09 -0500

    remove all placeholder secrets

--- modules/server/supabase/functions.nix
+++ modules/server/supabase/functions.nix
@@ -24,10 +24,6 @@
       config.sops.templates."edge-functions".path
     ];
     environment = {
-      # "JWT_SECRET" = "your-super-secret-jwt-token-with-at-least-32-characters-long";
-      # "SUPABASE_ANON_KEY" = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyAgCiAgICAicm9sZSI6ICJhbm9uIiwKICAgICJpc3MiOiAic3VwYWJhc2UtZGVtbyIsCiAgICAiaWF0IjogMTY0MTc2OTIwMCwKICAgICJleHAiOiAxNzk5NTM1NjAwCn0.dc_X5iR_VP_qT0zsiyj_I_OZ2T9FtRU2BBNWN8Bu4GE";
-      # "SUPABASE_DB_URL" = "postgresql://postgres:your-super-secret-and-long-postgres-password@db:5432/postgres";
-      # "SUPABASE_SERVICE_ROLE_KEY" = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyAgCiAgICAicm9sZSI6ICJzZXJ2aWNlX3JvbGUiLAogICAgImlzcyI6ICJzdXBhYmFzZS1kZW1vIiwKICAgICJpYXQiOiAxNjQxNzY5MjAwLAogICAgImV4cCI6IDE3OTk1MzU2MDAKfQ.DaYlNEoUrrEn2Ig7tqibS-PHK5vgusbcbo7X36XVt4Q";
       "SUPABASE_URL" = "http://kong:8000";
       "VERIFY_JWT" = "false";
     };
