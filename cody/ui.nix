4901b83e4e3223beb9402a72121e58098a803018
Cody W Tucker <cody@tmvsocial.com>
2025-07-08 14:26:25 -0500

    stylix

--- cody/ui.nix
+++ cody/ui.nix
@@ -4,16 +4,18 @@
     ./cli
   ];
 
-  # Keyboard
-  home.keyboard = {
-    layout = "us";
-    model = "pc105";
+  config = {
+    # Keyboard
+    home.keyboard = {
+      layout = "us";
+      model = "pc105";
+    };
+    # Set the wallpaper
+    stylix.image = ../modules/wallpapers/galaxy-waves.jpg;
+
+    home.sessionVariables.CUDA_CACHE_PATH = "\${HOME}/.cache/nv";
+
+    # The state version is required and should stay at the version you originally installed.
+    home.stateVersion = "24.05";
   };
-  # Set the wallpaper
-  stylix.image = ../modules/wallpapers/galaxy-waves.jpg;
-
-  home.sessionVariables.CUDA_CACHE_PATH = "\${HOME}/.cache/nv";
-
-  # The state version is required and should stay at the version you originally installed.
-  home.stateVersion = "24.05";
 }
