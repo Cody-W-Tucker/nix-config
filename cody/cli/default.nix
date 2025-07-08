b80b5597e956587ec885cc7e4d63873cdc1993ab
Cody W Tucker <cody@tmvsocial.com>
2025-07-08 15:00:56 -0500

    move color theme management to home-manager

--- cody/cli/default.nix
+++ cody/cli/default.nix
@@ -29,7 +29,6 @@
     # Stylix configuration
     stylix = {
       enable = true;
-      polarity = "dark";
       base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
       # Setting the fonts
       fonts = {
