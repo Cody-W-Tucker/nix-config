59bcae0d4dd0788447a4f9e706cb04033397f2e0
Cody Tucker <cody@tmvsocial.com>
2025-07-10 08:24:04 -0500

    improve monitor

--- flake.nix
+++ flake.nix
@@ -60,7 +60,7 @@
             "1, monitor:HDMI-A-3, default:true"
           ];
           monitor = [
-            "HDMI-A-3,2560x1440@240,0x0,1,bitdepth,10,cm,hdr"
+            "HDMI-A-3,2560x1440@240,0x0,1,bitdepth,10,cm,wide"
           ];
         };
         workstation = {
