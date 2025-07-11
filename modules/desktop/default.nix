3d1908f279023c83dd5d4db1e4749fc08c3a225b
Cody Tucker <cody@tmvsocial.com>
2025-07-10 21:52:28 -0500

    autologin

--- modules/desktop/default.nix
+++ modules/desktop/default.nix
@@ -6,7 +6,7 @@
   ];
   config = {
 
-    # services.displayManager.autoLogin.enable = true;
+    services.displayManager.autoLogin.enable = true;
 
     # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
     systemd.services."getty@tty1".enable = false;
