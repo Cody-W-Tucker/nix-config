01c06c0460c392f855242477f15429ebc33d82b7
Cody Tucker <cody@tmvsocial.com>
2025-07-12 12:42:14 -0500

    move AI to beast

--- modules/server/ai.nix
+++ modules/server/ai.nix
@@ -4,6 +4,8 @@ in
 {
   # Enable CUDA in containers
   hardware.nvidia-container-toolkit.enable = true;
+  systemd.enableUnifiedCgroupHierarchy = false;
+
   virtualisation.oci-containers.containers = {
     # docker run --name open-webui --add-host=host.docker.internal:host-gateway -e PORT=11435 -e OLLAMA_BASE_URL=http://server:11434 -v ~/open-webui:/app/backend/data ghcr.io/open-webui/open-webui:main
     "open-webui" = {
