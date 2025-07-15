f686e829e5d841c001a1e873adef39bb761b2394
Cody Tucker <cody@tmvsocial.com>
2025-07-14 19:51:30 -0500

    re-enable xwayland

--- cody/ui/hyprland.nix
+++ cody/ui/hyprland.nix
@@ -17,7 +17,7 @@
 
   wayland.windowManager.hyprland = {
     enable = true;
-    xwayland = true;
+    xwayland.enable = true;
     systemd.enable = false;
   };
   # Lockscreen: blurs after 15mins with another 15mins grace, then turns monitor off and locks
