9edefba95e92b839978cadc27204b68defcb8c80
Cody Tucker <cody@tmvsocial.com>
2025-07-18 19:05:23 -0500

    Move Git configuration from cli.nix to default.nix

--- cody/cli/default.nix
+++ cody/cli/default.nix
@@ -42,6 +42,51 @@
       enableZshIntegration = true;
     };
 
+    # Git configuration
+    programs.git = {
+      enable = true;
+
+      # Basic user configuration
+      userName = "Cody Tucker";
+      userEmail = "cody@tmvsocial.com";
+
+      # Useful aliases
+      aliases = {
+        st = "status";
+        co = "checkout";
+        br = "branch";
+        ci = "commit";
+        ca = "commit -a";
+        cm = "commit -m";
+        unstage = "reset HEAD --";
+        last = "log -1 HEAD";
+        visual = "!gitk";
+        lg = "log --oneline --graph --decorate";
+        lga = "log --oneline --graph --decorate --all";
+      };
+
+      # Global gitignore
+      ignores = [
+        ".DS_Store"
+        "*.swp"
+        "*.swo"
+        "*~"
+        ".nix-shell"
+      ];
+
+      # Additional configuration
+      extraConfig = {
+        init.defaultBranch = "main";
+        pull.rebase = true;
+        push.autoSetupRemote = true;
+        core.editor = "nvim";
+        color.ui = "auto";
+        color.branch = "auto";
+        color.diff = "auto";
+        color.status = "auto";
+      };
+    };
+
     sops.secrets.OPENAI_API_KEY = { };
 
     programs = {
