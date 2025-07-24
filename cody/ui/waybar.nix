ae8b725aa6a9caabcbf9a49e1e8ec24d7563ef1e
Cody Tucker <cody@tmvsocial.com>
2025-07-24 14:17:10 -0500

    waybar changes

--- cody/ui/waybar.nix
+++ cody/ui/waybar.nix
@@ -300,14 +300,6 @@ in
         color: #${config.lib.stylix.colors.base02};
       }
 
-      .module,
-      .module label,
-      .module span,
-      .module button,
-      .module * {
-        color: inherit;
-      }
-
       #workspaces {
         background-color: transparent;
         border: none;
