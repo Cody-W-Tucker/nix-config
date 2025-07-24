4815e8b7ceea1aec5a84eb803f3cd975f465faed
Cody Tucker <cody@tmvsocial.com>
2025-07-24 11:20:48 -0500

    waybar changes

--- cody/ui/waybar.nix
+++ cody/ui/waybar.nix
@@ -63,12 +63,8 @@ let
       ];
     };
     pulseaudio = {
-      format = "{volume}% {icon} {format_source}";
-      format-bluetooth = "{volume}% {icon} {format_source}";
-      format-bluetooth-muted = " {icon} {format_source}";
-      format-muted = " {format_source}";
-      format-source = "{volume}% ";
-      format-source-muted = "";
+      format = "{volume}% {icon}";
+      format-bluetooth = "{volume}% {icon}";
       ss = {
         headphone = "";
         hands-free = "";
