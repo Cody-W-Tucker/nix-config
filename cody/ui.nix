27a2ebb9681e257f7f7d777a015b97c111a76d36
Cody Tucker <cody@tmvsocial.com>
2025-07-25 18:37:24 -0500

    move vars to UWSM

--- cody/ui.nix
+++ cody/ui.nix
@@ -54,8 +54,6 @@
       image = ../modules/wallpapers/galaxy-waves.jpg;
     };
 
-    home.sessionVariables.CUDA_CACHE_PATH = "\${HOME}/.cache/nv";
-
     # The state version is required and should stay at the version you originally installed.
     home.stateVersion = "24.05";
   };
