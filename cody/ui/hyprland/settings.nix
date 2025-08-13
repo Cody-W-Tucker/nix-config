31a0d921cad3a15357172459381311d6255ff3b2
Cody Tucker <cody@tmvsocial.com>
2025-08-12 22:30:49 -0500

    streamline and remove redundant code

--- cody/ui/hyprland/settings.nix
+++ cody/ui/hyprland/settings.nix
@@ -40,6 +40,7 @@ let
     "${mainMod}, V, exec, cliphist list | rofi -dmenu | cliphist decode | wl-copy"
     "${mainMod} SHIFT, Tab, exec, web-search"
     "${mainMod}, BackSpace, exec, rofi -show calc -modi calc -no-show-match -no-sort -calc-command 'echo -n '{result}' | wl-copy'"
+    "${mainMod}, T, exec, ${terminal} -e taskwarrior-tui"
 
     # Screenshots
     ''${mainMod}, S, exec, screenshot-ocr''
--- modules/desktop/nvidia.nix
+++ modules/desktop/nvidia.nix
@@ -5,6 +5,7 @@
     # Enable OpenGL
     hardware.graphics = {
       enable = true;
+      enable32Bit = true;
       extraPackages = with pkgs; [
         nvidia-vaapi-driver
         vaapiVdpau
