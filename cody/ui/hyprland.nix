c76c82a0be3387a6ab0293540253d88937c888ad
Cody Tucker <cody@tmvsocial.com>
2025-08-12 22:02:48 -0500

    removed outdated option from hyprlock

--- cody/ui/hyprland.nix
+++ cody/ui/hyprland.nix
@@ -101,7 +101,7 @@
       ];
       input-field = lib.mkForce [
         {
-          monitor = "DP-1";
+          monitor = "";
           size = "200, 50";
           outline_thickness = 3;
           dots_size = 0.33;
@@ -111,7 +111,7 @@
           outer_color = "rgb(151515)";
           inner_color = "rgb(200, 200, 200)";
           font_color = "rgb(10, 10, 10)";
-          fade_on_empty = true;
+          fade_on_empty = false;
           fade_timeout = 1000;
           placeholder_text = "<i>Input Password...</i>";
           hide_input = false;
@@ -119,7 +119,6 @@
           check_color = "rgb(204, 136, 34)";
           fail_color = "rgb(204, 34, 34)";
           fail_text = "<i>$FAIL <b>($ATTEMPTS)</b></i>";
-          fail_transition = 300;
           capslock_color = -1;
           numlock_color = -1;
           bothlock_color = -1;
