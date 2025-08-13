7ac85d44287d2ad2c34e638d664955522b0824cc
Cody Tucker <cody@tmvsocial.com>
2025-08-12 23:01:08 -0500

    change lock cmd

--- cody/ui/hyprland.nix
+++ cody/ui/hyprland.nix
@@ -76,7 +76,7 @@
     systemd.enable = false;
     xwayland.enable = true;
   };
-  # Lockscreen: blurs after 15mins with another 15mins grace, then turns monitor off and locks
+  # Lock screen
   programs.hyprlock = {
     enable = true;
     settings = {
@@ -134,7 +134,7 @@
     settings = {
       general = {
         lock_cmd = "pidof hyprlock || hyprlock";
-        before_sleep_cmd = "pidof hyprlock || hyprlock";
+        before_sleep_cmd = "loginctl lock-session";
         after_sleep_cmd = "hyprctl dispatch dpms on";
       };
 
