bdd7681b37a5d88a9c0457ce4cf4d63f46570ce8
Cody Tucker <cody@tmvsocial.com>
2025-08-12 22:51:49 -0500

    change lock cmd

--- cody/ui/hyprland.nix
+++ cody/ui/hyprland.nix
@@ -134,14 +134,14 @@
     settings = {
       general = {
         lock_cmd = "pidof hyprlock || hyprlock";
-        before_sleep_cmd = "loginctl lock-session";
+        before_sleep_cmd = "pidof hyprlock || hyprlock";
         after_sleep_cmd = "hyprctl dispatch dpms on";
       };
 
       listener = [
         {
           timeout = 900; # 15min.
-          on-timeout = "loginctl lock-session";
+          on-timeout = "pidof hyprlock || hyprlock";
         }
         {
           timeout = 1800; # 30min.
