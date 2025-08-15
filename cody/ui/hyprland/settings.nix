28272d737e91f6da8dad0edf59b7066419adc44a
Cody Tucker <cody@tmvsocial.com>
2025-08-15 16:32:22 -0500

    add youtube WebApp

--- cody/ui/hyprland/settings.nix
+++ cody/ui/hyprland/settings.nix
@@ -28,7 +28,7 @@ let
     "${mainMod} SHIFT, Return, exec, [workspace special:ai] ${webApp}=https://grok.com/"
 
     # Media
-    "${mainMod}, Y, exec, [workspace special:media] ${webApp}=https://www.youtube.com/"
+    "${mainMod}, Y, exec, [workspace special:media] uwsm app -- zen --new-window=https://www.youtube.com/"
 
     # Browser navigation
     "${mainMod}, X, exec, ${browser}=https://x.com/"
