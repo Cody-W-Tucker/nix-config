35811beddf655dedda70f52aff6a733116931cc5
Cody Tucker <cody@tmvsocial.com>
2025-08-16 16:47:02 -0500

    remove old scripts

--- modules/scripts/default.nix
+++ modules/scripts/default.nix
@@ -6,20 +6,13 @@ let
     ./rofi-web-launcher.nix
     ./media-player.nix
     ./bluetooth-switch.nix
-    ./todoist-rofi.nix
     ./update.nix
-    ./waybar-tasks.nix
-    ./rofi-taskwarrior.nix
   ];
 
   scriptPackages = map (script: pkgs.callPackage (toString script) { inherit pkgs; }) scriptNames;
 in
 
 {
-  environment.systemPackages = with pkgs; [
-    jq # needed with web-search
-    inotify-tools # needed with waybar tasks
-    yt-dlp
-    # Adding the scripts to the system packages
-  ] ++ scriptPackages;
+  # Adding the scripts to the system packages
+  environment.systemPackages = scriptPackages;
 }
