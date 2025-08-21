b6915988da837b3074397461e7c9b6241ca147fa
Cody Tucker <cody@tmvsocial.com>
2025-08-21 17:03:56 -0500

    update nvidia and kernel

--- hosts/beast.nix
+++ hosts/beast.nix
@@ -33,8 +33,8 @@
   networking.useDHCP = lib.mkDefault true;
 
   # Use the latest kernel and matching NVIDIA driver
-  boot.kernelPackages = pkgs-unstable.linuxPackages_zen;
-  hardware.nvidia.package = lib.mkForce pkgs-unstable.linuxKernel.packages.linux_zen.nvidia_x11_beta;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+  hardware.nvidia.package = lib.mkForce config.boot.kernelPackages.nvidiaPackages.beta;
 
   # Ensure 14th Gen Intel CPU works correctly
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
