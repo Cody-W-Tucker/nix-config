9e81f0d08fee1fb4a50120a5fe4044503d7d4da5
Cody Tucker <cody@tmvsocial.com>
2025-08-21 10:07:07 -0500

    overlay kdenlive with speech plugin support

--- modules/overlays/kdenlive.nix
+++ modules/overlays/kdenlive.nix
@@ -1,40 +1,38 @@
 { pkgs, ... }:
 
-let
-  srt_equalizer = pkgs.python3Packages.buildPythonPackage rec {
-    pname = "srt_equalizer";
-    version = "0.1.10";
-    pyproject = true;
-    src = pkgs.fetchPypi {
-      inherit pname version;
-      sha256 = "sha256-X2sbLEixK7HKqxOCLX3dClSod3K4JKCqK6ZMAz03k1M=";
-    };
-    doCheck = false;
-    nativeBuildInputs = [ pkgs.python3Packages.poetry-core ];
-    propagatedBuildInputs = [ pkgs.python3Packages.srt ];
-  };
-
-  customPython = pkgs.python3.withPackages (ps: with ps; [
-    pip
-    openai-whisper
-    srt
-    srt_equalizer
-    (torch.override { cudaSupport = true; }) # Enable CUDA for GPU
-  ]);
-in
-
 {
-  # Overlay to enable speech recognition in Kdenlive
+  # Overlay to enable speech recognition in Kdenlive with CUDA support
   nixpkgs.overlays = [
-    (final: prev: {
+    (final: prev: rec {
+      python3Packages = prev.python3Packages.overrideScope (pyself: pysuper: {
+        srt_equalizer = pysuper.buildPythonPackage rec {
+          pname = "srt_equalizer";
+          version = "0.1.10";
+          pyproject = true;
+          src = prev.fetchPypi {
+            inherit pname version;
+            sha256 = "sha256-X2sbLEixK7HKqxOCLX3dClSod3K4JKCqK6ZMAz03k1M=";
+          };
+          doCheck = false;
+          nativeBuildInputs = [ pysuper.poetry-core ];
+          propagatedBuildInputs = [ pysuper.srt ];
+        };
+        torch = pysuper.torch-bin; # Use prebuilt binary with CUDA support to avoid building from source
+      });
+      customPython = final.python3.withPackages (ps: with ps; [
+        pip
+        openai-whisper
+        srt
+        srt_equalizer
+        torch
+      ]);
       kdePackages = prev.kdePackages.overrideScope (kfinal: kprev: {
         kdenlive = kprev.kdenlive.overrideAttrs (oldAttrs: {
           nativeBuildInputs = (oldAttrs.nativeBuildInputs or [ ]) ++ [ final.makeWrapper ];
-
           postFixup = (oldAttrs.postFixup or "") + ''
             wrapProgram $out/bin/kdenlive \
               --prefix PATH : ${final.lib.makeBinPath [ customPython final.cudaPackages.cudatoolkit ]} \
-              --prefix LD_LIBRARY_PATH : ${final.lib.makeLibraryPath [ final.cudaPackages.cudatoolkit final.stdenv.cc.cc.lib ]}
+              --prefix LD_LIBRARY_PATH : ${final.lib.makeLibraryPath [ final.cudaPackages.cudatoolkit final.cudaPackages.cudnn final.cudaPackages.libcublas final.stdenv.cc.cc.lib ]}
           '';
         });
       });
@@ -42,8 +40,7 @@ in
   ];
 
   # Machine specific packages
-  environment.systemPackages =
-    (with pkgs; [
-      kdePackages.kdenlive
-    ]);
+  environment.systemPackages = with pkgs; [
+    kdePackages.kdenlive
+  ];
 }
