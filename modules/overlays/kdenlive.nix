43e433b1b3bee7d87f5d5a24e0e4571531ecea45
Cody Tucker <cody@tmvsocial.com>
2025-08-21 10:10:43 -0500

    overlay kdenlive with speech plugin support

--- modules/overlays/kdenlive.nix
+++ modules/overlays/kdenlive.nix
@@ -4,22 +4,24 @@
   # Overlay to enable speech recognition in Kdenlive with CUDA support
   nixpkgs.overlays = [
     (final: prev: rec {
-      python3Packages = prev.python3Packages.overrideScope (pyself: pysuper: {
-        srt_equalizer = pysuper.buildPythonPackage rec {
-          pname = "srt_equalizer";
-          version = "0.1.10";
-          pyproject = true;
-          src = prev.fetchPypi {
-            inherit pname version;
-            sha256 = "sha256-X2sbLEixK7HKqxOCLX3dClSod3K4JKCqK6ZMAz03k1M=";
+      myPython = prev.python3.override {
+        packageOverrides = pyself: pysuper: {
+          srt_equalizer = pysuper.buildPythonPackage rec {
+            pname = "srt_equalizer";
+            version = "0.1.10";
+            pyproject = true;
+            src = prev.fetchPypi {
+              inherit pname version;
+              sha256 = "sha256-X2sbLEixK7HKqxOCLX3dClSod3K4JKCqK6ZMAz03k1M=";
+            };
+            doCheck = false;
+            nativeBuildInputs = [ pysuper.poetry-core ];
+            propagatedBuildInputs = [ pysuper.srt ];
           };
-          doCheck = false;
-          nativeBuildInputs = [ pysuper.poetry-core ];
-          propagatedBuildInputs = [ pysuper.srt ];
+          torch = pysuper.torch-bin; # Use prebuilt binary with CUDA support to avoid building from source
         };
-        torch = pysuper.torch-bin; # Use prebuilt binary with CUDA support to avoid building from source
-      });
-      customPython = final.python3.withPackages (ps: with ps; [
+      };
+      customPython = myPython.withPackages (ps: with ps; [
         pip
         openai-whisper
         srt
