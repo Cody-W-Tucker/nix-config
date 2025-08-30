8ff613f4a58962b207de6e48ee2f36316e9b5cd0
Cody Tucker <cody@tmvsocial.com>
2025-08-29 19:04:59 -0500

    upgrade flake

--- hosts/beast.nix
+++ hosts/beast.nix
@@ -1,4 +1,4 @@
-{ config, lib, pkgs, pkgs-unstable, ... }:
+{ config, lib, pkgs, ... }:
 
 {
   imports =
@@ -36,8 +36,8 @@
   networking.useDHCP = lib.mkDefault true;
 
   # Use the latest kernel and matching NVIDIA driver
-  boot.kernelPackages = pkgs-unstable.linuxPackages_zen;
-  hardware.nvidia.package = lib.mkForce pkgs-unstable.linuxKernel.packages.linux_zen.nvidia_x11_beta;
+  boot.kernelPackages = pkgs.linuxPackages_zen;
+  hardware.nvidia.package = lib.mkForce config.boot.kernelPackages.nvidiaPackages.production;
 
   # Ensure 14th Gen Intel CPU works correctly
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
