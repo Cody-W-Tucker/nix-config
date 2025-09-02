7d7e18832b17dc45f07b70097d619c18c7764796
Cody Tucker <cody@tmvsocial.com>
2025-09-02 13:32:43 -0500

    simplify animation for windows

--- cody/ui/hyprland/settings.nix
+++ cody/ui/hyprland/settings.nix
@@ -145,9 +145,9 @@ in
         "easeOutExpo, 0.16, 1, 0.3, 1"
       ];
       animation = [
-        "windows, 1, 1, easeInExpo, slide 80%"
-        "windowsIn, 1, 1, easeInExpo, slide"
-        "windowsOut, 1, 1, easeOutExpo, slide"
+        "windows, 1, 1, easeInExpo, slide"
+        "windowsIn, 1, 1, easeInExpo, slide 80%"
+        "windowsOut, 1, 1, easeOutExpo, slide 80%"
         "border, 1, 10, default"
         "borderangle, 1, 8, default"
         "fade, 1, 2, default"
