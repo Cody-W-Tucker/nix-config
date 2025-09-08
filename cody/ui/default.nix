23c341937ba939d9815a512e4f196da0154bfe8e
Cody Tucker <cody@tmvsocial.com>
2025-09-08 10:13:40 -0500

    enable obs virtual camera

--- cody/ui/default.nix
+++ cody/ui/default.nix
@@ -1,4 +1,4 @@
-{ config, inputs, pkgs, pkgs-unstable, ... }:
+{ inputs, pkgs, pkgs-unstable, ... }:
 
 {
   imports = [
@@ -79,15 +79,6 @@
     enableVirtualCamera = true;
     package = (pkgs-unstable.obs-studio.override { cudaSupport = true; });
   };
-
-  # v4l2loopback for virtual camera
-  boot.extraModulePackages = with config.boot.kernelPackages; [
-    v4l2loopback
-  ];
-  boot.kernelModules = [ "v4l2loopback" ];
-  boot.extraModprobeConfig = ''
-    options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
-  '';
 
   # Clipboard history
   services.cliphist = {
