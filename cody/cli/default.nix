fe1bc71fad0e4de9dbb35cde4f06362feb4acbfa
Cody Tucker <cody@tmvsocial.com>
2025-09-25 10:07:57 -0500

    revert: move opencode to unstable

--- cody/cli/default.nix
+++ cody/cli/default.nix
@@ -12,11 +12,12 @@
       fastfetch
       unzip
       zip
+      opencode
     ])
     ++
     (with pkgs-unstable; [
       # list of unstable packages go here
-      opencode
+      gemini-cli
     ]);
 
   home.sessionVariables = {
