7ca4b7c9cfcff17bd7de1a4371396b10b6cabb89
Cody Tucker <cody@tmvsocial.com>
2025-10-09 14:44:56 -0500

    keybinds for telescope

--- cody/cli/nixvim.nix
+++ cody/cli/nixvim.nix
@@ -38,6 +38,32 @@
         enable = true;
       };
     };
+    keymaps = [
+      {
+        mode = "n"; # Normal mode
+        key = "<Leader>ff";
+        action = "<cmd>Telescope find_files<CR>";
+        options = { silent = true; };
+      }
+      {
+        mode = "n";
+        key = "<Leader>fg";
+        action = "<cmd>Telescope live_grep<CR>";
+        options = { silent = true; };
+      }
+      {
+        mode = "n";
+        key = "<Leader>fb";
+        action = "<cmd>Telescope buffers<CR>";
+        options = { silent = true; };
+      }
+      {
+        mode = "n";
+        key = "<Leader>fh";
+        action = "<cmd>Telescope help_tags<CR>";
+        options = { silent = true; };
+      }
+    ];
     extraPlugins = with pkgs.vimPlugins; [
       vim-pencil
     ];
