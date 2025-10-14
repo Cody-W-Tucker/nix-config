1682e876cf6c6ccac7c1085ee541cfaad8a9b4cd
Cody Tucker <cody@tmvsocial.com>
2025-10-14 10:10:01 -0500

    rename hoarder to karakeep and update images

--- modules/server/karakeep.nix
+++ modules/server/karakeep.nix
@@ -3,7 +3,7 @@
 
 {
   services.nginx.virtualHosts = {
-    "hoarder.homehub.tv" = {
+    "karakeep.homehub.tv" = {
       forceSSL = true;
       useACMEHost = "homehub.tv";
       locations."/".proxyPass = "http://localhost:2468";
@@ -12,17 +12,17 @@
   };
 
   # Containers
-  virtualisation.oci-containers.containers."hoarder-chrome" = {
-    image = "gcr.io/zenika-hub/alpine-chrome:123";
+  virtualisation.oci-containers.containers."karakeep-chrome" = {
+    image = "gcr.io/zenika-hub/alpine-chrome:124";
     cmd = [ "--no-sandbox" "--disable-gpu" "--disable-dev-shm-usage" "--remote-debugging-address=0.0.0.0" "--remote-debugging-port=9222" "--hide-scrollbars" ];
     log-driver = "journald";
     extraOptions = [
       "--network-alias=chrome"
-      "--network=hoarder_default"
+      "--network=karakeep_default"
       "--pull=always"
     ];
   };
-  systemd.services."docker-hoarder-chrome" = {
+  systemd.services."docker-karakeep-chrome" = {
     serviceConfig = {
       Restart = lib.mkOverride 90 "always";
       RestartMaxDelaySec = lib.mkOverride 90 "1m";
@@ -30,16 +30,16 @@
       RestartSteps = lib.mkOverride 90 9;
     };
     after = [
-      "docker-network-hoarder_default.service"
+      "docker-network-karakeep_default.service"
     ];
     requires = [
-      "docker-network-hoarder_default.service"
+      "docker-network-karakeep_default.service"
     ];
     partOf = [
-      "docker-compose-hoarder-root.target"
+      "docker-compose-karakeep-root.target"
     ];
     wantedBy = [
-      "docker-compose-hoarder-root.target"
+      "docker-compose-karakeep-root.target"
     ];
   };
 
@@ -60,27 +60,27 @@
     '';
   };
 
-  virtualisation.oci-containers.containers."hoarder-meilisearch" = {
-    image = "getmeili/meilisearch:v1.11.1";
+  virtualisation.oci-containers.containers."karakeep-meilisearch" = {
+    image = "getmeili/meilisearch:v1.13.3";
     environmentFiles = [
       config.sops.templates."MEILI_and_NEXTAUTH".path
     ];
     environment = {
-      "HOARDER_VERSION" = "release";
+      "KARAKEEP_VERSION" = "release";
       "NEXTAUTH_URL" = "http://localhost:3000";
       "MEILI_NO_ANALYTICS" = "true";
     };
     volumes = [
-      "hoarder_meilisearch:/meili_data:rw"
+      "karakeep_meilisearch:/meili_data:rw"
     ];
     log-driver = "journald";
     extraOptions = [
       "--network-alias=meilisearch"
-      "--network=hoarder_default"
+      "--network=karakeep_default"
       "--pull=always"
     ];
   };
-  systemd.services."docker-hoarder-meilisearch" = {
+  systemd.services."docker-karakeep-meilisearch" = {
     serviceConfig = {
       Restart = lib.mkOverride 90 "always";
       RestartMaxDelaySec = lib.mkOverride 90 "1m";
@@ -88,22 +88,22 @@
       RestartSteps = lib.mkOverride 90 9;
     };
     after = [
-      "docker-network-hoarder_default.service"
-      "docker-volume-hoarder_meilisearch.service"
+      "docker-network-karakeep_default.service"
+      "docker-volume-karakeep_meilisearch.service"
     ];
     requires = [
-      "docker-network-hoarder_default.service"
-      "docker-volume-hoarder_meilisearch.service"
+      "docker-network-karakeep_default.service"
+      "docker-volume-karakeep_meilisearch.service"
     ];
     partOf = [
-      "docker-compose-hoarder-root.target"
+      "docker-compose-karakeep-root.target"
     ];
     wantedBy = [
-      "docker-compose-hoarder-root.target"
+      "docker-compose-karakeep-root.target"
     ];
   };
-  virtualisation.oci-containers.containers."hoarder-web" = {
-    image = "ghcr.io/hoarder-app/hoarder:release";
+  virtualisation.oci-containers.containers."karakeep-web" = {
+    image = "ghcr.io/karakeep-app/karakeep:release";
     environmentFiles = [
       config.sops.templates."MEILI_and_NEXTAUTH".path
       config.sops.templates."OPENAI_API".path
@@ -111,12 +111,12 @@
     environment = {
       "BROWSER_WEB_URL" = "http://chrome:9222";
       "DATA_DIR" = "/data";
-      "HOARDER_VERSION" = "release";
+      "KARAKEEP_VERSION" = "release";
       "MEILI_ADDR" = "http://meilisearch:7700";
       "NEXTAUTH_URL" = "http://localhost:3000";
     };
     volumes = [
-      "hoarder_data:/data:rw"
+      "karakeep_data:/data:rw"
     ];
     ports = [
       "2468:3000/tcp"
@@ -124,11 +124,11 @@
     log-driver = "journald";
     extraOptions = [
       "--network-alias=web"
-      "--network=hoarder_default"
+      "--network=karakeep_default"
       "--pull=always"
     ];
   };
-  systemd.services."docker-hoarder-web" = {
+  systemd.services."docker-karakeep-web" = {
     serviceConfig = {
       Restart = lib.mkOverride 90 "always";
       RestartMaxDelaySec = lib.mkOverride 90 "1m";
@@ -136,66 +136,66 @@
       RestartSteps = lib.mkOverride 90 9;
     };
     after = [
-      "docker-network-hoarder_default.service"
-      "docker-volume-hoarder_data.service"
+      "docker-network-karakeep_default.service"
+      "docker-volume-karakeep_data.service"
     ];
     requires = [
-      "docker-network-hoarder_default.service"
-      "docker-volume-hoarder_data.service"
+      "docker-network-karakeep_default.service"
+      "docker-volume-karakeep_data.service"
     ];
     partOf = [
-      "docker-compose-hoarder-root.target"
+      "docker-compose-karakeep-root.target"
     ];
     wantedBy = [
-      "docker-compose-hoarder-root.target"
+      "docker-compose-karakeep-root.target"
     ];
   };
 
   # Networks
-  systemd.services."docker-network-hoarder_default" = {
+  systemd.services."docker-network-karakeep_default" = {
     path = [ pkgs.docker ];
     serviceConfig = {
       Type = "oneshot";
       RemainAfterExit = true;
-      ExecStop = "docker network rm -f hoarder_default";
+      ExecStop = "docker network rm -f karakeep_default";
     };
     script = ''
-      docker network inspect hoarder_default || docker network create hoarder_default
+      docker network inspect karakeep_default || docker network create karakeep_default
     '';
-    partOf = [ "docker-compose-hoarder-root.target" ];
-    wantedBy = [ "docker-compose-hoarder-root.target" ];
+    partOf = [ "docker-compose-karakeep-root.target" ];
+    wantedBy = [ "docker-compose-karakeep-root.target" ];
   };
 
   # Volumes
-  systemd.services."docker-volume-hoarder_data" = {
+  systemd.services."docker-volume-karakeep_data" = {
     path = [ pkgs.docker ];
     serviceConfig = {
       Type = "oneshot";
       RemainAfterExit = true;
     };
     script = ''
-      docker volume inspect hoarder_data || docker volume create hoarder_data
+      docker volume inspect karakeep_data || docker volume create karakeep_data
     '';
-    partOf = [ "docker-compose-hoarder-root.target" ];
-    wantedBy = [ "docker-compose-hoarder-root.target" ];
+    partOf = [ "docker-compose-karakeep-root.target" ];
+    wantedBy = [ "docker-compose-karakeep-root.target" ];
   };
-  systemd.services."docker-volume-hoarder_meilisearch" = {
+  systemd.services."docker-volume-karakeep_meilisearch" = {
     path = [ pkgs.docker ];
     serviceConfig = {
       Type = "oneshot";
       RemainAfterExit = true;
     };
     script = ''
-      docker volume inspect hoarder_meilisearch || docker volume create hoarder_meilisearch
+      docker volume inspect karakeep_meilisearch || docker volume create karakeep_meilisearch
     '';
-    partOf = [ "docker-compose-hoarder-root.target" ];
-    wantedBy = [ "docker-compose-hoarder-root.target" ];
+    partOf = [ "docker-compose-karakeep-root.target" ];
+    wantedBy = [ "docker-compose-karakeep-root.target" ];
   };
 
   # Root service
   # When started, this will automatically create all resources and start
   # the containers. When stopped, this will teardown all resources.
-  systemd.targets."docker-compose-hoarder-root" = {
+  systemd.targets."docker-compose-karakeep-root" = {
     unitConfig = {
       Description = "Root target generated by compose2nix.";
     };
