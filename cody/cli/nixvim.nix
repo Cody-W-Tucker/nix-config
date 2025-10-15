3f9eda917c5558fdda3acee402eb3a39f30416f8
Cody Tucker <cody@tmvsocial.com>
2025-10-15 15:33:25 -0500

    add troubleshooting keybinds for lsp

--- cody/cli/nixvim.nix
+++ cody/cli/nixvim.nix
@@ -86,6 +86,23 @@
               action = "rename";
               desc = "Rename";
             };
+            # Add diagnostic keymaps:
+            "<leader>do" = {
+              action = "lua vim.diagnostic.open_float()";
+              desc = "Show Diagnostics";
+            };
+            "<leader>d[" = {
+              action = "lua vim.diagnostic.goto_prev()";
+              desc = "Previous Diagnostic";
+            };
+            "<leader>d]" = {
+              action = "lua vim.diagnostic.goto_next()";
+              desc = "Next Diagnostic";
+            };
+            "<leader>dd" = {
+              action = "Telescope diagnostics";
+              desc = "List All Diagnostics";
+            };
           };
         };
       };
