e7dc51204b78d126cda2c373a8db806a1ef54760
Cody Tucker <cody@tmvsocial.com>
2025-10-15 15:51:23 -0500

    add troubleshooting keybinds for lsp

--- cody/cli/nixvim.nix
+++ cody/cli/nixvim.nix
@@ -62,34 +62,16 @@
         keymaps = {
           silent = true;
           lspBuf = {
-            gd = {
-              action = "definition";
-              desc = "Goto Definition";
-            };
-            gr = {
-              action = "references";
-              desc = "Goto References";
-            };
-            gD = {
-              action = "declaration";
-              desc = "Goto Declaration";
-            };
-            gI = {
-              action = "implementation";
-              desc = "Goto Implementation";
-            };
-            gT = {
-              action = "type_definition";
-              desc = "Type Definition";
-            };
-            "<leader>cr" = {
-              action = "rename";
-              desc = "Rename";
-            };
+            gd = "definition";
+            gD = "references";
+            gt = "type_definition";
+            gi = "implementation";
+            K = "hover";
+            re = "rename";
           };
           diagnostic = {
-            "<leader>j" = "goto_next";
             "<leader>k" = "goto_prev";
+            "<leader>j" = "goto_next";
           };
         };
       };
