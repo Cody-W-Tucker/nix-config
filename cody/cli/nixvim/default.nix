b0d83a3ae65326982ac0de9465ee6417922c85b9
Cody Tucker <cody@tmvsocial.com>
2025-10-16 16:09:22 -0500

    enable lsp-format

--- cody/cli/nixvim/default.nix
+++ cody/cli/nixvim/default.nix
@@ -3,6 +3,7 @@
 {
   imports = [
     ./startup.nix
+    ./keymaps.nix
   ];
 
   programs.nixvim = {
@@ -102,6 +103,15 @@
           };
         };
       };
+      lsp-format.enable = true;
+      none-ls = {
+        enable = true;
+        enableLspFormat = true;
+        sources.formatting = {
+          nixfmt.enable = true;
+          black.enable = true;
+        };
+      };
       # Enable completion
       cmp = {
         enable = true;
@@ -122,15 +132,6 @@
           ];
         };
       };
-      lsp-format.enable = true;
-      none-ls = {
-        enable = true;
-        enableLspFormat = true;
-        sources.formatting = {
-          nixfmt.enable = true;
-          black.enable = true;
-        };
-      };
       direnv.enable = true;
       twilight = {
         enable = true;
@@ -144,56 +145,6 @@
     globals = {
       mapleader = " ";
     };
-    keymaps = [
-      {
-        mode = "n"; # Normal mode
-        key = "<Leader>ff";
-        action = "<cmd>Telescope find_files<CR>";
-        options = {
-          silent = true;
-        };
-      }
-      {
-        mode = "n";
-        key = "<Leader>fg";
-        action = "<cmd>Telescope live_grep<CR>";
-        options = {
-          silent = true;
-        };
-      }
-      {
-        mode = "n";
-        key = "<Leader>fb";
-        action = "<cmd>Telescope buffers<CR>";
-        options = {
-          silent = true;
-        };
-      }
-      {
-        mode = "n";
-        key = "<Leader>fh";
-        action = "<cmd>Telescope help_tags<CR>";
-        options = {
-          silent = true;
-        };
-      }
-      {
-        mode = "n";
-        key = "<Leader>-";
-        action = "<cmd>Yazi<CR>";
-        options = {
-          silent = true;
-        };
-      }
-      {
-        mode = "n";
-        key = "<Leader>==";
-        action = "<cmd>Yazi cwd<CR>";
-        options = {
-          silent = true;
-        };
-      }
-    ];
     extraPlugins = with pkgs.vimPlugins; [ vim-pencil ];
   };
 }
