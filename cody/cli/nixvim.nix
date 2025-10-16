4c36fda4a959a225616b3392256bfc3be70a2405
Cody Tucker <cody@tmvsocial.com>
2025-10-16 12:08:02 -0500

    add statup

--- cody/cli/nixvim.nix
+++ cody/cli/nixvim.nix
@@ -26,7 +26,66 @@
     ];
     plugins = {
       snacks.enable = true;
-      startup.enable = true;
+      startup = {
+        enable = true;
+        sections = {
+          body = {
+            align = "center";
+            content = [
+              [
+                " Find File"
+                "Telescope find_files"
+                "<leader>ff"
+              ]
+              [
+                "󰍉 Find Word"
+                "Telescope live_grep"
+                "<leader>lg"
+              ]
+              [
+                " Recent Files"
+                "Telescope oldfiles"
+                "<leader>of"
+              ]
+              [
+                " File Browser"
+                "Telescope file_browser"
+                "<leader>fb"
+              ]
+              [
+                " Colorschemes"
+                "Telescope colorscheme"
+                "<leader>cs"
+              ]
+              [
+                " New File"
+                "lua require'startup'.new_file()"
+                "<leader>nf"
+              ]
+            ];
+            defaultColor = "";
+            foldSection = true;
+            highlight = "String";
+            margin = 5;
+            oldfilesAmount = 0;
+            title = "Basic Commands";
+            type = "mapping";
+          };
+          header = {
+            align = "center";
+            content = {
+              __raw = "require('startup.headers').hydra_header";
+            };
+            defaultColor = "";
+            foldSection = false;
+            highlight = "Statement";
+            margin = 5;
+            oldfilesAmount = 0;
+            title = "Header";
+            type = "text";
+          };
+        };
+      };
       lualine = {
         enable = true;
         settings = {
