2b62b7ff2b88ca0a7848a458e4b9b339b81a139e
Cody Tucker <cody@tmvsocial.com>
2025-10-16 18:13:37 -0500

    add vim binding for zoxide

--- cody/cli/nixvim/default.nix
+++ cody/cli/nixvim/default.nix
@@ -55,6 +55,12 @@
       web-devicons.enable = true;
       telescope = {
         enable = true;
+        keymaps = {
+          "<leader>cd" = {
+            action = "zoxide list";
+            options.desc = "Change directory with Zoxide"; # Optional: for which-key integration
+          };
+        };
         extensions.zoxide = {
           enable = true;
         };
