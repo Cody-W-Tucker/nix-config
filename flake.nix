ffe7606c66cfe0ca2bd56da9c97655b683ef465f
Cody Tucker <cody@tmvsocial.com>
2025-11-19 09:56:31 -0600

    Revert "add opencode flake"
    This reverts commit 26d58cc3715baffe53d53f7e370c23b9e2fe9163.

--- flake.nix
+++ flake.nix
@@ -36,7 +36,7 @@
       url = "github:Cody-W-Tucker/Qdrant-Upload";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    opencode.url = "github:sst/opencode/dev";
+    opencode.url = "github:sst/opencode";
   };
 
   outputs =
