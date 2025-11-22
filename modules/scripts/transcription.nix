98aa63f999f2602f215bde522587a172ad776905
Cody Tucker <cody@tmvsocial.com>
2025-11-22 12:51:54 -0600

    Revert "Fix transcription script packaging"
    This reverts commit 09fc5782eb83f2e5370e5af91f66bad4cdcadefb.

--- modules/scripts/transcription.nix
+++ modules/scripts/transcription.nix
@@ -48,4 +48,14 @@ let
     rm -r "$OUTPUT_DIR"
   '';
 in
-whisper-transcribe
+{
+  environment.systemPackages = with pkgs; [
+    openai-whisper
+    sox
+    xdotool
+    whisper-transcribe
+  ];
+
+  # Optional: Enable CUDA support if you have NVIDIA GPU
+  # hardware.nvidia.cudaSupport = true;
+}
