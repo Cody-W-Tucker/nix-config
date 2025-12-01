93aba46d1e853d5487182622e06a1f3bf1dcde3d
Cody Tucker <cody@tmvsocial.com>
2025-11-30 19:59:22 -0600

    Revert "enable bluelight filter"
    This reverts commit 5dd0f6cd6cce81408f2647db97d697b99ad81886.

--- cody/ui/hyprland.nix
+++ cody/ui/hyprland.nix
@@ -30,26 +30,6 @@
   # Use the Hyprland Polkit
   services.hyprpolkitagent.enable = true;
 
-  # Use blue light filter from hyprland
-  services.hyprsunset = {
-    enable = true;
-    settings = {
-      max-gamma = 150;
-
-      profile = [
-        {
-          time = "7:30";
-          identity = true;
-        }
-        {
-          time = "21:00";
-          temperature = 5000;
-          gamma = 0.8;
-        }
-      ];
-    };
-  };
-
   wayland.windowManager.hyprland = {
     enable = true;
     # set the Hyprland and XDPH packages to null to use the ones from the NixOS module
