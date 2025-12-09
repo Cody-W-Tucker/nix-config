679984907e3f7df73c312117028c8a08e2784a20
Cody Tucker <cody@tmvsocial.com>
2025-12-08 20:16:29 -0600

    use conform plugin

--- cody/cli/nixvim/default.nix
+++ cody/cli/nixvim/default.nix
@@ -6,6 +6,7 @@
     ./keymaps.nix
     ./plugins/lsp.nix
     ./plugins/none-ls.nix
+    ./plugins/conform.nix
   ];
 
   programs.nixvim = {
@@ -110,66 +111,6 @@
           astro
           tsx
         ];
-      };
-      conform-nvim = {
-        enable = true;
-        settings = {
-          format_on_save = {
-            timeoutMs = 500;
-            lspFallback = true;
-          };
-          formatters_by_ft = {
-            astro = [ "prettier" ];
-            html = [
-              [
-                "prettierd"
-                "prettier"
-              ]
-            ];
-            css = [
-              [
-                "prettierd"
-                "prettier"
-              ]
-            ];
-            javascript = [
-              [
-                "prettierd"
-                "prettier"
-              ]
-            ];
-            javascriptreact = [
-              [
-                "prettierd"
-                "prettier"
-              ]
-            ];
-            typescript = [
-              [
-                "prettierd"
-                "prettier"
-              ]
-            ];
-            typescriptreact = [
-              [
-                "prettierd"
-                "prettier"
-              ]
-            ];
-            python = [ "black" ];
-            nix = [ "nixfmt" ];
-            markdown = [
-              [
-                "prettierd"
-                "prettier"
-              ]
-            ];
-            yaml = [
-              "yamllint"
-              "yamlfmt"
-            ];
-          };
-        };
       };
       ts-autotag = {
         enable = true;
---
+++ cody/cli/nixvim/plugins/conform.nix
@@ -0,0 +1,64 @@
+{
+  programs.nixvim.plugins = {
+    conform-nvim = {
+      enable = true;
+      settings = {
+        format_on_save = {
+          timeoutMs = 500;
+          lspFallback = true;
+        };
+        formatters_by_ft = {
+          astro = [ "prettier" ];
+          html = [
+            [
+              "prettierd"
+              "prettier"
+            ]
+          ];
+          css = [
+            [
+              "prettierd"
+              "prettier"
+            ]
+          ];
+          javascript = [
+            [
+              "prettierd"
+              "prettier"
+            ]
+          ];
+          javascriptreact = [
+            [
+              "prettierd"
+              "prettier"
+            ]
+          ];
+          typescript = [
+            [
+              "prettierd"
+              "prettier"
+            ]
+          ];
+          typescriptreact = [
+            [
+              "prettierd"
+              "prettier"
+            ]
+          ];
+          python = [ "black" ];
+          nix = [ "nixfmt" ];
+          markdown = [
+            [
+              "prettierd"
+              "prettier"
+            ]
+          ];
+          yaml = [
+            "yamllint"
+            "yamlfmt"
+          ];
+        };
+      };
+    };
+  };
+}
