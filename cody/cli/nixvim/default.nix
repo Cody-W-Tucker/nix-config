f985ecf503c4ba93f01acee8baa1739ab402b247
Cody Tucker <cody@tmvsocial.com>
2025-12-08 20:03:25 -0600

    refactor lsp to plugin dir

--- cody/cli/nixvim/default.nix
+++ cody/cli/nixvim/default.nix
@@ -4,6 +4,7 @@
   imports = [
     ./startup.nix
     ./keymaps.nix
+    ./plugins/lsp.nix
   ];
 
   programs.nixvim = {
@@ -54,16 +55,6 @@
       which-key.enable = true;
       rainbow-delimiters.enable = true;
       snacks.enable = true;
-      autoclose = {
-        enable = true;
-        settings.options = {
-          auto_indent = true;
-          disabled_filetypes = [
-            "text"
-          ];
-        };
-      };
-      jupytext.enable = true;
       lualine = {
         enable = true;
         settings = {
@@ -118,58 +109,6 @@
           astro
           tsx
         ];
-      };
-      lsp-format.enable = true;
-      lsp = {
-        enable = true;
-        inlayHints = true;
-        servers = {
-          nixd.enable = true; # Nix
-          cssls.enable = true; # CSS
-          tailwindcss.enable = true; # TailwindCSS
-          html.enable = true; # HTML
-          pyright.enable = true; # Python
-          dockerls.enable = true; # Docker
-          bashls.enable = true; # Bash
-          markdown_oxide.enable = true; # markdown
-          # Keep ts_ls disabled or limited for .astro files – it conflicts
-          ts_ls = {
-            enable = true;
-            extraOptions = {
-              # Prevent ts_ls from handling .astro files
-              filetypes = [
-                "javascript"
-                "javascriptreact"
-                "typescript"
-                "typescriptreact"
-              ];
-            };
-          };
-          astro = {
-            enable = true;
-            # Critical: use project-local TypeScript (Astro requires this)
-            settings = {
-              typescript = {
-                tsdk = "./node_modules/typescript/lib";
-              };
-            };
-          };
-        };
-        keymaps = {
-          silent = true;
-          lspBuf = {
-            gd = "definition";
-            gD = "references";
-            gt = "type_definition";
-            gi = "implementation";
-            K = "hover";
-            re = "rename";
-          };
-          diagnostic = {
-            "<leader>k" = "goto_prev";
-            "<leader>j" = "goto_next";
-          };
-        };
       };
       none-ls = {
         enable = true;
