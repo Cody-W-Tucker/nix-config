4096a6debba081d8853fbbbe1f04c9a85b658d53
Cody Tucker <cody@tmvsocial.com>
2025-12-08 20:45:08 -0600

    use treesitter plugin

---
+++ cody/cli/nixvim/plugins/treesitter.nix
@@ -0,0 +1,30 @@
+{ pkgs, ... }:
+{
+  programs.nixvim.plugins.treesitter = {
+    enable = true;
+    settings = {
+      indent = {
+        enable = true;
+      };
+      highlight = {
+        enable = true;
+      };
+    };
+    grammarPackages = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
+      bash
+      json
+      nix
+      python
+      toml
+      yaml
+      javascript
+      typescript
+      html
+      css
+      lua
+      xml
+      astro
+      tsx
+    ];
+  };
+}
