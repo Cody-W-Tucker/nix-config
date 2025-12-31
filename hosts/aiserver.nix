3f64ea3705664d2ee8ebb4ea09be1c9c0fabac8e
Cody Tucker <cody@tmvsocial.com>
2025-12-31 16:11:44 -0600

    add aiserver to flake

---
+++ hosts/aiserver.nix
@@ -0,0 +1,51 @@
+{ config, lib, ... }:
+
+{
+  imports = [
+    # Include the results of the hardware scan.
+    ../configuration.nix
+  ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  networking.hostName = "ai_server";
+
+  boot.initrd.availableKernelModules = [
+    "nvme"
+    "xhci_pci"
+    "thunderbolt"
+    "usbhid"
+    "usb_storage"
+    "sd_mod"
+    "sdhci_pci"
+  ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-amd" ];
+  boot.extraModulePackages = [ ];
+
+  # Networking
+  networking.networkmanager.enable = true;
+
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/73998e5d-b64f-4148-bacb-af7b7883746a";
+    fsType = "ext4";
+  };
+
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/BD3E-2CD9";
+    fsType = "vfat";
+    options = [
+      "fmask=0077"
+      "dmask=0077"
+    ];
+  };
+
+  swapDevices = [ ];
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  system.stateVersion = "25.11"; # Did you read the comment?
+
+}
