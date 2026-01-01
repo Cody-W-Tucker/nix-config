acd9559cb4b78937351391ff5dabeef313799c69
Cody Tucker <cody@tmvsocial.com>
2025-12-31 19:00:03 -0600

    add drivers

--- hosts/aiserver.nix
+++ hosts/aiserver.nix
@@ -6,32 +6,34 @@
     ../configuration.nix
   ];
 
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.kernelParams = [
-    "amdgpu.vramlimit=0"
-    "amdgpu.gttlimit=0"
-  ];
-  networking.hostName = "ai_server";
+  # Bootloader
+  boot = {
+    loader.systemd-boot.enable = true;
+    loader.efi.canTouchEfiVariables = true;
+    kernelParams = [
+      "amdgpu.vramlimit=0"
+      "amdgpu.gttlimit=0"
+    ];
 
-  boot.initrd.availableKernelModules = [
-    "nvme"
-    "xhci_pci"
-    "thunderbolt"
-    "usbhid"
-    "usb_storage"
-    "sd_mod"
-    "sdhci_pci"
-  ];
-  boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-amd" ];
-  boot.extraModulePackages = [ ];
+    initrd.availableKernelModules = [
+      "nvme"
+      "xhci_pci"
+      "thunderbolt"
+      "usbhid"
+      "usb_storage"
+      "sd_mod"
+      "sdhci_pci"
+    ];
+    initrd.kernelModules = [ ];
+    kernelModules = [ "kvm-amd" ];
+    extraModulePackages = [ ];
+  };
 
-  # Networking
-  networking.networkmanager.enable = true;
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  networking.useDHCP = lib.mkDefault true;
+  networking = {
+    hostName = "ai_server";
+    networkmanager.enable = true;
+    useDHCP = lib.mkDefault true;
+  };
 
   fileSystems."/" = {
     device = "/dev/disk/by-uuid/73998e5d-b64f-4148-bacb-af7b7883746a";
