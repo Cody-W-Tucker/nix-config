b1f5c5301912d8695cf83ebf90d87a499792fb85
Cody Tucker <cody@tmvsocial.com>
2026-01-06 15:36:18 -0600

    Switch aiserver to use nixpkgs-unstable entirely

--- flake.nix
+++ flake.nix
@@ -224,12 +224,13 @@
             )
           ];
         };
-        aiserver = nixpkgs.lib.nixosSystem {
-          inherit system;
-          specialArgs = {
-            inherit inputs;
-            inherit pkgs-unstable;
-          };
+         aiserver = nixpkgs-unstable.lib.nixosSystem {
+           inherit system;
+           specialArgs = {
+             inherit inputs;
+             nixpkgs = nixpkgs-unstable;
+             inherit pkgs-unstable;
+           };
           modules = [
             ./hosts/aiserver.nix
             # Using community hardware nixosConfigurations
