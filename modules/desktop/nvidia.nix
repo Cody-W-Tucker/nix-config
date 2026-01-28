c007f1d3c28526e3ce4e3713a6fb98484cad448f
Cody Tucker <cody@tmvsocial.com>
2026-01-28 13:31:55 -0600

    Revert "use beta nvidia driver"
    This reverts commit e7eac07495123e2304b3a59a2e316cf42104f65e.

--- modules/desktop/nvidia.nix
+++ modules/desktop/nvidia.nix
@@ -7,7 +7,7 @@
   hardware.nvidia = {
     open = true;
     powerManagement.enable = true;
-    package = config.boot.kernelPackages.nvidiaPackages.beta;
+    package = config.boot.kernelPackages.nvidiaPackages.production;
   };
 
   # Build apps with CUDA support
