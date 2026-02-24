0c12a5b2d61abd137d81cfe660f3aef84d03a5b5
Cody Tucker <cody@tmvsocial.com>
2026-02-24 13:56:33 -0600

    feat: add airplay (uxplay) support for screen mirroring
    fix: enable Avahi publishing for AirPlay service discovery

--- modules/desktop/default.nix
+++ modules/desktop/default.nix
@@ -19,6 +19,27 @@
     gvfs.enable = true;
     # Enable support for removable devices.
     udisks2.enable = true;
+
+    # Avahi for AirPlay mDNS discovery
+    avahi = {
+      enable = true;
+      nssmdns4 = true;
+      openFirewall = true;
+      publish = {
+        enable = true;
+        userServices = true;
+      };
+    };
+  };
+
+  # uxplay: AirPlay server
+  systemd.user.services.uxplay = {
+    description = "uXPlay AirPlay Server";
+    wantedBy = [ "hyprland-session.target" ]; # Auto-start on Hyprland
+    serviceConfig = {
+      ExecStart = "${pkgs.uxplay}/bin/uxplay -v --no-video-ads";
+      Restart = "always";
+    };
   };
 
   # Install basic desktop environment packages that I want on all my systems.
@@ -38,10 +59,11 @@
       cifs-utils # For mounting CIFS shares
       seahorse # GNOME keyring manager
       openrazer-daemon # Razer device support
+      uxplay # AirPlay server
     ]
   );
 
-  # Open ports for rustdesk
+  # Open ports for rustdesk and AirPlay
   networking.firewall.allowedTCPPorts = [
     21115
     21116
@@ -50,6 +72,11 @@
     21119
     8384 # Syncthing GUI
     22000 # Syncthing sync protocol
+    # AirPlay (uxplay)
+    5000
+    7000
+    7100
+    9000
   ];
   networking.firewall.allowedUDPPorts = [
     21115
@@ -59,5 +86,9 @@
     21119
     22000 # Syncthing sync protocol
     21027 # Syncthing local discovery
+    # AirPlay (uxplay)
+    5353
+    5351
+    7000
   ];
 }
