86a06a4eeefa832c6502cc01f0dd9237692c7b6c
Cody Tucker <cody@tmvsocial.com>
2026-02-24 14:04:28 -0600

    Revert "feat: add airplay (uxplay) support for screen mirroring"
    This reverts commit 2f1aa8b45106d74167ef962502827f5f06e2b55b.

--- modules/desktop/default.nix
+++ modules/desktop/default.nix
@@ -19,27 +19,6 @@
     gvfs.enable = true;
     # Enable support for removable devices.
     udisks2.enable = true;
-
-    # Avahi for AirPlay mDNS discovery
-    avahi = {
-      enable = true;
-      nssmdns4 = true;
-      openFirewall = true;
-      publish = {
-        enable = true;
-        userServices = true;
-      };
-    };
-  };
-
-  # uxplay: AirPlay server
-  systemd.user.services.uxplay = {
-    description = "uXPlay AirPlay Server";
-    wantedBy = [ "hyprland-session.target" ]; # Auto-start on Hyprland
-    serviceConfig = {
-      ExecStart = "${pkgs.uxplay}/bin/uxplay -v --no-video-ads";
-      Restart = "always";
-    };
   };
 
   # Install basic desktop environment packages that I want on all my systems.
@@ -59,11 +38,10 @@
       cifs-utils # For mounting CIFS shares
       seahorse # GNOME keyring manager
       openrazer-daemon # Razer device support
-      uxplay # AirPlay server
     ]
   );
 
-  # Open ports for rustdesk and AirPlay
+  # Open ports for rustdesk
   networking.firewall.allowedTCPPorts = [
     21115
     21116
@@ -72,11 +50,6 @@
     21119
     8384 # Syncthing GUI
     22000 # Syncthing sync protocol
-    # AirPlay (uxplay)
-    5000
-    7000
-    7100
-    9000
   ];
   networking.firewall.allowedUDPPorts = [
     21115
@@ -86,9 +59,5 @@
     21119
     22000 # Syncthing sync protocol
     21027 # Syncthing local discovery
-    # AirPlay (uxplay)
-    5353
-    5351
-    7000
   ];
 }
