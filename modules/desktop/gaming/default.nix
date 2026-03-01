b6100f07e8c0c273be7cd4599066343c569f4e3a
Cody W Tucker <cody@tmvsocial.com>
2026-03-01 15:59:22 -0600

    formatting

--- modules/desktop/gaming/default.nix
+++ modules/desktop/gaming/default.nix
@@ -5,13 +5,16 @@
   hardware.graphics.enable32Bit = true;
 
   # Gaming Configuration
-  programs.steam.enable = true;
-  programs.steam.gamescopeSession.enable = true;
-  programs.gamemode.enable = true;
+  programs = {
+    steam = {
+      enable = true;
+      gamescopeSession.enable = true;
+    };
+    gamemode.enable = true;
+  };
 
   # Gaming specific packages
   environment.systemPackages = with pkgs; [
-    # list of stable packages go here
     gamescope-wsi
   ];
 
