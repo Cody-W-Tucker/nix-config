f7439c9cae34f0d9ef897d92fef5bd62e3cafe41
Cody W Tucker <cody@tmvsocial.com>
2026-03-03 10:48:01 -0600

    use nixos service for budget tool
    use nixos service for budget tool

--- modules/server/actualBudget.nix
+++ modules/server/actualBudget.nix
@@ -1,12 +1,4 @@
 {
-  virtualisation.oci-containers.containers."actualBudget" = {
-    autoStart = true;
-    image = "ghcr.io/actualbudget/actual-server:latest";
-    ports = [ "5006:5006" ];
-    volumes = [ "/var/lib/actual-server:/data" ];
-    extraOptions = [ "--pull=always" ];
-  };
-
   services.nginx.virtualHosts."budget.homehub.tv" = {
     forceSSL = true;
     useACMEHost = "homehub.tv";
@@ -17,30 +9,11 @@
     kTLS = true;
   };
 
-  systemd.services.restartActualBudget = {
-    description = "Restart ActualBudget service";
-    serviceConfig = {
-      Type = "oneshot";
-      ExecStart = "systemctl restart docker-actualBudget.service";
-    };
+  services.actual = {
+    enable = true;
+    settings.port = 5006;
+    openFirewall = true;
+    # settings.hostname = "budget.homehub.tv";
   };
-
-  systemd.timers.restartActualBudget = {
-    wantedBy = [ "timers.target" ];
-    partOf = [ "restartActualBudget.service" ];
-    timerConfig = {
-      OnCalendar = "Wed *-*-* 02:00:00";
-      RandomizedDelaySec = "2h";
-      Persistent = true;
-    };
-  };
-
-  # TODO: Once actual is added to nixos-stable we can implement this.
-  # services.actual = {
-  #   enable = true;
-  #   port = 5007;
-  #   settings.hostname = "budget.homehub.tv";
-  # };
 
 }
-# docker run --pull=always --restart=unless-stopped -d -p 5006:5006 -v /var/lib/actual-budget:/data --name actualBudget ghcr.io/actualbudget/actual-server:latest
