df55f1f7cd95780c96899e4b779cb0a674193708
Cody W Tucker <cody@tmvsocial.com>
2026-03-04 10:21:48 -0600

    use cache to implement cuda support for qmd

--- modules/desktop/hardware/nvidia.nix
+++ modules/desktop/hardware/nvidia.nix
@@ -28,11 +28,11 @@
 
   # Use substituters to avoid building from source
   nix.settings = {
-    substituters = [
+    extra-substituters = [
       "https://cache.nixos-cuda.org" # https://wiki.nixos.org/wiki/CUDA#Setting_up_CUDA_Binary_Cache
       "https://cache.flox.dev" # https://discourse.nixos.org/t/nix-flox-nvidia-opening-up-cuda-redistribution-on-nix/69189
     ];
-    trusted-public-keys = [
+    extra-trusted-public-keys = [
       "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
       "flox-cache-public-1:7F4OyH7ZCnFhcze3fJdfyXYLQw/aV7GEed86nQ7IsOs="
     ];
