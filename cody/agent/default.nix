b2eb9d4c4b5e35fc3c94b7a9165246dda68534d1
Cody W Tucker <cody@tmvsocial.com>
2026-03-04 09:17:10 -0600

    add qmd search

--- cody/agent/default.nix
+++ cody/agent/default.nix
@@ -1,6 +1,13 @@
+{ inputs, pkgs, ... }:
+
 {
   imports = [
     ./opencode
     ./mcp.nix
+  ];
+
+  home.packages = [
+    # Quick Markdown Search (QMD)
+    inputs.qmd.packages.${pkgs.stdenv.hostPlatform.system}.qmd
   ];
 }
