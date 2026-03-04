658dd09b469139aca9166af75b46ffca33927dd1
Cody W Tucker <cody@tmvsocial.com>
2026-03-04 09:43:53 -0600

    Revert "add qmd search"
    This reverts commit 4aaa0e455c83525dd31312044bd944347135645c.

--- cody/agent/default.nix
+++ cody/agent/default.nix
@@ -1,13 +1,6 @@
-{ inputs, pkgs, ... }:
-
 {
   imports = [
     ./opencode
     ./mcp.nix
-  ];
-
-  home.packages = [
-    # Quick Markdown Search (QMD)
-    inputs.qmd.packages.${pkgs.stdenv.hostPlatform.system}.qmd
   ];
 }
