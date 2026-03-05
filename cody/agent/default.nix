ce747b4174099b62e5eb07a11f89edc54e331e84
Cody W Tucker <cody@tmvsocial.com>
2026-03-05 07:50:08 -0600

    add desloppify

--- cody/agent/default.nix
+++ cody/agent/default.nix
@@ -1,5 +1,21 @@
 { inputs, pkgs, ... }:
 
+let
+  desloppify = pkgs.python3Packages.buildPythonPackage rec {
+    pname = "desloppify";
+    version = "0.7.7";
+    format = "wheel";
+    src = pkgs.fetchPypi {
+      inherit pname version format;
+      python = "py3";
+      platform = "any";
+      sha256 = "18f5pmqp98r06fba5yamc3bwf423b5fmvg6d63970ykm5564922q";
+    };
+    propagatedBuildInputs = with pkgs.python3Packages; [
+      # Add dependencies here if needed
+    ];
+  };
+in
 {
   imports = [
     ./opencode
@@ -12,5 +28,6 @@
     rtk # Token reducer for command-line tools
     openspec # Spec driven development tool
     ck # Semantic search for code
+    desloppify
   ];
 }
