bf09cf010b08fe3bf4143eeb11eb58f5a49014d7
Cody W Tucker <cody@tmvsocial.com>
2026-03-12 21:21:05 -0500

    remove virtual camera from obs

--- hosts/beast.nix
+++ hosts/beast.nix
@@ -67,15 +67,8 @@ in
     ];
     kernelModules = [
       "kvm-intel"
-      "v4l2loopback"
       "btusb"
     ];
-    extraModulePackages = with config.boot.kernelPackages; [
-      v4l2loopback # for virtual camera in obs
-    ];
-    extraModprobeConfig = ''
-      options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
-    '';
   };
 
   # Networking
