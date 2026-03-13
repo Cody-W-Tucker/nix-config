5a4b5f3b6e5f06cea6e41e4b3759c95934db74e9
Cody W Tucker <cody@tmvsocial.com>
2026-03-12 21:18:00 -0500

    formatting

--- hosts/server.nix
+++ hosts/server.nix
@@ -34,59 +34,57 @@
   };
 
   # Bootloader
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  networking.hostName = "server";
-
-  boot.initrd.availableKernelModules = [
-    "xhci_pci"
-    "ahci"
-    "nvme"
-    "usbhid"
-    "sd_mod"
-  ];
-  boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [
-    "kvm-intel"
-    "i915"
-  ];
-  boot.kernelParams = [ "i915.enable_guc=2" ];
-  boot.extraModulePackages = [ ];
+  boot = {
+    loader.systemd-boot.enable = true;
+    loader.efi.canTouchEfiVariables = true;
+    initrd.availableKernelModules = [
+      "xhci_pci"
+      "ahci"
+      "nvme"
+      "usbhid"
+      "sd_mod"
+    ];
+    initrd.kernelModules = [ ];
+    kernelModules = [
+      "kvm-intel"
+      "i915"
+    ];
+    kernelParams = [ "i915.enable_guc=2" ];
+    extraModulePackages = [ ];
+  };
 
   # Networking
   networking.networkmanager.enable = true;
 
-  fileSystems."/" = {
-    device = "/dev/disk/by-uuid/c6c7b5c2-8edf-4aa5-9c6d-cbcd7498db1d";
-    fsType = "ext4";
-  };
-
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/763D-AB92";
-    fsType = "vfat";
-  };
-
-  fileSystems."/mnt/media" = {
-    device = "/dev/disk/by-uuid/27ddc2ef-8f21-401d-b9eb-3ed4541c16c9";
-    fsType = "ext4";
-  };
-
-  fileSystems."/mnt/dev/sr0" = {
-    device = "/dev/sr0";
-    fsType = "udf,iso9660";
-    options = [
-      "users"
-      "noauto"
-      "exec"
-      "utf8"
-    ];
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-uuid/c6c7b5c2-8edf-4aa5-9c6d-cbcd7498db1d";
+      fsType = "ext4";
+    };
+    "/boot" = {
+      device = "/dev/disk/by-uuid/763D-AB92";
+      fsType = "vfat";
+    };
+    "/mnt/media" = {
+      device = "/dev/disk/by-uuid/27ddc2ef-8f21-401d-b9eb-3ed4541c16c9";
+      fsType = "ext4";
+    };
+    "/mnt/dev/sr0" = {
+      device = "/dev/sr0";
+      fsType = "udf,iso9660";
+      options = [
+        "users"
+        "noauto"
+        "exec"
+        "utf8"
+      ];
+    };
   };
 
   swapDevices = [ ];
 
   # Auto configure usb etc, when plugedin
   services.udisks2.enable = true;
-  security.polkit.enable = true;
 
   # NFS server for sharing media directory
   services.nfs.server.enable = true;
