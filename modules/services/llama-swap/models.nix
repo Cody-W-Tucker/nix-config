7cd7b9926184988b124cc5ca82d3d612b91fc3f5
Cody W Tucker <cody@tmvsocial.com>
2026-03-13 22:08:31 -0500

    add models defaults

--- modules/services/llama-swap/models.nix
+++ modules/services/llama-swap/models.nix
@@ -1,27 +1,27 @@
 {
   "qwen3.5-0.8b" = {
     file = "Qwen3.5-0.8B-UD-Q8_K_XL.gguf";
-    gpuLayers = 1;
+    gpuLayers = 999;
     contextSize = 8192;
-    threads = 4;
+    threads = 2;
     batchSize = 1024;
     ubatchSize = 512;
     ttl = 300;
   };
   "qwen3.5-4b" = {
     file = "Qwen3.5-4B-Q8_0.gguf";
-    gpuLayers = 8;
+    gpuLayers = 999;
     contextSize = 16384;
-    threads = 8;
+    threads = 4;
     batchSize = 1536;
     ubatchSize = 768;
     ttl = 450;
   };
   "qwen3.5-9b" = {
     file = "Qwen3.5-9B-Q4_K_M.gguf";
-    gpuLayers = 16;
+    gpuLayers = 999;
     contextSize = 32768;
-    threads = 16;
+    threads = 8;
     batchSize = 2048;
     ubatchSize = 512;
     ttl = 900;
@@ -30,7 +30,7 @@
     file = "Qwen3.5-9B-UD-Q8_K_XL.gguf";
     gpuLayers = 999;
     contextSize = 32768;
-    threads = 16;
+    threads = 8;
     batchSize = 2048;
     ubatchSize = 512;
     ttl = 900;
@@ -39,8 +39,8 @@
     file = "Qwen3.5-35B-A3B-Q8_0.gguf";
     gpuLayers = 999;
     contextSize = 65536;
-    threads = 32;
-    batchSize = 2048;
+    threads = 16;
+    batchSize = 1024;
     ubatchSize = 768;
     ttl = 1800;
   };
