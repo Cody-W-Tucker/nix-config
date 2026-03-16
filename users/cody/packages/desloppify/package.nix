8005b4cb5442b2b6b4971452ed2aee2c96bc85dd
Cody W Tucker <cody@tmvsocial.com>
2026-03-16 11:05:12 -0500

    give deslop python runtime

--- users/cody/packages/desloppify/package.nix
+++ users/cody/packages/desloppify/package.nix
@@ -8,6 +8,9 @@ pkgs.python3Packages.buildPythonPackage rec {
     pkgs.python3Packages.setuptools
     pkgs.python3Packages.defusedxml
   ];
+  propagatedBuildInputs = [
+    pkgs.python3
+  ];
   src = pkgs.fetchPypi {
     inherit pname version;
     sha256 = "sha256-KbGEJ1U06mAM5vGfNxc9qa514ZFT7lUvuARMyrbs+J0=";
