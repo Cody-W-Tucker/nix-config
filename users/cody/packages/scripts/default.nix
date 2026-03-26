968574776ec3e378a4c0c274fb55aa99b89953e4
Cody W Tucker <cody@tmvsocial.com>
2026-03-26 12:00:50 -0500

    remove all task runners and kanban
    remove all task runners and kanban

--- users/cody/packages/scripts/default.nix
+++ users/cody/packages/scripts/default.nix
@@ -3,23 +3,14 @@
 let
   scriptNames = [
     ./focus-or-run.nix
-    ./kanban-launcher.nix
     ./project.nix
     ./rofi-launcher.nix
     ./rofi-web-launcher.nix
-    ./task-runner.nix
   ];
 
   scriptPackages = map (script: pkgs.callPackage (toString script) { inherit pkgs; }) scriptNames;
 in
 
 {
-  imports = [
-    ./kanban-watcher.nix
-  ];
-
   home.packages = scriptPackages;
-
-  # Enable kanban watcher service
-  services.kanban-watcher.enable = true;
 }
