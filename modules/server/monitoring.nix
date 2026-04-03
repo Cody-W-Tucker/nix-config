ab13e567b6b5d728689d6532583ded30bd617662
Cody W Tucker <cody@tmvsocial.com>
2026-04-03 18:05:49 -0500

    fix: grafana couldn't find datasource

--- modules/server/monitoring.nix
+++ modules/server/monitoring.nix
@@ -24,18 +24,24 @@
             uid = "prometheus";
             type = "prometheus";
             url = "http://localhost:${toString config.services.prometheus.port}";
+            jsonData = { };
+            secureJsonData = { };
           }
           {
             name = "Loki";
             uid = "loki";
             type = "loki";
             url = "http://localhost:${toString config.services.loki.configuration.server.http_listen_port}";
+            jsonData = { };
+            secureJsonData = { };
           }
           {
             name = "Tempo";
             uid = "tempo";
             type = "tempo";
             url = "http://localhost:${toString config.services.tempo.settings.server.http_listen_port}";
+            jsonData = { };
+            secureJsonData = { };
           }
         ];
       };
