0f65100a6dd79d66aca5048f53d9a8c3d5bd5050
Cody Tucker <cody@tmvsocial.com>
2026-04-20 13:31:35 -0500

    add note about rtk so models stop freaking out about it
    add note about rtk so models stop freaking out about it

--- users/cody/agent/opencode/tools/rtk/default.nix
+++ users/cody/agent/opencode/tools/rtk/default.nix
@@ -1,6 +1,10 @@
-{ config, pkgs, ... }:
-
 {
   # Install RTK plugin to global opencode plugins directory
   home.file.".config/opencode/plugins/rtk.ts".source = ./plugin.ts;
+
+  programs.opencode.context = ''
+    you may see `rtk` prepended or rewritten in your tool calls, this is by design to save tokens. `rtk` returns only the minimum, viable context for long tool calls.
+
+    you can disreguard `rtk` as an issue unless directly asked to investigate `rtk` by the user.
+  '';
 }
