83e1b4dfa9527f5d49a17a0ad2e766fa9e3cc4ec
Cody W Tucker <cody@tmvsocial.com>
2026-05-04 18:37:11 -0500

    use nixpkgs version of open-webui
    use nixpkgs version of open-webui
    
    use nixpkgs version of open-webui

--- modules/server/ai/default.nix
+++ modules/server/ai/default.nix
@@ -1,44 +1,48 @@
 { pkgs, ... }:
 
 {
-  # Enable CUDA in containers
-  hardware.nvidia-container-toolkit.enable = true;
-
-  # Docker
-  virtualisation.docker = {
-    enable = true;
-    rootless = {
+  # Docker is still used for Open WebUI pipelines.
+  virtualisation = {
+    docker = {
       enable = true;
-      setSocketVariable = true;
+      rootless = {
+        enable = true;
+        setSocketVariable = true;
+      };
+      autoPrune = {
+        enable = true;
+        dates = "weekly";
+      };
     };
-    autoPrune = {
-      enable = true;
-      dates = "weekly";
+    oci-containers.backend = "docker";
+    oci-containers.containers = {
+      "pipelines" = {
+        autoStart = true;
+        image = "ghcr.io/open-webui/pipelines:main";
+        ports = [ "9099:9099" ];
+        volumes = [ "/var/lib/pipelines/pipelines:/app/pipelines" ];
+        extraOptions = [
+          "--add-host=host.docker.internal:host-gateway"
+          "--network=host"
+          "--pull=always"
+        ];
+      };
     };
   };
-  virtualisation.oci-containers.backend = "docker";
-
-  virtualisation.oci-containers.containers = {
-    "open-webui" = {
-      autoStart = true;
-      image = "ghcr.io/open-webui/open-webui:cuda";
-      ports = [ "8080:8080" ];
-      volumes = [ "/var/lib/open-webui:/app/backend/data" ];
-      extraOptions = [
-        "--pull=always"
-        "--add-host=host.docker.internal:host-gateway"
-        "--network=host"
-        "--device=nvidia.com/gpu=all"
-      ];
+  # Local AI models
+  services = {
+    open-webui = {
+      enable = true;
+      package = pkgs.open-webui.overridePythonAttrs (oldAttrs: {
+        dependencies = oldAttrs.dependencies ++ [ pkgs.python313Packages.qdrant-client ];
+      });
+      host = "0.0.0.0";
+      port = 8080;
+      stateDir = "/var/lib/open-webui";
+      openFirewall = true;
       environment = {
         WEBUI_URL = "https://ai.homehub.tv";
-        ANONYMIZED_TELEMETRY = "False";
-        DO_NOT_TRACK = "True";
-        SCARF_NO_ANALYTICS = "True";
         WEBUI_SECRET_KEY = "local-only";
-
-        # Local AI
-        USE_CUDA_DOCKER = "True";
 
         # Vector DB
         VECTOR_DB = "qdrant";
@@ -58,23 +62,8 @@
         WHISPER_VAD_FILTER = "True";
       };
     };
-    # docker run -d -p 9099:9099 --add-host=host.docker.internal:host-gateway -v pipelines:/app/pipelines --name pipelines --restart always ghcr.io/open-webui/pipelines:main
-    "pipelines" = {
-      autoStart = true;
-      image = "ghcr.io/open-webui/pipelines:main";
-      ports = [ "9099:9099" ];
-      volumes = [ "/var/lib/pipelines/pipelines:/app/pipelines" ];
-      extraOptions = [
-        "--add-host=host.docker.internal:host-gateway"
-        "--network=host"
-        "--pull=always"
-      ];
-    };
-  };
-  # Local AI models
-  services = {
-    # Content extraction
     tika = {
+      # Content extraction
       enable = true;
       port = 9998;
     };
@@ -98,11 +87,12 @@
       };
     };
   };
-  # Make the open-webui and pipelines dirs
+  # Make the pipelines dir
   systemd.tmpfiles.rules = [
-    "d /var/lib/open-webui 0755 root root - -"
     "d /var/lib/pipelines 0755 root root - -"
   ];
+  systemd.services."docker-pipelines".requires = [ "docker.service" ];
+
   # Since we run open-webui on beast and the nginx server is on the server, we must open the port so the web server can proxy them
   networking.firewall.allowedTCPPorts = [
     8080
