fcb8147dee8e52a04078c05a23a0832a9c826ff7
Cody W Tucker <cody@tmvsocial.com>
2026-05-04 19:47:45 -0500

    add keyring unlocker via greetd

--- modules/desktop/hyprland.nix
+++ modules/desktop/hyprland.nix
@@ -1,5 +1,3 @@
-{ ... }:
-
 {
   # Enable the Hyprland Desktop Environment.
   programs.hyprland = {
@@ -12,6 +10,8 @@
     hyprlock = { };
     # Enable a keyring service for storing secrets
     login.enableGnomeKeyring = true;
+    # Keyring auto-unlock on greetd login (this is the missing piece for most greetd users)
+    greetd.enableGnomeKeyring = true;
   };
 
   # Use Greetd to launch Hyprland from TTY2
