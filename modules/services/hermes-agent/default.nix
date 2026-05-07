75be27ae63fedf9a3229701a0679313bab2a6cc2
Cody W Tucker <cody@tmvsocial.com>
2026-05-07 18:18:45 -0500

    add discord bot for hermes

--- modules/services/hermes-agent/default.nix
+++ modules/services/hermes-agent/default.nix
@@ -19,12 +19,16 @@ in
   sops = {
     secrets = {
       "opencode-zen-api-key" = { };
+      "hermes-discord-bot-token" = { };
+      "hermes-discord-allowed-users" = { };
       "hermes-telegram-bot-token" = { };
       "hermes-telegram-allowed-users" = { };
     };
     templates."hermes-env" = {
       content = ''
         OPENCODE_ZEN_API_KEY=${config.sops.placeholder."opencode-zen-api-key"}
+        DISCORD_BOT_TOKEN=${config.sops.placeholder."hermes-discord-bot-token"}
+        DISCORD_ALLOWED_USERS=${config.sops.placeholder."hermes-discord-allowed-users"}
         TELEGRAM_BOT_TOKEN=${config.sops.placeholder."hermes-telegram-bot-token"}
         TELEGRAM_ALLOWED_USERS=${config.sops.placeholder."hermes-telegram-allowed-users"}
       '';
