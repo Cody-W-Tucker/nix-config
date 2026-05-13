f71fc5e7ba44984b3c6c6148e82117a229492fef
Cody W Tucker <cody@tmvsocial.com>
2026-05-13 10:40:25 -0500

    fix Focus to run trade behavior

--- users/cody/desktop/packages/scripts/focus-or-run.nix
+++ users/cody/desktop/packages/scripts/focus-or-run.nix
@@ -19,6 +19,14 @@ pkgs.writeShellApplication {
     APP_CMD="$2"
     APP_KEY="''${APP_CLASS%% *}"
     APP_KEY="''${APP_KEY##*/}"
+    APP_KEY_LOWER="$(printf '%s' "$APP_KEY" | tr '[:upper:]' '[:lower:]')"
+
+    matches_app_key() {
+      printf '%s\n' "$@" \
+        | tr '[:upper:]' '[:lower:]' \
+        | tr -cs '[:alnum:]_-' '\n' \
+        | grep -Fxq "$APP_KEY_LOWER"
+    }
 
     activate_tray_item() {
       if ! busctl --user --quiet status org.kde.StatusNotifierWatcher >/dev/null 2>&1; then
@@ -28,10 +36,11 @@ pkgs.writeShellApplication {
       while IFS= read -r item; do
         service="''${item%%/*}"
         path="/''${item#*/}"
+        id=$(busctl --user --json=short get-property "$service" "$path" org.kde.StatusNotifierItem Id 2>/dev/null | jq -r '.data // ""' || true)
         title=$(busctl --user --json=short get-property "$service" "$path" org.kde.StatusNotifierItem Title 2>/dev/null | jq -r '.data // ""' || true)
         tooltip=$(busctl --user --json=short get-property "$service" "$path" org.kde.StatusNotifierItem ToolTip 2>/dev/null | jq -r '.data[2] // ""' || true)
 
-        if printf '%s\n%s\n' "$title" "$tooltip" | grep -Fqi "$APP_KEY"; then
+        if matches_app_key "$id" "$title" "$tooltip"; then
           busctl --user call "$service" "$path" org.kde.StatusNotifierItem Activate ii 0 0 >/dev/null
           return 0
         fi
