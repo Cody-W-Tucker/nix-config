ee087763693d108aa064f2197df98cc011ccdd3e
Cody W Tucker <cody@tmvsocial.com>
2026-05-30 18:14:59 -0500

    stop keeping crashes forever

--- modules/system/base.nix
+++ modules/system/base.nix
@@ -5,6 +5,13 @@
 }:
 
 {
+  # Cap coredump storage so /var/lib/systemd/coredump does not balloon.
+  systemd.coredump.extraConfig = ''
+    MaxUse=500M
+    KeepFree=2G
+    ProcessSizeMax=100M
+  '';
+
   imports = [
     inputs.sops-nix.nixosModules.sops
     home-manager-input.nixosModules.home-manager
