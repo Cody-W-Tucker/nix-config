e9402f9af3431151b3e64fe1cbc12239843d118e
Cody W Tucker <cody@tmvsocial.com>
2026-06-19 15:33:48 -0500

    remove miniflux mcp from hermes

--- modules/services/hermes-agent/mcp/default.nix
+++ modules/services/hermes-agent/mcp/default.nix
@@ -16,39 +16,6 @@ let
       exec npx -y @karakeep/mcp "$@"
     '';
   };
-
-  minifluxMcp = pkgs.buildGoModule {
-    pname = "miniflux-mcp";
-    version = "unstable-2025-11-25";
-
-    src = pkgs.fetchFromGitHub {
-      owner = "tssujt";
-      repo = "miniflux-mcp";
-      rev = "793ed2198eddfdb4efc8163251e70091425909a2";
-      hash = "sha256-OQKQYH7ZtTQh0BJfQzsgVRQoMnfFXwrCi3Q8hbCiBdY=";
-    };
-
-    vendorHash = "sha256-K7Gg65q/XOFu8wHKiyPIHKoF13GcIL3TICrQ0mh+HcQ=";
-    subPackages = [ "." ];
-
-    meta = {
-      description = "Writable MCP server for Miniflux";
-      homepage = "https://github.com/tssujt/miniflux-mcp";
-      license = pkgs.lib.licenses.mit;
-      mainProgram = "miniflux-mcp";
-    };
-  };
-
-  minifluxMcpWrapped = pkgs.writeShellApplication {
-    name = "miniflux-mcp";
-    text = ''
-      export MINIFLUX_URL="https://rss.homehub.tv"
-      MINIFLUX_API_KEY="$(< ${config.sops.secrets."miniflux/API_KEY".path})"
-      export MINIFLUX_API_KEY
-
-      exec ${minifluxMcp}/bin/miniflux-mcp "$@"
-    '';
-  };
 in
 {
   config = {
@@ -58,16 +25,9 @@ in
         inherit (config.services.hermes-agent) group;
         mode = "0440";
       };
-      "miniflux/API_KEY" = {
-        owner = config.services.hermes-agent.user;
-        inherit (config.services.hermes-agent) group;
-        mode = "0440";
-      };
     };
-
     services.hermes-agent = {
       mcpServers.karakeep.command = "${karakeepMcp}/bin/karakeep-mcp";
-      mcpServers.miniflux.command = "${minifluxMcpWrapped}/bin/miniflux-mcp";
     };
   };
 }
--- modules/services/hermes-agent/secrets/default.nix
+++ modules/services/hermes-agent/secrets/default.nix
@@ -6,7 +6,6 @@
       "opencode-api-key" = { };
       "xai-api-key" = { };
       "firecrawl-api-key" = { };
-      "miniflux/API_KEY" = { };
       "hermes-discord-bot-token" = { };
       "hermes-discord-allowed-users" = { };
       "hermes-telegram-bot-token" = { };
@@ -18,8 +17,6 @@
         OPENCODE_GO_API_KEY=${config.sops.placeholder."opencode-api-key"}
         XAI_API_KEY=${config.sops.placeholder."xai-api-key"}
         FIRECRAWL_API_KEY=${config.sops.placeholder."firecrawl-api-key"}
-        MINIFLUX_URL=https://rss.homehub.tv
-        MINIFLUX_API_KEY=${config.sops.placeholder."miniflux/API_KEY"}
         DISCORD_BOT_TOKEN=${config.sops.placeholder."hermes-discord-bot-token"}
         DISCORD_ALLOWED_USERS=${config.sops.placeholder."hermes-discord-allowed-users"}
         TELEGRAM_BOT_TOKEN=${config.sops.placeholder."hermes-telegram-bot-token"}
