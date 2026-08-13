ba9aab98c68872c4cf86dc6e2e929bdd45a4ed81
Cody W Tucker <cody@tmvsocial.com>
2026-08-13 14:28:56 -0500

    change karakeep defaults

--- modules/nas/karakeep.nix
+++ modules/nas/karakeep.nix
@@ -8,17 +8,16 @@
     enable = true;
     extraEnvironment = {
       PORT = "3005";
-      LOG_LEVEL = "warning"; # Switch to warning after live.
-      DB_WAL_MODE = "true"; # Enables WAL mode for the sqlite database. This should improve the performance of the database.
-      # DISABLE_SIGNUPS = "true";
+      DB_WAL_MODE = "true"; # This should improve the performance of the database.
+      DISABLE_SIGNUPS = "true";
       DISABLE_NEW_RELEASE_CHECK = "true";
-      OPENAI_API_KEY = "opencode";
+      OPENAI_API_KEY = "blank";
       OPENAI_BASE_URL = "http://nas:8081/v1";
 
       INFERENCE_TEXT_MODEL = "qwen-3.5-9b";
       OCR_USE_LLM = "true";
       INFERENCE_IMAGE_MODEL = "qwen-3.5-9b-task";
-      INFERENCE_CONTEXT_LENGTH = "65536";
+      INFERENCE_CONTEXT_LENGTH = "8192";
       INFERENCE_ENABLE_AUTO_SUMMARIZATION = "true";
 
       EMBEDDING_ENABLE_AUTO_INDEXING = "true";
