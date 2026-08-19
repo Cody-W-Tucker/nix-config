c10d2c63f824b78bb55b4f3b7a81a3ebd94514c9
Cody W Tucker <cody@tmvsocial.com>
2026-08-18 19:44:14 -0500

    use unstable for mealie

--- modules/nas/mealie.nix
+++ modules/nas/mealie.nix
@@ -1,17 +1,15 @@
 {
+  inputs,
   mkNginxVhost,
+  pkgs,
   ...
 }:
 
 {
   services.mealie = {
     enable = true;
+    package = inputs.nixpkgs-unstable.legacyPackages.${pkgs.stdenv.hostPlatform.system}.mealie;
     port = 9000;
-    settings = {
-      OPENAI_BASE_URL = "http://127.0.0.1:8081/v1";
-      OPENAI_API_KEY = "local-only";
-      OPENAI_MODEL = "qwen-3.5-9b-task";
-    };
   };
 
   services.nginx.virtualHosts = mkNginxVhost {
