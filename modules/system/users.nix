8254ee85596be66b8588f4700a30da87eb499f1f
Cody W Tucker <cody@tmvsocial.com>
2026-09-10 07:56:09 -0500

    remove codyt from docker group

--- modules/system/users.nix
+++ modules/system/users.nix
@@ -4,38 +4,36 @@
 
 {
   # Make passwords uneditable
-  users.mutableUsers = false;
-
-  users.users.codyt = {
-    isNormalUser = true;
-    description = "Cody Tucker";
-    extraGroups = [
-      "networkmanager"
-      "wheel"
-      "docker"
-      "media"
-      "scanner"
-      "lp"
-      "bluetooth"
-      "input"
-      "documents"
-    ];
-    shell = pkgs.zsh;
-    hashedPassword = "$y$j9T$2gGzaHfv1JMUMtHdaXBGF/$RoEaBINI46v1yFpR1bSgPc9ovAyzqjgSSTxuNhRiOn4";
-    openssh = {
-      authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJkUAtqd1GcKYejbmpxjLzXdMoDojpVuNXEEBhYQjVgY cody@tmvsocial.com"
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMmuJot98ty1ToPLarWkw2gwNbYMltdREaiUPuDQbTC9 codyt@nas"
+  users = {
+    mutableUsers = false;
+    users.codyt = {
+      isNormalUser = true;
+      description = "Cody Tucker";
+      extraGroups = [
+        "networkmanager"
+        "wheel"
+        "media"
+        "scanner"
+        "lp"
+        "bluetooth"
+        "input"
+        "documents"
       ];
+      shell = pkgs.zsh;
+      hashedPassword = "$y$j9T$2gGzaHfv1JMUMtHdaXBGF/$RoEaBINI46v1yFpR1bSgPc9ovAyzqjgSSTxuNhRiOn4";
+      openssh = {
+        authorizedKeys.keys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJkUAtqd1GcKYejbmpxjLzXdMoDojpVuNXEEBhYQjVgY cody@tmvsocial.com"
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMmuJot98ty1ToPLarWkw2gwNbYMltdREaiUPuDQbTC9 codyt@nas"
+        ];
+      };
     };
+    groups.media = { };
+    groups.documents = { };
   };
 
-  # Create user groups for different services
-  users.groups.media = { };
-  users.groups.documents = { };
-
-  # Pre-create borgbackup directories to avoid duplicate tmpfiles warnings
   systemd.tmpfiles.rules = [
+    # Pre-create borgbackup directories to avoid duplicate tmpfiles warnings
     "d /home/codyt/.cache 0755 codyt users - -"
     "d /home/codyt/.cache/borg 0755 codyt users - -"
     "d /home/codyt/.config 0755 codyt users - -"
