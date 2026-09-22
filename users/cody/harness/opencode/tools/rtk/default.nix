35f89424a74c74a4b43d2f3466894efa97f38c07
Cody W Tucker <cody@tmvsocial.com>
2026-09-22 12:18:05 -0500

    simplify rtk plugin

--- users/cody/harness/opencode/tools/rtk/default.nix
+++ users/cody/harness/opencode/tools/rtk/default.nix
@@ -8,8 +8,9 @@ in
   home.packages = [
     llmPkgs.rtk
   ];
-  # Install RTK plugin to global opencode plugins directory
-  home.file.".config/opencode/plugins/rtk.ts".source = ./plugin.ts;
+  programs.opencode.settings.plugin = [
+    "${llmPkgs.rtk}/libexec/rtk/hooks/opencode/rtk.ts"
+  ];
 
   programs.opencode.context = ''
     you may see `rtk` prepended or rewritten in your tool calls, this is by design to save tokens. `rtk` returns only the minimum, viable context for long tool calls.
