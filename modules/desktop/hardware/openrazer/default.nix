a5b86cbe3150c95b1e9dc099302738dfd5d0f109
Cody W Tucker <cody@tmvsocial.com>
2026-09-22 11:28:40 -0500

    stop writing config for openrazer

--- modules/desktop/hardware/openrazer/default.nix
+++ modules/desktop/hardware/openrazer/default.nix
@@ -1,41 +1,14 @@
-# Beast-only Razer lighting: OpenRazer daemon + Polychromatic UI.
-# NixOS hardware.openrazer does not expose restore_persistence, so we
-# replace the user-unit ExecStart with a config that includes it.
 {
-  config,
-  lib,
   pkgs,
   ...
 }:
 
-let
-  cfg = config.hardware.openrazer;
-  toPyBoolStr = b: if b then "True" else "False";
-  daemonConfFile = pkgs.writeText "razer.conf" ''
-    [General]
-    verbose_logging = ${toPyBoolStr cfg.verboseLogging}
-
-    [Startup]
-    sync_effects_enabled = ${toPyBoolStr cfg.syncEffectsEnabled}
-    devices_off_on_screensaver = ${toPyBoolStr cfg.devicesOffOnScreensaver}
-    battery_notifier = ${toPyBoolStr cfg.batteryNotifier.enable}
-    battery_notifier_freq = ${toString cfg.batteryNotifier.frequency}
-    battery_notifier_percent = ${toString cfg.batteryNotifier.percentage}
-    restore_persistence = True
-
-    [Statistics]
-    key_statistics = ${toPyBoolStr cfg.keyStatistics}
-  '';
-in
 {
   hardware.openrazer = {
     enable = true;
     users = [ "codyt" ];
-    syncEffectsEnabled = true;
+    devicesOffOnScreensaver = false;
   };
 
   environment.systemPackages = [ pkgs.polychromatic ];
-
-  systemd.user.services.openrazer-daemon.serviceConfig.ExecStart =
-    lib.mkForce "${cfg.packages.daemon}/bin/openrazer-daemon --config ${daemonConfFile} --foreground";
 }
